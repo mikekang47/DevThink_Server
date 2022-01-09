79eb39af818d6a3db26d92f803ae9eb9cb287c6a
mikekang47 <gyeong0308@gmail.com>
2022-01-10 00:55:02 +0900

    Add: Set environment

---
+++ src/main/java/com/devthink/devthink_server/DevThinkServerApplication.java
@@ -0,0 +1,13 @@
+package com.devthink.devthink_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevThinkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevThinkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devthink/devthink_server/DevThinkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthink.devthink_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevThinkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
