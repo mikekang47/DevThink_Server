46354ed0b4a85f6f522770274f5435d7cb24f95d
mikekang47 <gyeong0308@gmail.com>
2022-01-10 22:30:04 +0900

    Add: UserControllerTest.class

---
+++ src/test/java/com/devthink/devthink_server/controllers/UserControllerTest.java
@@ -0,0 +1,25 @@
+package com.devthink.devthink_server.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void registerUser() throws Exception {
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk());
+    }
+}
