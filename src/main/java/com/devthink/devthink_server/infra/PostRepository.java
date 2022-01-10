4949e89ae4c43f18077f5d8e6e0b562b7e453c48
minu kim <kmw106933@naver.com>
2022-01-11 00:45:39 +0900

    Add: PostRepository.class

---
+++ src/main/java/com/devthink/devthink_server/infra/PostRepository.java
@@ -0,0 +1,10 @@
+package com.devthink.devthink_server.infra;
+
+import com.devthink.devthink_server.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
