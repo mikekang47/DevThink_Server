5bfc764b258550215e3375e785c1c55a9c14e6c1
mikekang47 <gyeong0308@gmail.com>
2022-01-10 17:23:07 +0900

    Add: make User domain class

---
+++ src/main/java/com/devthink/devthink_server/domain/User.java
@@ -0,0 +1,36 @@
+package com.devthink.devthink_server.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    String email;
+
+    String password;
+
+    String nickName;
+
+    String phoneNum;
+
+    String stack;
+
+    String blog_addr;
+
+    String git_nickname;
+
+
+}
