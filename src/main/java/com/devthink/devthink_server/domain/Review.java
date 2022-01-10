ab7671b6a14f986083f8f799f919dc6a524833f1
jaehee <vito822@naver.com>
2022-01-10 23:36:32 +0900

    Add: create Review domain

--- src/main/java/com/devthink/devthink_server/domain/Review.java
+++ src/main/java/com/devthink/devthink_server/domain/Review.java
@@ -1,2 +1,37 @@
-package com.devthink.devthink_server.domain;public class Review {
+package com.devthink.devthink_server.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @ManyToOne(fetch = LAZY)
+    User user;
+
+    @ManyToOne(fetch = LAZY)
+    Book book;
+
+    @OneToMany
+    List<Comment> comments = new ArrayList<>();
+
+    String content;
+
+    Float Score;
+
+    String status;
 }
