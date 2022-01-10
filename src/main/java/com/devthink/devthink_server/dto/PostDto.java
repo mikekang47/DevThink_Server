e06113480e988c59b51db062be42cea203c2fa44
minu kim <kmw106933@naver.com>
2022-01-11 01:36:01 +0900

    Add: make PostDto

---
+++ src/main/java/com/devthink/devthink_server/dto/PostDto.java
@@ -0,0 +1,14 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class PostDto {
+
+}
