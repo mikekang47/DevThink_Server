ad6d30fd9c16c02658099743384b5f8f719cde1e
jaehee <vito822@naver.com>
2022-01-10 23:36:08 +0900

    Add: create Comment domain

--- src/main/java/com/devthink/devthink_server/domain/Comment.java
+++ src/main/java/com/devthink/devthink_server/domain/Comment.java
@@ -1,2 +1,35 @@
-package com.devthink.devthink_server.domain;public class Comment {
+package com.devthink.devthink_server.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @ManyToOne(fetch = LAZY)
+    User user;
+
+    //Post post;
+
+    @ManyToOne(fetch = LAZY)
+    Review review;
+
+    String content;
+
+    String status;
+
 }
