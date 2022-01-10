e097efc80d2d353327790fa2de4b7542e78cedc7
jaehee <vito822@naver.com>
2022-01-11 00:24:40 +0900

    Add: JpaReviewRepository class

--- src/main/java/com/devthink/devthink_server/infra/JpaReviewRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/JpaReviewRepository.java
@@ -1,4 +1,11 @@
 package com.devthink.devthink_server.infra;
 
-public class JPAReviewRepository {
+import com.devthink.devthink_server.domain.Review;
+import com.devthink.devthink_server.domain.ReviewRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaReviewRepository extends ReviewRepository, JpaRepository<Long, Review> {
+    Review save(Review review);
 }
