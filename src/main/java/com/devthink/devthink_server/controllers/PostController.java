873750f17e1cbf4ce56f8dbc28ef5fe2fe6165f3
minu kim <kmw106933@naver.com>
2022-01-11 00:55:51 +0900

    Feat: make write method in PostController

--- src/main/java/com/devthink/devthink_server/controllers/PostController.java
+++ src/main/java/com/devthink/devthink_server/controllers/PostController.java
@@ -1,6 +1,10 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.domain.Post;
+import com.devthink.devthink_server.infra.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/post")
 public class PostController {
+
+    private final PostRepository postRepository;
+
+    @PostMapping("/write")
+    public Post write(@RequestBody Post post){
+        postRepository.save(post);
+        return post;
+    }
 
 }
