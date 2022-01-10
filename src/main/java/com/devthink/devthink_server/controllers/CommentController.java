0e7156a8945db1ac8075fb257f55566fdea15fe0
linuxlight <lnxlht4j@gmail.com>
2022-01-11 01:55:12 +0900

    Add: create CommentController class

---
+++ src/main/java/com/devthink/devthink_server/controllers/CommentController.java
@@ -0,0 +1,24 @@
+package com.devthink.devthink_server.controllers;
+
+import com.devthink.devthink_server.domain.Comment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<Comment> getComments() {
+        return commentService.getComments();
+    }
+}
