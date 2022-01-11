43a5e7c5d9523c5e3acd3db481d93ac042e563d6
mikekang47 <gyeong0308@gmail.com>
2022-01-11 23:30:31 +0900

    Fix: make registerUser send 201

--- src/main/java/com/devthink/devthink_server/controllers/UserController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserController.java
@@ -1,6 +1,11 @@
+// 1. 가입 -> POST /users
+// 2. 사용자 정보 갱신 -> PUT/PATCH /users/{id}
+// 3. 탈퇴 -> DELETE /users/{id}
 package com.devthink.devthink_server.controllers;
 
 
+import com.devthink.devthink_server.application.UserService;
+import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.dto.UserData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +14,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    UserData registerUser() {
-        return null;
+    UserData create(@RequestBody UserData userData) {
+        User user = userService.registerUser(userData);
+
+        return UserData.builder()
+                .email(user.getEmail())
+                .build();
     }
 
 
