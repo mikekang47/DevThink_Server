acc6c0039b59378dd2748b84827b1d70042a8c6f
minu kim <kmw106933@naver.com>
2022-01-11 17:49:33 +0900

    Feat: make getPostList method in PostService

--- src/main/java/com/devthink/devthink_server/service/PostService.java
+++ src/main/java/com/devthink/devthink_server/service/PostService.java
@@ -1,9 +1,13 @@
 package com.devthink.devthink_server.service;
 
+import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.PostDto;
 import com.devthink.devthink_server.infra.PostRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -16,5 +20,27 @@ public class PostService {
     @Transactional
     public Long savePost(PostDto postDto){
         return postRepository.save(postDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostDto> getPostList(){
+        List<Post> posts = postRepository.findAll();
+        List<PostDto> postDtoList = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostDto postDto = PostDto.builder()
+                    .id(post.getId())
+                    .user_id(post.getUser_id())
+                    .category_id(post.getCategory_id())
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .status(post.getStatus())
+                    .build();
+
+            postDtoList.add(postDto);
+        }
+        return postDtoList;
+
+
     }
 }
