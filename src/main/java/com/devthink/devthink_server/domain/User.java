738494d1a3d5b99641767cad098884c8a9fb45a0
mikekang47 <gyeong0308@gmail.com>
2022-01-12 00:11:05 +0900

    Fix: add more attribute in User and UserData fix size of password and nickname

--- src/main/java/com/devthink/devthink_server/domain/User.java
+++ src/main/java/com/devthink/devthink_server/domain/User.java
@@ -1,4 +1,37 @@
 package com.devthink.devthink_server.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private String role;
+
+    private String stack;
+
+    private String blogAddr;
+
+    private String gitNickname;
+
+    private Integer point;
 }
--- src/main/java/com/devthink/devthink_server/dto/UserData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserData.java
@@ -1,13 +1,39 @@
 package com.devthink.devthink_server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserData {
     private Long id;
 
+    @NotBlank
     private String email;
 
+    @NotBlank
+    @Size(min=8,max=15)
     private String password;
+
+    @NotBlank
+    @Size(max=8)
+    private String nickname;
+
+    @NotBlank
+    private String role;
+
+    private String stack;
+
+    private String blogAddr;
+
+    private String gitNickname;
+
+    private Integer point;
 }
