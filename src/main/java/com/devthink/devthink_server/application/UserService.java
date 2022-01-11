387b6f62e0119a5d7e3bd6ee12caa7431907b40b
mikekang47 <gyeong0308@gmail.com>
2022-01-11 20:24:21 +0900

    Feat: make UserService class

---
+++ src/main/java/com/devthink/devthink_server/application/UserService.java
@@ -0,0 +1,13 @@
+package com.devthink.devthink_server.application;
+
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.UserData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User register(UserData userData) {
+        return null;
+    }
+}
--- src/main/java/com/devthink/devthink_server/controllers/UserController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserController.java
@@ -1,20 +1,18 @@
 package com.devthink.devthink_server.controllers;
 
 
-import com.devthink.devthink_server.domain.User;
-import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.dto.UserData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private UserRepository userRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    User registerUser(@RequestBody User user) {
-        return userRepository.save(user);
+    UserData registerUser() {
+        return null;
     }
 
 
