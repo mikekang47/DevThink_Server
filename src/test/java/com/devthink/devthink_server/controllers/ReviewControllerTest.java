ef8f1667001ce1146c03043b7e07771a51fc6ea6
jaehee <vito822@naver.com>
2022-01-12 03:02:05 +0900

    Test: add create_review_success method in ReviewControllerTest

--- src/test/java/com/devthink/devthink_server/controllers/ReviewControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/ReviewControllerTest.java
@@ -1,7 +1,39 @@
 package com.devthink.devthink_server.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.devthink.devthink_server.application.ReviewService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(ReviewController.class)
 class ReviewControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private ReviewService reviewService;
+
+    @Test
+    void create_review_success() throws Exception {
+        //given
+        String requestBody = "{\"user_id\": \"13\", \n\"book_id\": \"10234\", " +
+                "\n\"content\": \"유용한 책입니다.\", \n\"score\": \"10.04\"}";
+
+        //then
+        this.mvc.perform(post("/reviews")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
 
 }
