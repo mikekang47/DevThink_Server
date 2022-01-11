9f591567d797e2b54610d08e02946dbee0432c45
jaehee <vito822@naver.com>
2022-01-12 03:00:55 +0900

    Feat: add createReview method in ReviewService

--- src/main/java/com/devthink/devthink_server/application/ReviewService.java
+++ src/main/java/com/devthink/devthink_server/application/ReviewService.java
@@ -1,7 +1,28 @@
 package com.devthink.devthink_server.application;
-
+import com.devthink.devthink_server.domain.Book;
+import com.devthink.devthink_server.domain.Review;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.ReviewRequestDto;
+import com.devthink.devthink_server.infra.JpaReviewRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final JpaReviewRepository jpaReviewRepository;
+
+    // 리뷰 등록
+    public String createReview(User user, Book book, ReviewRequestDto reviewRequestDto){
+        Review review = jpaReviewRepository.save(
+            Review.builder()
+                    .user(user)
+                    .book(book)
+                    .content(reviewRequestDto.getContent())
+                    .score(reviewRequestDto.getScore())
+                    .build()
+        );
+        return review.getId().toString();
+    }
 }
