654bdc4ff128c8aad642af0e57e1d2b149532ba5
mikekang47 <gyeong0308@gmail.com>
2022-01-11 20:06:31 +0900

    Feat: make UserData class For dto

---
+++ src/main/java/com/devthink/devthink_server/dto/UserData.java
@@ -0,0 +1,13 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class UserData {
+    private Long id;
+
+    private String email;
+
+    private String password;
+}
