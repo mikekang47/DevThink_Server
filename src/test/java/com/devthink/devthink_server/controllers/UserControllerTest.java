2593af055a0789d82592047d9958a21e15a10515
mikekang47 <gyeong0308@gmail.com>
2022-01-11 23:31:34 +0900

    Test: make registerUserTest work well

--- src/test/java/com/devthink/devthink_server/controllers/UserControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/UserControllerTest.java
@@ -1,12 +1,22 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.application.UserService;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.UserData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -14,13 +24,27 @@ public class UserControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     void registerUser() throws Exception {
+        given(userService.registerUser(any(UserData.class))).will(invocation -> {
+            UserData userData = invocation.getArgument(0);
+            
+            return User.builder()
+                    .email(userData.getEmail())
+                    .build();
+        });
+
         mvc.perform(
                 post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
+                        .content("{\"email\":\"test@email.com\"}")
         )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString("\"email\":\"test@email.com\"")));
+
+        verify(userService).registerUser(any(UserData.class));
     }
 }
