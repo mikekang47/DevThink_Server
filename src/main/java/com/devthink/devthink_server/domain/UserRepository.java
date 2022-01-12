d6a44b6b8c3d1f57cac9204c24b1edf6f54f4b62
mikekang47 <gyeong0308@gmail.com>
2022-01-12 21:50:43 +0900

    Feat: make UserRepository by using JPA

---
+++ src/main/java/com/devthink/devthink_server/domain/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devthink.devthink_server.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User user);
+}
