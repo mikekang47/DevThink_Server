0d38d1343dd59fa15aad05f1acf479d753859f8f
linuxlight <lnxlht4j@gmail.com>
2022-01-13 02:48:11 +0900

    Feat: add createComment method in CommentService

--- src/main/java/com/devthink/devthink_server/application/CommentService.java
+++ src/main/java/com/devthink/devthink_server/application/CommentService.java
@@ -2,6 +2,9 @@ package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.Comment;
 import com.devthink.devthink_server.domain.CommentRepository;
+import com.devthink.devthink_server.domain.Review;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.CommentRequestDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,7 +19,29 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
+    /**
+     * 모든 Comment를 조회합니다.
+     * @return
+     */
     public List<Comment> getComments() {
         return commentRepository.findAll();
+    }
+
+    /**
+     * 새로운 Comment를 등록합니다.
+     * @param user Comment를 생성하려고 하는 User
+     * @param review Comment가 달리게 되는 Review
+     * @param commentRequestDto
+     * @return
+     */
+    public String createComment(User user, Review review, CommentRequestDto commentRequestDto) {
+        Comment comment = commentRepository.save(
+                Comment.builder()
+                        .user(user)
+                        .review(review)
+                        .content(commentRequestDto.getContent())
+                        .build()
+        );
+        return comment.getId().toString();
     }
 }
