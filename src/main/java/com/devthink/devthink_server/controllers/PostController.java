c18c7e6896ef9100568421318eb38a8f1c08678b
minu kim <kmw106933@naver.com>
2022-01-12 12:53:21 +0900

    Fix: modify return type from PostDto to Post

--- src/main/java/com/devthink/devthink_server/controllers/PostController.java
+++ src/main/java/com/devthink/devthink_server/controllers/PostController.java
@@ -1,12 +1,12 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.PostDto;
 import com.devthink.devthink_server.service.PostService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/post")
@@ -18,21 +18,20 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping("/")
-    public List<PostDto> list(){
-        return PostService.getPostlist();
+    @GetMapping
+    public List<Post> list(){
+        return postService.getPostList();
     }
 
     @GetMapping("/{id}")
-    public PostDto findById(@PathVariable Long id){
-        return PostService.getPost(id);
+    public Optional<Post> findById(@PathVariable Long id){
+        return postService.getPost(id);
 
     }
 
     @PostMapping("/write")
-    public PostDto write(@RequestBody PostDto postDto){
-        postService.savePost(postDto);
-        return postDto;
+    public Post write(@RequestBody PostDto postDto){
+        return postService.savePost(postDto);
     }
 
 }
