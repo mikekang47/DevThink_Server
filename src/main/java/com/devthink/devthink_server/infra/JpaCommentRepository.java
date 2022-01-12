6a2d86ae3d8b2c780f7e0ada8825d0a6247279b3
linuxlight <lnxlht4j@gmail.com>
2022-01-13 02:23:20 +0900

    Feat: add save method in JpaCommentRepository

--- src/main/java/com/devthink/devthink_server/infra/JpaCommentRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/JpaCommentRepository.java
@@ -2,11 +2,16 @@ package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Comment;
 import com.devthink.devthink_server.domain.CommentRepository;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@Primary
 public interface JpaCommentRepository
         extends CommentRepository, CrudRepository<Comment, Long> {
     List<Comment> findAll();
+    Comment save(Comment comment);
 }
