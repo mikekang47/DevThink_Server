f826baa7186a9945b74a6e6b3fb280daf4051a11
mikekang47 <gyeong0308@gmail.com>
2022-01-12 21:51:34 +0900

    Refactor: make List object work in domain

--- src/main/java/com/devthink/devthink_server/domain/User.java
+++ src/main/java/com/devthink/devthink_server/domain/User.java
@@ -1,19 +1,16 @@
 package com.devthink.devthink_server.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
     @Id
     @GeneratedValue
@@ -21,14 +18,14 @@ public class User {
 
     private String email;
 
-    private String password;
-
     private String nickname;
 
     private String role;
 
-    private String stack;
-
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private List<String> stack;
+    
     private String blogAddr;
 
     private String gitNickname;
