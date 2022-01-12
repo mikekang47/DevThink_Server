8ba1892e5fb3f82aafe59bb578e8574810a7ecee
mikekang47 <gyeong0308@gmail.com>
2022-01-12 21:46:32 +0900

    Refactor: make registerUser more clear Inject depedency between UserService and UserRepository

--- src/main/java/com/devthink/devthink_server/application/UserService.java
+++ src/main/java/com/devthink/devthink_server/application/UserService.java
@@ -1,13 +1,26 @@
 package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.User;
-import com.devthink.devthink_server.dto.UserData;
+import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.dto.UserRegistrationData;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import javax.transaction.Transactional;
 
-    public User register(UserData userData) {
-        return null;
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+    private final Mapper mapper;
+
+    public UserService(UserRepository userRepository, Mapper mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    public User registerUser(UserRegistrationData userRegistrationData) {
+        User user = mapper.map(userRegistrationData, User.class);
+        return userRepository.save(user);
     }
 }
