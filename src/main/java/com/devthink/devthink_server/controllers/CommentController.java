c08504cf2cc5128dc51f668217361a038588ecff
linuxlight <lnxlht4j@gmail.com>
2022-01-13 01:46:29 +0900

    Feat: add createComment method in CommentController

--- src/main/java/com/devthink/devthink_server/controllers/CommentController.java
+++ src/main/java/com/devthink/devthink_server/controllers/CommentController.java
@@ -2,9 +2,11 @@ package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.application.CommentService;
 import com.devthink.devthink_server.domain.Comment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devthink.devthink_server.domain.Review;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.CommentRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +20,24 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    /**
+     * 모든 Comment를 조회합니다.
+     * @return
+     */
     @GetMapping
     public List<Comment> getComments() {
         return commentService.getComments();
+    }
+
+    /**
+     * 새로운 Comment를 생성합니다.
+     * @return
+     */
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createComment(@RequestBody CommentRequestDto commentRequestDto){
+        User user = null; //TODO: user_id 로 User 가져오기
+        Review review = null; //TODO: review_id 로 Review 가져오기
+        return commentService.createComment(user, review, commentRequestDto);
     }
 }
