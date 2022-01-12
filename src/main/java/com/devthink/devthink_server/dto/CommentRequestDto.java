3504bc888456c5e1e1692cf3927a1b03133a638a
linuxlight <lnxlht4j@gmail.com>
2022-01-13 01:38:40 +0900

    Add: create CommentRequestDto class

---
+++ src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
@@ -0,0 +1,15 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommentRequestDto {
+    private final Long user_id;
+    private final Long review_id;
+    private final String content;
+    private final String status;
+}
