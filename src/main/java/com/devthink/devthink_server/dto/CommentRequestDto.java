54d4ae592b7fa37cba9f82486782ff0b3ac1d7fd
linuxlight <lnxlht4j@gmail.com>
2022-01-13 02:58:40 +0900

    Feat: add Spring validation into CommentRequestDto

--- src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
+++ src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
@@ -4,11 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentRequestDto {
+
+    @NotBlank
     private final Long user_id;
+
+    @NotBlank
     private final Long review_id;
+
+    @NotBlank
+    @Size(min=1)
     private final String content;
+
 }
