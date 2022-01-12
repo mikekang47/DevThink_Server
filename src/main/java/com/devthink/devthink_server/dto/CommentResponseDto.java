98efe26b9f66017f2340c2447db692704a286bd1
linuxlight <lnxlht4j@gmail.com>
2022-01-13 03:05:30 +0900

    Add: create CommentResponseDto class

---
+++ src/main/java/com/devthink/devthink_server/dto/CommentResponseDto.java
@@ -0,0 +1,23 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommentResponseDto {
+
+    @NotBlank
+    private final String user_nickname;
+
+    @NotBlank
+    private final String user_role;
+
+    @NotBlank
+    private final String content;
+
+}
