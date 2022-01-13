99f89dac4b7bb76d5e93e67d3648f63825119667
jaehee <vito822@naver.com>
2022-01-13 21:49:06 +0900

    Feat: add builder in Book class
    isbn 으로 객체 생성 추가
    private 접근 제어자 추가

--- src/main/java/com/devthink/devthink_server/domain/Book.java
+++ src/main/java/com/devthink/devthink_server/domain/Book.java
@@ -1,13 +1,12 @@
 package com.devthink.devthink_server.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +16,24 @@ import java.util.List;
 @NoArgsConstructor
 public class Book {
     @Id
-    @GeneratedValue
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    int isbn;
+    private Integer isbn;
 
-    float score;
+    private Float scoreTotal;
 
-    String status;
+    private Integer reviewCnt;
+
+    private Float scoreAvg;
 
     @OneToMany
-    List<Review> reviews = new ArrayList<>();;
+    private List<Review> reviews = new ArrayList<>();
+
+    @Builder
+    public Book(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+
 }
