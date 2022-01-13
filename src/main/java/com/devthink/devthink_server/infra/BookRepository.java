1df412b9bf0cbcf6adfacc4683a1e1254194c9fb
jaehee <vito822@naver.com>
2022-01-13 21:52:47 +0900

    Feat: add getBookByIsbn method in Book class

--- src/main/java/com/devthink/devthink_server/infra/BookRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/BookRepository.java
@@ -4,7 +4,10 @@ import com.devthink.devthink_server.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface BookRepository  extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
+    Optional<Book> getBookByIsbn(int isbn);
 }
