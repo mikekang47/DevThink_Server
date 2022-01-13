49cc3f229697718479bc3b7b5895f1c90b0a5e95
jaehee <vito822@naver.com>
2022-01-13 22:54:11 +0900

    Test: add review_get_success method in ReviewControllerTest class

--- src/test/java/com/devthink/devthink_server/controllers/ReviewControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/ReviewControllerTest.java
@@ -1,6 +1,8 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.application.BookService;
 import com.devthink.devthink_server.application.ReviewService;
+import com.devthink.devthink_server.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,17 +25,24 @@ class ReviewControllerTest {
     @MockBean
     private ReviewService reviewService;
 
-    @Test
-    void create_review_success() throws Exception {
-        //given
-        String requestBody = "{\"user_id\": \"13\", \n\"book_id\": \"10234\", " +
-                "\n\"content\": \"유용한 책입니다.\", \n\"score\": \"10.04\"}";
+    @MockBean
+    private BookService bookService;
 
-        //then
+    @Test
+    public void review_create_success() throws Exception {
         this.mvc.perform(post("/reviews")
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"userId\": 1, \n\"bookIsbn\": 1, " +
+                                "\n\"content\": \"유용한 책입니다.\", \n\"score\": 4.04}"))
                 .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void review_get_success() throws Exception {
+        this.mvc.perform(get("/reviews/5")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());
     }
 
