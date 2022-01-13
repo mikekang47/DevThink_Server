056da839aabda3d8475e38c6deeaf31993535a94
minu kim <kmw106933@naver.com>
2022-01-13 12:26:03 +0900

    Feat: Add dozermapper in PostService

--- src/main/java/com/devthink/devthink_server/service/PostService.java
+++ src/main/java/com/devthink/devthink_server/service/PostService.java
@@ -3,6 +3,7 @@ package com.devthink.devthink_server.service;
 import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.PostDto;
 import com.devthink.devthink_server.infra.PostRepository;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,52 +16,27 @@ import java.util.Optional;
 @Service
 @Transactional
 public class PostService {
+    private final PostRepository postRepository;
+    private final Mapper mapper;
 
-    private PostRepository postRepository;
-    private static final int BLOCK_PAGE_NUM_COUNT = 5; // 블럭에 존재하는 페이지 수
-    private static final int PAGE_POST_COUNT = 4; // 한 페이지에 존재하는 게시글 수
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, Mapper mapper) {
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     public Post savePost(PostDto postDto){
-        return postRepository.save(postDto.toEntity());
-
+        Post post = mapper.map(postDto, Post.class);
+        return postRepository.save(post);
     }
+
     public List<Post> getPostList(){
         return postRepository.findAll();
-
     }
 
-    public List<Post> getPostList(Integer pageNum){
-        Page<Post> page = postRepository.findAll(PageRequest
-                .of(pageNum - 1, PAGE_POST_COUNT, Sort.by(Sort.Direction.ASC, "create_at")));
-
-        List<Post> posts = page.getContent();
-
-        return posts;
-
-    }
-
-    public Integer[] getPageList(Integer curPageNum){
-        Integer[] pageList = new Integer[BLOCK_PAGE_NUM_COUNT];
-
-        Double postsTotal = Double.valueOf(postRepository.count());
-
-        Integer totalLastNum = (int)(Math.ceil((postsTotal/PAGE_POST_COUNT)));
-
-        Integer blockLastPage = (totalLastNum > curPageNum + BLOCK_PAGE_NUM_COUNT)
-                ? curPageNum + BLOCK_PAGE_NUM_COUNT
-                : totalLastNum;
-
-        curPageNum = (curPageNum<=3) ? 1 : curPageNum-2;
-
-        for(int val=curPageNum, i=0; val<=blockLastPage; val++, i++){
-            pageList[i] = val;
-        }
-        return pageList;
-
+    public List<Post> list(int page){
+        Page<Post> pageList = postRepository.findAll(PageRequest.of(page - 1, 6, Sort.by(Sort.Direction.DESC, "id")));
+        return pageList.getContent();
     }
 
     public Optional<Post> getPost(Long id){
@@ -73,14 +49,15 @@ public class PostService {
 
         post.update(postDto.getTitle(), postDto.getContent());
         return postRepository.save(post);
-
     }
 
     public void deletePost(Long id){
         postRepository.deleteById(id);
     }
 
-    public List<Post> searchPosts(String keyword){
-        return postRepository.findByTitleContaining(keyword);
+    public List<Post> search(String keyword)
+    {
+        List<Post> postList = postRepository.findByTitleContaining(keyword);
+        return postList;
     }
 }
