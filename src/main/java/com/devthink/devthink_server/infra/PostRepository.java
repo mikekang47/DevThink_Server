301a9764f104361ac5f9d512d63f26f5423aa659
minu kim <kmw106933@naver.com>
2022-01-13 12:24:51 +0900

    Feat: add findByTitleContaining method in PostRepository

--- src/main/java/com/devthink/devthink_server/infra/PostRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/PostRepository.java
@@ -2,9 +2,9 @@ package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    List<Post> findByTitleContaining(String keyword);
 }
