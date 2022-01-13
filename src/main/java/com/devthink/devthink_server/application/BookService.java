4d29e5eba5047f54e7b2c18cad42d397005efe55
jaehee <vito822@naver.com>
2022-01-13 21:58:08 +0900

    Feat: add getBookByIsbn method in BookService

--- src/main/java/com/devthink/devthink_server/application/BookService.java
+++ src/main/java/com/devthink/devthink_server/application/BookService.java
@@ -5,6 +5,8 @@ import com.devthink.devthink_server.infra.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
@@ -17,5 +19,14 @@ public class BookService {
         return bookRepository.save(book);
     }
 
+    //isbn으로 책 조회, 없으면 생성
+    public Book getBookByIsbn(int isbn){
+        Optional<Book> book = bookRepository.getBookByIsbn(isbn);
+        if (book.isEmpty()) {
+            return createBook(isbn);
+        } else {
+            return book.get();
+        }
+    }
 
 }
