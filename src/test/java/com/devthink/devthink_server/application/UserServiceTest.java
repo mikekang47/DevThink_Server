966f6d9a503c3e6b6c9566b51da6742f3a066488
mikekang47 <gyeong0308@gmail.com>
2022-01-14 03:00:05 +0900

    Test: make test method for update user data

--- src/test/java/com/devthink/devthink_server/application/UserServiceTest.java
+++ src/test/java/com/devthink/devthink_server/application/UserServiceTest.java
@@ -2,13 +2,19 @@ package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.dto.UserModificationData;
 import com.devthink.devthink_server.dto.UserRegistrationData;
+import com.devthink.devthink_server.errors.UserEmailDuplicationException;
+import com.devthink.devthink_server.errors.UserNotFoundException;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -17,6 +23,7 @@ import static org.mockito.Mockito.verify;
 
 class UserServiceTest {
     private final String EXISTED_EMAIL = "exited@email.com";
+    private final Long NOT_EXISTED_ID = 1000L;
 
     private UserService userService;
 
@@ -29,8 +36,6 @@ class UserServiceTest {
 
         userService = new UserService(userRepository, mapper);
 
-
-
         given(userRepository.save(any(User.class))).will(invocation -> {
             User user = User.builder()
                     .id(13L)
@@ -42,8 +47,18 @@ class UserServiceTest {
             return user;
         });
 
+        given(userRepository.findById(1L)).willReturn(
+                Optional.of(User.builder()
+                        .id(1L)
+                        .email(EXISTED_EMAIL)
+                        .build())
+        );
         
+        given(userRepository.existsByEmail(EXISTED_EMAIL))
+                .willThrow(new UserEmailDuplicationException(EXISTED_EMAIL));
 
+        given(userRepository.findById(NOT_EXISTED_ID))
+                .willThrow(new UserNotFoundException(NOT_EXISTED_ID));
     }
 
     @Test
@@ -61,4 +76,33 @@ class UserServiceTest {
         verify(userRepository).save(any(User.class));
     }
 
+    @Test
+    void 올바른_정보로_사용자정보를_수정하려는_경우() {
+        UserModificationData modificationData = UserModificationData.builder()
+                .nickname("test")
+                .role("senior")
+                .build();
+        Long userId = 1L;
+        User user = userService.updateUser(userId, modificationData);
+
+        assertThat(user.getId()).isEqualTo(1L);
+        assertThat(user.getNickname()).isEqualTo("test");
+        assertThat(user.getEmail()).isEqualTo(EXISTED_EMAIL);
+        assertThat(user.getRole()).isEqualTo("senior");
+
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    void 올바르지_않은_정보로_사용자정보를을_수정하려는는_경우() {
+        UserModificationData modificationData = UserModificationData.builder()
+                .nickname("test")
+                .role("senior")
+                .build();
+
+        assertThatThrownBy(() ->userService.updateUser(NOT_EXISTED_ID, modificationData))
+                .isInstanceOf(UserNotFoundException.class);
+        
+        verify(userRepository).findById(NOT_EXISTED_ID);
+    }
 }
--- src/test/java/com/devthink/devthink_server/controllers/UserControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/UserControllerTest.java
@@ -2,7 +2,9 @@ package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.application.UserService;
 import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.UserModificationData;
 import com.devthink.devthink_server.dto.UserRegistrationData;
+import com.devthink.devthink_server.errors.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +32,7 @@ public class UserControllerTest {
 
     @MockBean
     private UserService userService;
+    private Long NOT_EXISTED_ID = 100L;
 
     @BeforeEach
     void setUp() {
@@ -34,23 +40,37 @@ public class UserControllerTest {
             UserRegistrationData userRegistrationData = invocation.getArgument(0);
 
             return User.builder()
+                    .id(13L)
                     .email(userRegistrationData.getEmail())
                     .nickname(userRegistrationData.getNickname())
                     .role(userRegistrationData.getRole())
                     .build();
         });
 
+        given(userService.updateUser(eq(1L), any(UserModificationData.class)))
+                .will(invocation -> {
+                    Long id = invocation.getArgument(0);
+                    UserModificationData modificationData = invocation.getArgument(1);
+                    return User.builder()
+                            .id(id)
+                            .nickname(modificationData.getNickname())
+                            .email("tester@email.com")
+                            .role(modificationData.getRole())
+                            .build();
+                });
 
+        given(userService.updateUser(eq(NOT_EXISTED_ID), any(UserModificationData.class)))
+                .willThrow(new UserNotFoundException(NOT_EXISTED_ID));
     }
 
     @Test
     void 올바른_회원정보로_회원가입을_하려는_경우() throws Exception {
         mvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"tester@email.com\"," +
-                                "\"nickname\":\"Tester\",\"role\":\"senior\"}")
-        )
+                        post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"email\":\"tester@email.com\"," +
+                                        "\"nickname\":\"Tester\",\"role\":\"senior\"}")
+                )
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString("\"email\":\"tester@email.com\"")))
                 .andExpect(content().string(containsString("Test")))
@@ -62,13 +82,48 @@ public class UserControllerTest {
     @Test
     void 올바르지_않은_회원정보로_회원가입을_하려는_경우() throws Exception {
         mvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
-        )
+                        post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{}")
+                )
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    void 올바른_회원정보로_회원정보를_수정하는_경우() throws Exception {
+        mvc.perform(
+                        patch("/users/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"nickname\":\"newTest\",\"role\":\"senior\"}")
+                )
+                .andExpect(content().string(containsString("newTest")))
+                .andExpect(content().string(containsString("senior")))
+                .andExpect(status().isOk());
+
+        verify(userService).updateUser(eq(1L), any(UserModificationData.class));
+    }
 
 
+    @Test
+    void 올바르지_않은_회원정보로_회원정보를_수정하는_경우() throws Exception {
+        mvc.perform(
+                        patch("/users/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"nickname\":\"\",\"role\":\"\"}")
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void 존재하지_않은_회원의_정보를_수정하는_경우() throws Exception {
+        mvc.perform(
+                        patch("/users/100")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"nickname\":\"TEST\",\"role\":\"TEST\"}")
+                )
+                .andExpect(status().isNotFound());
+
+        verify(userService).updateUser(eq(NOT_EXISTED_ID), any(UserModificationData.class));
+    }
 }
+
