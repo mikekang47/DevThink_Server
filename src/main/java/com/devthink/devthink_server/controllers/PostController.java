06756e07f4d0746407e092f6202a542ed42d88a1
minu kim <kmw106933@naver.com>
2022-01-13 12:21:31 +0900

    Fix: improve paging controller in PostControlelr

--- src/main/java/com/devthink/devthink_server/controllers/PostController.java
+++ src/main/java/com/devthink/devthink_server/controllers/PostController.java
@@ -18,43 +18,67 @@ public class PostController {
         this.postService = postService;
     }
 
+    /**
+     * 페이징 처리하여 글 가져오기
+     * @param page
+     * @return
+     */
     @GetMapping
-    public List<Post> list(@RequestParam(value="page", defaultValue = "1") Integer pageNum){
-        List<Post> postList = postService.getPostList(pageNum);
-        Integer[] pageList = postService.getPageList(pageNum);
-
-        for (Integer integer : pageList) {
-            System.out.println("integer = " + integer);
-        }
-        return postList;
-
+    public List<Post> list(@RequestParam(value="page", defaultValue = "1") int page){
+        return postService.list(page); // 불러올 페이지는 1부터 시작
     }
 
+    /**
+     * 글 id로 검색
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public Optional<Post> findById(@PathVariable Long id){
         return postService.getPost(id);
 
     }
 
+    /**
+     * 게시글 쓰기(저장)
+     * @param postDto
+     * @return
+     */
     @PostMapping("/write")
     public Post write(@RequestBody PostDto postDto){
         return postService.savePost(postDto);
     }
+
+    /**
+     * 글 업데이트
+     * @param id
+     * @param postDto
+     * @return
+     */
 
     @PutMapping("/{id}")
     public Post update(@PathVariable Long id, @RequestBody PostDto postDto){
         return postService.update(id, postDto);
     }
 
+    /**
+     * 글 삭제
+     * @param id
+     */
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable Long id)
     {
         postService.deletePost(id);
     }
 
+    /**
+     * 키워드로 검색
+     */
+
     @GetMapping("/search")
-    public List<Post> search(@RequestParam(value = "keyword") String keyword) {
-        return postService.searchPosts(keyword);
+    public List<Post> search(@RequestParam String keyword)
+    {
+        return postService.search(keyword);
     }
 
 }
