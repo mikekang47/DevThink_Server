5e6ebf44f04133c2d7632649d9d2a14f4d885e20
jaehee <vito822@naver.com>
2022-01-13 21:54:32 +0900

    Feat: add createBook method in BookService class

--- src/main/java/com/devthink/devthink_server/application/BookService.java
+++ src/main/java/com/devthink/devthink_server/application/BookService.java
@@ -1,5 +1,6 @@
 package com.devthink.devthink_server.application;
 
+import com.devthink.devthink_server.domain.Book;
 import com.devthink.devthink_server.infra.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,12 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     private final BookRepository bookRepository;
+
+    //책 등록
+    public Book createBook(int isbn){
+        Book book = Book.builder().isbn(isbn).build();
+        return bookRepository.save(book);
+    }
+
 
 }
