684c7279806f38e68e727a785cef3c07581318ce
mikekang47 <gyeong0308@gmail.com>
2022-01-14 02:54:33 +0900

    Refactor: add message in NotBlank annotation

--- src/main/java/com/devthink/devthink_server/dto/UserRegistrationData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserRegistrationData.java
@@ -4,6 +4,7 @@ import com.devthink.devthink_server.domain.BaseTimeEntity;
 import com.github.dozermapper.core.Mapping;
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -15,34 +16,30 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationData extends BaseTimeEntity {
-    @NotBlank
+    @NotBlank(message = "이메일 주소를 입력하세요.")
+    @Email
     @Mapping("email")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "닉네임을 입력해주세요.")
     @Size(max=8)
     @Mapping("nickname")
     private String nickname;
 
-    @NotBlank
+    @NotBlank(message = "직무를 선택해주세요.")
     @Mapping("role")
     private String role;
 
-    @Builder.Default
     @Mapping("stack")
-    private List<String> stack = new ArrayList<>();
+    private List<String> stack;
 
-    @Builder.Default
     @Mapping("blogAddr")
-    private String blogAddr = "";
+    private String blogAddr;
 
-    @Builder.Default
     @Mapping("gitNickname")
-    private String gitNickname = "";
+    private String gitNickname;
 
-    @Builder.Default
-    private Integer point = 0;
+    private Integer point;
 
-    @Builder.Default
-    private String status = "active";
+    private String status;
 }
