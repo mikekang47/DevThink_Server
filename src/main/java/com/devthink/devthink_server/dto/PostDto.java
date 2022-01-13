c0842058b1961ef6b1e033b5a883709f3248b8cc
minu kim <kmw106933@naver.com>
2022-01-13 12:24:15 +0900

    pref: make builder in PostDto concise

--- src/main/java/com/devthink/devthink_server/dto/PostDto.java
+++ src/main/java/com/devthink/devthink_server/dto/PostDto.java
@@ -1,49 +1,28 @@
 package com.devthink.devthink_server.dto;
 
-import com.devthink.devthink_server.domain.Post;
+import com.github.dozermapper.core.Mapping;
 import lombok.*;
-import java.time.LocalDateTime;
 
+@Builder
 @Getter
-@Setter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
 
-    private Long id;
-
+    @Mapping("user_id")
     private Long user_id;
 
+    @Mapping("category_id")
+    private Long category_id;
+
+    @Mapping("title")
     private String title;
 
+    @Mapping("content")
     private String content;
 
+    @Mapping("status")
     private String status;
-
-    private LocalDateTime create_at;
-
-    private LocalDateTime update_at;
-
-    public Post toEntity(){
-        Post build = Post.builder()
-                .id(id)
-                .user_id(user_id)
-                .title(title)
-                .content(content)
-                .status(status)
-                .build();
-        return build;
-    }
-
-    @Builder
-    public PostDto(Long id, Long user_id, String title, String content, String status, LocalDateTime create_at, LocalDateTime update_at) {
-        this.id = id;
-        this.user_id = user_id;
-        this.title = title;
-        this.content = content;
-        this.status = status;
-        this.create_at = create_at;
-        this.update_at = update_at;
-    }
 
 
 }
