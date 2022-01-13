a3d2957fa6d41912ca99fd48d449b90d666e44c5
염연웅 <ritch1530@gmail.com>
2022-01-13 21:35:37 +0900

    Feat: add list posts

--- src/main/java/com/devthink/devthink_server/application/PostsService.java
+++ src/main/java/com/devthink/devthink_server/application/PostsService.java
@@ -1,12 +1,16 @@
 package com.devthink.devthink_server.application;
 
 
+import com.devthink.devthink_server.domain.posts.Posts;
 import com.devthink.devthink_server.domain.posts.PostsRepository;
+import com.devthink.devthink_server.dto.posts.PostsResponseDto;
 import com.devthink.devthink_server.dto.posts.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +20,11 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<Posts> list(){
+        List<Posts> postList = postsRepository.findAll();
+        return postList;
     }
 }
--- src/main/java/com/devthink/devthink_server/controller/PostsApiController.java
+++ src/main/java/com/devthink/devthink_server/controller/PostsApiController.java
@@ -1,30 +1,27 @@
 package com.devthink.devthink_server.controller;
 
 import com.devthink.devthink_server.application.PostsService;
+import com.devthink.devthink_server.domain.posts.Posts;
+import com.devthink.devthink_server.dto.posts.PostsResponseDto;
 import com.devthink.devthink_server.dto.posts.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor    //Bean 주입
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
 
-    @GetMapping(value="/")
-    public String root() {
-        return "index";
-    }
-    @GetMapping(value="/index")
-    public String index() {
-        return "index";
-    }
-    @GetMapping(value="/board")
-    public String board() {
-        return "board";
-    }
-
     @PostMapping("/saveForm/post")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @GetMapping("/post")
+    public List<Posts> list(){
+        List<Posts> postList = postsService.list();
+        return postList;
     }
 }
