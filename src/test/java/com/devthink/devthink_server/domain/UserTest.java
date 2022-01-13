2b632215c6bc8fd84142caa10f0b080d90a2c43b
mikekang47 <gyeong0308@gmail.com>
2022-01-14 02:59:44 +0900

    Test: make UserTest class

---
+++ src/test/java/com/devthink/devthink_server/domain/UserTest.java
@@ -0,0 +1,26 @@
+package com.devthink.devthink_server.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    void changeWith() {
+        User user = User.builder().build();
+
+        user.changeWith(
+                User.builder()
+                        .nickname("Test")
+                        .role("senior")
+                        .build()
+        );
+
+        assertThat(user.getNickname()).isEqualTo("Test");
+        assertThat(user.getRole()).isEqualTo("senior");
+    }
+
+
+
+}
