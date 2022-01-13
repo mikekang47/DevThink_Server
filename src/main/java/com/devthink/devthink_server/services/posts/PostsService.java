eb8acb2523207dbe94a41419db00306de00b016c
염연웅 <ritch1530@gmail.com>
2022-01-13 20:58:41 +0900

    Add: PostsService.java
    게시글 dto를 받아 entity화하고 save. 저장이 되었다면 게시글 id 반환(추후 response code 추가)

---
+++ src/main/java/com/devthink/devthink_server/services/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.devthink.devthink_server.services.posts;
+
+
+import com.devthink.devthink_server.domain.posts.PostsRepository;
+import com.devthink.devthink_server.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
