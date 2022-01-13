79f8db9f99de710cdc8374db6adf8785405e1864
mikekang47 <gyeong0308@gmail.com>
2022-01-13 14:59:40 +0900

    Refactor: add exitsByEmail, exitsByNickname in repository by JPA

--- src/main/java/com/devthink/devthink_server/domain/UserRepository.java
+++ src/main/java/com/devthink/devthink_server/domain/UserRepository.java
@@ -3,7 +3,13 @@ package com.devthink.devthink_server.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
