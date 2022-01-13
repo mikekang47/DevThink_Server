32acc339143a2252ae2af7d17b2f4ce89e1ae0f0
염연웅 <ritch1530@gmail.com>
2022-01-13 20:55:48 +0900

    Add: PostsRepository.java

---
+++ src/main/java/com/devthink/devthink_server/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.devthink.devthink_server.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
