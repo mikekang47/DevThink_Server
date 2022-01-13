84bbfc23954a73d77c4e87ca0ef97be63da8ea13
jaehee <vito822@naver.com>
2022-01-13 22:28:15 +0900

    Feat: add getReview method in ReviewService class

--- src/main/java/com/devthink/devthink_server/application/ReviewService.java
+++ src/main/java/com/devthink/devthink_server/application/ReviewService.java
@@ -3,7 +3,8 @@ import com.devthink.devthink_server.domain.Book;
 import com.devthink.devthink_server.domain.Review;
 import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.dto.ReviewRequestDto;
-import com.devthink.devthink_server.infra.JpaReviewRepository;
+import com.devthink.devthink_server.dto.ReviewResponseDto;
+import com.devthink.devthink_server.infra.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final JpaReviewRepository jpaReviewRepository;
+    private final ReviewRepository reviewRepository;
 
     // 리뷰 등록
     public String createReview(User user, Book book, ReviewRequestDto reviewRequestDto){
-        Review review = jpaReviewRepository.save(
+        Review review = reviewRepository.save(
             Review.builder()
                     .user(user)
                     .book(book)
@@ -24,5 +25,17 @@ public class ReviewService {
                     .build()
         );
         return review.getId().toString();
+    }
+
+    //리뷰 조회
+    public ReviewResponseDto getReview(Long reviewId) {
+        Review review = reviewRepository.getById(reviewId);
+        return ReviewResponseDto.builder()
+                .id(reviewId)
+                .bookIsbn(review.getBook().getIsbn())
+                .content(review.getContent())
+                .score(review.getScore())
+                .userId(review.getUser().getId())
+                .build();
     }
 }
