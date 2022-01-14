efa9e82172bc29334d2e9f3d6481a259e37cbf4d
jaehee <vito822@naver.com>
2022-01-15 03:40:05 +0900

    Refactor: add @Transactional in BookService

--- src/main/java/com/devthink/devthink_server/application/BookService.java
+++ src/main/java/com/devthink/devthink_server/application/BookService.java
@@ -4,16 +4,19 @@ import com.devthink.devthink_server.domain.Book;
 import com.devthink.devthink_server.infra.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
 
     //책 등록
+    @Transactional
     public Book createBook(int isbn){
         Book book = Book.builder().isbn(isbn).build();
         return bookRepository.save(book);
