b56f230a678402719406492ae3c72284c608db02
minu kim <kmw106933@naver.com>
2022-01-14 13:04:09 +0900

    Test: make test method for create post data

--- src/test/java/com/devthink/devthink_server/controllers/PostControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/PostControllerTest.java
@@ -1,63 +1,64 @@
 package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.domain.Post;
+import com.devthink.devthink_server.dto.PostDto;
+import com.devthink.devthink_server.infra.PostRepository;
 import com.devthink.devthink_server.service.PostService;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.any;
-import static org.hamcrest.Matchers.containsString;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-@WebMvcTest(PostController.class)
 class PostControllerTest {
-    @Autowired
-    MockMvc mvc;
 
-    @MockBean
-    public PostService postService;
+    private PostService postService;
+    private PostRepository postRepository = mock(PostRepository.class);
 
+    @BeforeEach
+    void setup(){
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
-    @Test
-    void list() throws Exception {
-        Post post = Post.builder()
-                        .user_id(1L)
-                        .title("abc")
-                        .content("abcd")
-                        .status("active")
-                        .build();
-        given(postService.getPostList()).willReturn(List.of(post));
+        postService = new PostService(postRepository, mapper);
 
-        mvc.perform(get("/posts"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("abc")))
-                .andExpect(content().string(containsString("abcd")))
-                .andExpect(content().string(containsString("active")));
-    }
-
-    @Test
-    void detail() throws Exception {
-        mvc.perform(get("/posts/1")
-                .accept(MediaType.APPLICATION_JSON)
-                        .content("{\"user_id\":\"1L\"}")
-                )
-                .andExpect(status().isOk());
-
+        given(postRepository.save(any(Post.class))).will(invocation -> {
+            Post post = Post.builder()
+                    .user_id(1L)
+                    .category_id(1L)
+                    .title("test")
+                    .content("test")
+                    .status("active")
+                    .build();
+            return post;
+        });
 
 
     }
+
+    @Test
+    void 올바른_정보로_글을_쓰려는_경우() {
+        PostDto postDto = PostDto.builder()
+                .user_id(1L)
+                .category_id(1L)
+                .title("테스트")
+                .content("테스트")
+                .build();
+
+        Post post = postService.savePost(postDto);
+
+        assertThat(post.getUser_id()).isEqualTo(1L);
+        assertThat(post.getStatus()).isEqualTo("active");
+        verify(postRepository).save(any(Post.class));
+
+    }
+
 
 }
