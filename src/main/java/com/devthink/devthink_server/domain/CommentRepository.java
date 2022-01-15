f590de56ec61604e9cfbe6865436eee74554f389
Seokjoo Cho <lnxlht4j@gmail.com>
2022-01-15 15:41:00 +0900

    Feat: add findById method in CommentRepository interface

--- src/main/java/com/devthink/devthink_server/domain/CommentRepository.java
+++ src/main/java/com/devthink/devthink_server/domain/CommentRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
     List<Comment> findAll();
+    Optional<Comment> findById(Long id);
     Comment save(Comment comment);
 }
