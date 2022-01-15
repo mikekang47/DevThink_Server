2772a07dd685aa65a0df989cd7f2267324aa46dd
minu kim <kmw106933@naver.com>
2022-01-15 11:21:01 +0900

    Feat: make CRUD test in PostControllerTest

--- src/test/java/com/devthink/devthink_server/controllers/PostControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/PostControllerTest.java
@@ -2,107 +2,139 @@ package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.PostDto;
-import com.devthink.devthink_server.errors.PostIdNotFoundException;
-import com.devthink.devthink_server.infra.PostRepository;
 import com.devthink.devthink_server.service.PostService;
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
-import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PostController.class)
 class PostControllerTest {
+
+    @Autowired
+    MockMvc mvc;
 
     private final Long NOT_EXISTED_ID = 2L;
 
+    @MockBean
     private PostService postService;
-    private PostRepository postRepository = mock(PostRepository.class);
 
     @BeforeEach
     void setup(){
-        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+        given(postService.savePost(any(PostDto.class))).will(invocation ->{
+            PostDto postDto = invocation.getArgument(0);
 
-        postService = new PostService(postRepository, mapper);
-
-        given(postRepository.save(any(Post.class))).will(invocation -> {
-            Post post = Post.builder()
-                    .id(1L)
+            return Post.builder()
                     .user_id(1L)
                     .category_id(1L)
-                    .title("test")
-                    .content("test")
-                    .status("active")
+                    .title(postDto.getTitle())
+                    .content(postDto.getContent())
+                    .status(postDto.getStatus())
                     .build();
-            return post;
         });
 
-        given(postRepository.findById(1L)).willReturn(
-                Optional.of(Post.builder()
-                        .user_id(1L)
-                        .build())
-        );
+        given(postService.update(eq(1L), any(PostDto.class)))
+                .will(invocation -> {
+                   Long id = invocation.getArgument(0);
+                   PostDto postDto = invocation.getArgument(1);
 
-        given(postRepository.findById(NOT_EXISTED_ID))
-                .willThrow(new PostIdNotFoundException(NOT_EXISTED_ID));
+                    return Post.builder()
+                            .title(postDto.getTitle())
+                            .content(postDto.getContent())
+                            .status(postDto.getStatus())
+                            .build();
+
+                });
+
+        given(postService.getPost(eq(1L)))
+                .will(invocation -> {
+                    Long id = invocation.getArgument(0);
+
+                    return Post.builder()
+                            .title("test")
+                            .content("test2")
+                            .status("active")
+                            .build();
+
+                });
+
 
     }
 
     @Test
-    void 올바른_정보로_글을_쓰려는_경우() {
-        PostDto postDto = PostDto.builder()
-                .user_id(1L)
-                .category_id(1L)
-                .title("테스트")
-                .content("테스트")
-                .build();
+    void 올바른_정보로_글을_불러오는_경우() throws Exception{
+        mvc.perform(
+                get("/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("test")))
+                .andExpect(content().string(containsString("test2")))
+                .andExpect(content().string(containsString("active")));
 
-        Post post = postService.savePost(postDto);
-
-        assertThat(post.getUser_id()).isEqualTo(1L);
-        assertThat(post.getStatus()).isEqualTo("active");
-        verify(postRepository).save(any(Post.class));
-
+        verify(postService).getPost(eq(1L));
     }
 
     @Test
-    void 올바른_정보로_글을_수정하려는_경우(){
-        PostDto postDto = PostDto.builder()
-                .title("test22")
-                .content("test22")
-                .build();
+    void 올바른_정보로_글을_쓰려는_경우() throws Exception{
+         mvc.perform(
+                 post("/posts/write")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"tester\"," +
+                                "\"content\":\"Tester\",\"status\":\"active\"}")
 
-        Long user_id =1L;
-        Post post = postService.update(user_id ,postDto);
+                )
+                 .andExpect(status().isOk())
+                 .andExpect(content().string(containsString("\"title\":\"tester\"")))
+                 .andExpect(content().string(containsString("Tester")))
+                 .andExpect(content().string(containsString("active")));
 
-       assertThat(post.getUser_id()).isEqualTo(1L);
-       assertThat(post.getTitle()).isEqualTo("test");
-       assertThat(post.getContent()).isEqualTo("test");
 
-       verify(postRepository).findById(1L);
+        verify(postService).savePost(any(PostDto.class));
+    }
+
+
+    @Test
+    void 올바른_정보로_글을_수정하려는_경우() throws Exception{
+        mvc.perform(
+                        put("/posts/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"title\":\"new\"," +
+                                        "\"content\":\"new1\",\"status\":\"new2\"}")
+
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"title\":\"new\"")))
+                .andExpect(content().string(containsString("new1")))
+                .andExpect(content().string(containsString("new2")));
+
+
+        verify(postService).update(eq(1L), any(PostDto.class));
     }
 
     @Test
-    void 올바르지_않은_정보로_글을_수정하려는_경우(){
-        PostDto postDto = PostDto.builder()
-                .title("test22")
-                .content("test22")
-                .build();
+    void 존재하는_게시글을_삭제하는_경우() throws Exception{
+        mvc.perform(
+                delete("/posts/1"))
+                        .andExpect(status().isOk());
 
-        assertThatThrownBy(() -> postService.update(NOT_EXISTED_ID, postDto))
-                .isInstanceOf(PostIdNotFoundException.class);
+            verify(postService).deletePost(1L);
 
-        verify(postRepository).findById(NOT_EXISTED_ID);
     }
+
 
 
 }
