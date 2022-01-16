bb5e68fdbf48fb5c1e907538f5c3d035caeacbac
minu kim <kmw106933@naver.com>
2022-01-16 19:36:47 +0900

    Feat: add CRUD Method in CategoryController.class

--- src/main/java/com/devthink/devthink_server/controllers/CategoryController.java
+++ src/main/java/com/devthink/devthink_server/controllers/CategoryController.java
@@ -1,12 +1,13 @@
 package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.domain.Category;
+import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.CategoryDto;
+import com.devthink.devthink_server.dto.PostDto;
 import com.devthink.devthink_server.service.CategoryService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/categories")
@@ -26,5 +27,50 @@ public class CategoryController {
     public Category save(@RequestBody CategoryDto categoryDto)
     {
         return categoryService.savePost(categoryDto);
+    }
+
+    /**
+     * 카테고리의 id를 검색하여 게시글을 가져옵니다.
+     * @param id 카테고리의 고유 id값
+     * @return id의 카테고리
+     */
+    @GetMapping("/{id}")
+    public CategoryDto findById(@PathVariable Long id){
+        Category data = categoryService.getCategory(id);
+        return getCategoryData(data);
+
+    }
+
+    /**
+     * 입력한 카테고리의 식별자 값과 valid한 카테고리의 정보를 받아, 기존의 게시글을 입력한 정보로 변경합니다.
+     * @param id 카테고리의 식별자
+     * @param categoryDto id, 이름
+     * @return 기존 게시글의 정보 수정
+     */
+    @PutMapping("/{id}")
+    public CategoryDto update(@PathVariable Long id, @RequestBody @Valid CategoryDto categoryDto){
+        Category update = categoryService.update(id, categoryDto);
+        return getCategoryData(update);
+    }
+
+    /**
+     * 입력한 카테고리의 식별자 값을 받아 게시글을 삭제합니다.
+     * @param id 카테고리의 식별자
+     */
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id)
+    {
+        categoryService.deletePost(id);
+    }
+
+    private CategoryDto getCategoryData(Category category)
+    {
+        if(category == null)
+            return null;
+
+        return CategoryDto.builder()
+                .user_id(category.getId())
+                .name(category_getName())
+                .build();
     }
 }
