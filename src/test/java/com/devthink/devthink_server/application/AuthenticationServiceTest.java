e71ab751e831894433b158733b1c0fceca4be119
mikekang47 <gyeong0308@gmail.com>
2022-01-17 02:20:24 +0900

    Test: add login test in authenticationService Test

--- src/test/java/com/devthink/devthink_server/application/AuthenticationServiceTest.java
+++ src/test/java/com/devthink/devthink_server/application/AuthenticationServiceTest.java
@@ -1,24 +1,26 @@
 package com.devthink.devthink_server.application;
 
+import com.devthink.devthink_server.utils.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AuthenticationServiceTest {
-
+    private static final String secret = "12345678901234567890123456789012";
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void setUp() {
-        authenticationService = new AuthenticationService();
+
+        JwtUtil jwtUtil = new JwtUtil(secret);
+        authenticationService = new AuthenticationService(jwtUtil);
     }
     
     @Test
     void login() {
         String accessToken = authenticationService.login();
-
+        System.out.println(accessToken);
         assertThat(accessToken).contains(".");
     }
 
