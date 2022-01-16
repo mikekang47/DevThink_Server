fba84617f0547ef91cb3c4a99a1e82e187ad889c
mikekang47 <gyeong0308@gmail.com>
2022-01-17 00:17:21 +0900

    Feat: make authenticationSerivce class

---
+++ src/main/java/com/devthink/devthink_server/application/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.devthink.devthink_server.application;
+
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.errors.LoginFailException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public String login() {
+        return "a.b.c";
+
+
+    }
+
+
+}
