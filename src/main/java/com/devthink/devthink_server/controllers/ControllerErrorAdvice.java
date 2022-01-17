a3566255b8f89069208df8fe11a124e4b89e449c
jaehee <vito822@naver.com>
2022-01-17 23:32:44 +0900

    Add: ControllerErrorAdvice class

---
+++ src/main/java/com/devthink/devthink_server/controllers/ControllerErrorAdvice.java
@@ -0,0 +1,10 @@
+package com.devthink.devthink_server.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@ControllerAdvice
+public class ControllerErrorAdvice {
+
+}
