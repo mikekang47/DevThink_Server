b317c912f3b75584a1cf9b28939dad0a356f5982
minu kim <kmw106933@naver.com>
2022-01-19 02:39:23 +0900

    Feat: make addMessage and getLetterData method in LetterController

--- src/main/java/com/devthink/devthink_server/controllers/LetterController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LetterController.java
@@ -1,10 +1,18 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.domain.Letter;
+import com.devthink.devthink_server.domain.Post;
+import com.devthink.devthink_server.dto.LetterDto;
+import com.devthink.devthink_server.dto.PostDto;
 import com.devthink.devthink_server.service.LetterService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 
 // 메시지 전송 코드
 @RestController
@@ -17,9 +25,61 @@ public class LetterController {
         this.letterService = letterService;
     }
 
+    /**
+     * 쪽지 보내기 기능
+     * @param letterdto
+     * @return
+     */
     @PostMapping
-    public LetterDto addMessage(@RequestBody LetterDto letterDto)
+    public ResponseEntity<String> addMessage(@RequestBody @Valid LetterDto letterdto){
+        ResponseEntity<String> entity = null;
+        try {
+            letterService.addMessage(letterdto);
+            entity = new ResponseEntity<String>("쪽지를 성공적으로 보냈습니다.", HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
+        }
+        return entity;
+    }
+
+    /**
+     * 보낸 쪽지 확인
+     */
+    @GetMapping
+    public List<LetterDto> checkMessage(@RequestParam Long user_id, @RequestParam Long room_id)
     {
+        List<Letter> messages = letterService.getMessage(user_id, room_id);
+        return getLetterDtos(messages);
+    }
+
+    private List<LetterDto> getLetterDtos(List<Letter> letters) {
+        List<LetterDto> letterDtos = new ArrayList<>();
+
+        for (Letter letter : letters) {
+            letterDtos.add(getLetterData(letter));
+
+        }
+        return letterDtos;
+    }
+
+
+    /**
+     * 게시글의 정보를 받아 게시글을 dto 데이터로 변환하여 반환합니다.
+     * @return 입력된 dto 데이터로 변환된 값
+     */
+    private LetterDto getLetterData(Letter letter)
+    {
+        if(letter == null)
+            return null;
+
+        return LetterDto.builder()
+                .userId(letter.getUserId())
+                .roomId(letter.getRoomId())
+                .content(letter.getContent())
+                .create_at(letter.getCreate_at())
+                .build();
 
     }
 }
