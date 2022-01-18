070f97f3d7009a5db474c11a6406028f6ae3bfe0
minu kim <kmw106933@naver.com>
2022-01-19 02:40:44 +0900

    Feat: make getMessage method in LetterService

--- src/main/java/com/devthink/devthink_server/service/LetterService.java
+++ src/main/java/com/devthink/devthink_server/service/LetterService.java
@@ -1,10 +1,15 @@
 package com.devthink.devthink_server.service;
 
+import com.devthink.devthink_server.domain.Letter;
+import com.devthink.devthink_server.domain.Post;
+import com.devthink.devthink_server.dto.LetterDto;
+import com.devthink.devthink_server.errors.PostNotFoundException;
 import com.devthink.devthink_server.infra.LetterRepository;
 import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Service
@@ -22,8 +27,21 @@ public class LetterService {
      * 메시지 작성
      * @param dto
      */
-    public void addMessage(MessageDto dto){
+    public void addMessage(LetterDto dto){
+        Letter letter = mapper.map(dto, Letter.class);
+        letterRepository.save(letter);
 
+    }
+
+    /**
+     * 해당 유저 id의 해당 방번호에 해당하는 받은 쪽지, 보낸 쪽지를 구현합니다.
+     * @param user_id
+     * @param room_id
+     * @return
+     */
+    public List<Letter> getMessage(Long user_id, Long room_id){
+        List<Letter> letters = letterRepository.findByUserIdAndRoomId(user_id, room_id);
+        return letters;
     }
 
 }
