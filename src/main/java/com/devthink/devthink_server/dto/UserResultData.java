288cee15c62e3716deb9fb279b24818749902e3a
mikekang47 <gyeong0308@gmail.com>
2022-01-19 02:26:00 +0900

    Refactor: add password attribute

--- src/main/java/com/devthink/devthink_server/dto/UserResultData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserResultData.java
@@ -1,10 +1,14 @@
 package com.devthink.devthink_server.dto;
 
 import com.devthink.devthink_server.domain.BaseTimeEntity;
+import com.github.dozermapper.core.Mapping;
 import lombok.*;
+import org.apache.tomcat.jni.Local;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -18,19 +22,31 @@ public class UserResultData extends BaseTimeEntity {
     private String email;
 
     @NotBlank
+    private String password;
+
+    @NotBlank
     @Size(max=8)
     private String nickname;
 
     @NotBlank
     private String role;
 
-    private List<String> stack;
+    @Builder.Default
+    private List<String> stack = new ArrayList<>();
 
+    @Mapping("bloagAddr")
     private String blogAddr;
 
+    @Mapping("gitNickname")
     private String gitNickname;
 
     private Integer point;
 
     private boolean deleted;
+
+    @Mapping("createAt")
+    private LocalDateTime createAt;
+
+    @Mapping("updateAt")
+    private LocalDateTime updateAt;
 }
