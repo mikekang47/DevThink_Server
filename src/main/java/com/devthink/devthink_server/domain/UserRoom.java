3011d3f8d222e1d1ef61871119573bb1405afc13
minu kim <kmw106933@naver.com>
2022-01-19 00:38:12 +0900

    Add: userRoom entity

---
+++ src/main/java/com/devthink/devthink_server/domain/UserRoom.java
@@ -0,0 +1,32 @@
+package com.devthink.devthink_server.domain;
+
+import com.devthink.devthink_server.domain.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "ROOM_ID")
+    private Room room;
+
+    @Builder.Default
+    private String status = "active";
+
+}
