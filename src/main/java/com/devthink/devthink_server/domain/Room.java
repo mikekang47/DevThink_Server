90e50ce06d8cdb7f72c1839e5a6433904f167281
minu kim <kmw106933@naver.com>
2022-01-18 16:13:54 +0900

    Add: make Room Entity

---
+++ src/main/java/com/devthink/devthink_server/domain/Room.java
@@ -0,0 +1,24 @@
+package com.devthink.devthink_server.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Builder.Default
+    private String status = "active"
+
+
+}
