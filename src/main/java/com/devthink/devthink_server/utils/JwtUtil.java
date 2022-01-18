9669afd4de11f541711e280601130de849f64245
mikekang47 <gyeong0308@gmail.com>
2022-01-19 02:22:28 +0900

    Refactor: specify decdoe method

--- src/main/java/com/devthink/devthink_server/utils/JwtUtil.java
+++ src/main/java/com/devthink/devthink_server/utils/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.devthink.devthink_server.utils;
 
+import com.devthink.devthink_server.errors.InvalidTokenException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +23,23 @@ public class JwtUtil {
                 .claim("userId",userId)
                 .signWith(key)
                 .compact();
+    }
+
+    public Claims decode(String token) {
+        if(token == null || token.isBlank()) {
+            throw new IndexOutOfBoundsException(token);
+        }
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException e) {
+            throw new InvalidTokenException(token);
+        }
+
+
+        
     }
 }
