83e9d3f3933a22141b261ba047a6d7e9dc95074a
minu kim <kmw106933@naver.com>
2022-01-20 00:38:29 +0900

    Feat: add pageable into findByTitleContaining

--- src/main/java/com/devthink/devthink_server/infra/PostRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/PostRepository.java
@@ -1,6 +1,7 @@
 package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByTitleContaining(String keyword);
+    List<Post> findByTitleContaining(String keyword, Pageable pageable);
 
     @Modifying
     @Query("update Post p set p.hit = p.hit + 1 where p.id = :id")
