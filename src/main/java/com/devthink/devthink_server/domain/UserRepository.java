13a58ea282f00427edae0e20c943d9a5c4269a48
mikekang47 <gyeong0308@gmail.com>
2022-01-20 00:34:22 +0900

    Refactor: change sequence

--- src/main/java/com/devthink/devthink_server/domain/UserRepository.java
+++ src/main/java/com/devthink/devthink_server/domain/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devthink.devthink_server.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,12 +9,14 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByNickname(String nickname);
-
+    
     Optional<User> findById(Long id);
 
     Optional<User> findByIdAndDeletedIsFalse(Long id);
+
+    Optional<User> findByEmail(String email);
+    
+    boolean existsByEmail(String existed_email);
+
+    boolean existsByNickname(String userNickName);
 }
