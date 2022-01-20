155e87056c8dc8273b35319270e15e7b91512305
minu kim <kmw106933@naver.com>
2022-01-20 22:32:48 +0900

    Feat: add findView method in LetterRepository

--- src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
@@ -2,9 +2,17 @@ package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Letter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+
 public interface LetterRepository extends JpaRepository<Letter, Long> {
-    List<Letter> findByUserIdAndRoomId(Long userId, Long roomId);
+
+   @Modifying
+   @Query("select u from Letter u " +
+           "where (u.recvId = :userId or u.sendId = :userId) and u.roomId = :roomId " +
+           "order by u.id DESC")
+   List<Letter> findView(Long userId, Long roomId);
 }
