640352735c57f792e9fb1a6d8fb269da9ce2bcdb
mikekang47 <gyeong0308@gmail.com>
2022-01-21 01:55:39 +0900

    Refactor: clear login and parseToken method in AuthenticationService

--- src/main/java/com/devthink/devthink_server/application/AuthenticationService.java
+++ src/main/java/com/devthink/devthink_server/application/AuthenticationService.java
@@ -1,20 +1,43 @@
 package com.devthink.devthink_server.application;
 
 
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.errors.InvalidTokenException;
+import com.devthink.devthink_server.errors.LoginFailException;
 import com.devthink.devthink_server.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class AuthenticationService {
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public AuthenticationService(JwtUtil jwtUtil) {
+    public AuthenticationService(UserRepository userRepository, JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
-    public String login() {
+    @Transactional
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new LoginFailException(email));
+
+        if(!user.authenticate(password)) {
+            throw new LoginFailException(email);
+        }
         return jwtUtil.encode(1L);
     }
 
+    public Long parseToken(String token) {
+        if(token == null) {
+            throw new InvalidTokenException(token);
+        }
+        Claims claims = jwtUtil.decode(token);
+        return claims.get("userId", Long.class);
+    }
 
 }
