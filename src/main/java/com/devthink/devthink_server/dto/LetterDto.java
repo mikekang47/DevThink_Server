4a56921a4c13a60940a6de49b1d6fce7e9114569
minu kim <kmw106933@naver.com>
2022-01-20 22:32:16 +0900

    refactor; make sendId, recvId in letterDto

--- src/main/java/com/devthink/devthink_server/dto/LetterDto.java
+++ src/main/java/com/devthink/devthink_server/dto/LetterDto.java
@@ -15,13 +15,16 @@ public class LetterDto {
     @Mapping("roomId")
     private Long roomId;
 
-    @Mapping("userId")
-    private Long userId;   // 메시지 보낸 사람
+    @Mapping("sendId")
+    private Long sendId;  // 메시지 보낸 사람
+
+    @Mapping("recvId")
+    private Long recvId; // 메시지 받는 사람
 
     @NotBlank(message = "쪽지의 내용을 입력해주세요.")
     @Mapping("content")
     private String content;
 
-    private LocalDateTime create_at;
+    private LocalDateTime create_at; // 보낸 시각
 
 }
