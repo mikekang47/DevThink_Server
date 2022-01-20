5e9242f68f8d61d68257d63d57f2d7bb508ca055
mikekang47 <gyeong0308@gmail.com>
2022-01-21 01:50:11 +0900

    Refactor: delete any other annotation in UserResultData

--- src/main/java/com/devthink/devthink_server/dto/UserResultData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserResultData.java
@@ -1,52 +1,41 @@
 package com.devthink.devthink_server.dto;
 
 import com.devthink.devthink_server.domain.BaseTimeEntity;
-import com.github.dozermapper.core.Mapping;
 import lombok.*;
-import org.apache.tomcat.jni.Local;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.LocalTime;
 import java.util.List;
 
 
 @Getter
 @Builder
 @AllArgsConstructor
-public class UserResultData extends BaseTimeEntity {
+public class UserResultData {
     private Long id;
 
-    @NotBlank
     private String email;
 
-    @NotBlank
-    private String password;
+    private String name;
 
-    @NotBlank
-    @Size(max=8)
     private String nickname;
 
-    @NotBlank
+    private String phoneNum;
+
     private String role;
 
-    @Builder.Default
-    private List<String> stack = new ArrayList<>();
+    private List<String> stack;
 
-    @Mapping("bloagAddr")
     private String blogAddr;
 
-    @Mapping("gitNickname")
     private String gitNickname;
 
     private Integer point;
 
     private boolean deleted;
 
-    @Mapping("createAt")
-    private LocalDateTime createAt;
+    private LocalDateTime create_at;
 
-    @Mapping("updateAt")
-    private LocalDateTime updateAt;
+    private LocalDateTime update_at;
 }
