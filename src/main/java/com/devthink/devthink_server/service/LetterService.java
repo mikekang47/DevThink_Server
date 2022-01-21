1699b19b679ccd172220fac9b3872547abb20a17
minu kim <kmw106933@naver.com>
2022-01-21 15:27:21 +0900

    Feat: make Letter Service

--- src/main/java/com/devthink/devthink_server/service/LetterService.java
+++ src/main/java/com/devthink/devthink_server/service/LetterService.java
@@ -1,15 +1,14 @@
 package com.devthink.devthink_server.service;
 
 import com.devthink.devthink_server.domain.Letter;
-import com.devthink.devthink_server.domain.Post;
-import com.devthink.devthink_server.dto.LetterDto;
-import com.devthink.devthink_server.errors.PostNotFoundException;
+import com.devthink.devthink_server.dto.LetterAddData;
 import com.devthink.devthink_server.infra.LetterRepository;
 import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Service
@@ -27,9 +26,14 @@ public class LetterService {
      * 메시지 작성
      * @param dto
      */
-    public void addMessage(LetterDto dto){
-        Letter letter = mapper.map(dto, Letter.class);
-        letterRepository.save(letter);
+    public void addMessage(LetterAddData dto){
+
+
+        Letter RecvData = getLetterData(dto, dto.getRecvId());
+        Letter SendData = getLetterData(dto, dto.getSendId());
+
+        letterRepository.save(RecvData);
+        letterRepository.save(SendData);
 
     }
 
@@ -40,7 +44,44 @@ public class LetterService {
      * @return
      */
     public List<Letter> getMessage(Long user_id, Long room_id){
-        List<Letter> letters = letterRepository.findView(user_id, room_id);
+        List<Letter> letters = letterRepository.findByUserIdAndRoomIdOrderByIdDesc(user_id, room_id);
         return letters;
     }
+
+
+    /**
+     * 유저의 특정방의 최근쪽지를 조회합니다.
+     * @param dto
+     * @param user_id
+     * @return
+     */
+    public Letter getRecentSms(Long user_id, Long room_id)
+    {
+        return letterRepository.
+                findFirstByUserIdAndRoomIdOrderByIdDesc(user_id, room_id);
+    }
+
+    /**
+     * 유저의 roomid의 최댓값을 불러옵니다.
+     */
+    public Long getMaxRoomId(Long user_id)
+    {
+        return letterRepository.getMaxRoomId(user_id);
+    }
+
+    private Letter getLetterData(LetterAddData dto, Long user_id)
+    {
+        if(dto == null)
+            return null;
+
+        return Letter.builder()
+                .userId(user_id)
+                .roomId(dto.getRoomId())
+                .content(dto.getContent())
+                .build();
+
+    }
+
+
+
 }
