607679b3bd642457c054d513a39418636f6e9105
jaehee <vito822@naver.com>
2022-01-21 16:34:16 +0900

    Feat: add find methods in BookRepository class

--- src/main/java/com/devthink/devthink_server/infra/BookRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/BookRepository.java
@@ -1,18 +1,28 @@
 package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
+
     Optional<Book> getBookByIsbn(int isbn);
+
+    Page<Book> findAllOrderByScoreAvgDesc(Pageable pageable); // 평점 높은 순
+    Page<Book> findAllOrderByScoreAvg(Pageable pageable); // 평점 낮은 순
+    Page<Book> findAllOrderByReviewCntDesc(Pageable pageble); // 인기순 (리뷰 수 순)
+    Page<Book> findAllOrderByCreateAtDesc(Pageable pageable); // 최신순
+    Optional<Book> findTopByReviewCnt(); // 베스트 리뷰
 
     @Query(value = "select round(avg(score),2) from Review where book_id = :id and deleted = false")
     BigDecimal calcScoreAvg(@Param("id") long id);
