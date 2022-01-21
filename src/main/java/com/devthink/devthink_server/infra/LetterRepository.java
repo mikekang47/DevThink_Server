a101ecebe4712469f61b64a5538867c6024713af
minu kim <kmw106933@naver.com>
2022-01-21 15:27:10 +0900

    Feat: make query in LetterRepository

--- src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
@@ -1,18 +1,23 @@
 package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Letter;
+import com.devthink.devthink_server.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
 
-   @Modifying
-   @Query("select u from Letter u " +
-           "where (u.recvId = :userId or u.sendId = :userId) and u.roomId = :roomId " +
-           "order by u.id DESC")
-   List<Letter> findView(Long userId, Long roomId);
+    List<Letter> findByUserIdAndRoomIdOrderByIdDesc(Long userId, Long roomId);
+
+    Letter findFirstByUserIdAndRoomIdOrderByIdDesc(Long userId, Long roomId);
+
+    @Modifying
+    @Query("select max(p.roomId) from Letter p where p.userId = :userId")
+    Long getMaxRoomId(Long userId);
 }
