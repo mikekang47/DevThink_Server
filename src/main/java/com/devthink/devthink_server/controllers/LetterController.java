b68e32cb9758db192857068a6182ff98429bf017
minu kim <kmw106933@naver.com>
2022-01-21 15:24:46 +0900

    Feat: make getList method in LetterController

--- src/main/java/com/devthink/devthink_server/controllers/LetterController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LetterController.java
@@ -1,9 +1,10 @@
 package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.domain.Letter;
-import com.devthink.devthink_server.domain.Post;
-import com.devthink.devthink_server.dto.LetterDto;
-import com.devthink.devthink_server.dto.PostDto;
+import com.devthink.devthink_server.domain.UserRoom;
+import com.devthink.devthink_server.dto.LetterAddData;
+import com.devthink.devthink_server.dto.LetterResultData;
+import com.devthink.devthink_server.dto.UserRoomResultData;
 import com.devthink.devthink_server.service.LetterService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 // 메시지 전송 코드
@@ -27,14 +29,14 @@ public class LetterController {
 
     /**
      * 쪽지 보내기 기능
-     * @param letterdto
+     * @param
      * @return
      */
     @PostMapping
-    public ResponseEntity<String> addMessage(@RequestBody @Valid LetterDto letterdto){
+    public ResponseEntity<String> addMessage(@RequestBody @Valid LetterAddData letterAddData){
         ResponseEntity<String> entity = null;
         try {
-            letterService.addMessage(letterdto);
+            letterService.addMessage(letterAddData);
             entity = new ResponseEntity<String>("쪽지를 성공적으로 보냈습니다.", HttpStatus.OK);
         } catch (Exception e){
             e.printStackTrace();
@@ -45,22 +47,37 @@ public class LetterController {
     }
 
     /**
-     * 유저 방에 있는 보낸 쪽지와 받은 쪽지 모두 확인
+     * 유저 방에 있는 보낸 쪽지와 받은 쪽지 정렬하여 모두 확인
      */
     @GetMapping
-    public List<LetterDto> getMessage(@RequestParam Long user_id, @RequestParam Long room_id)
+    public List<LetterResultData> getMessage(@RequestParam Long user_id, @RequestParam Long room_id)
     {
         List<Letter> messages = letterService.getMessage(user_id, room_id);
         return getLetterDtos(messages);
     }
 
     /**
-     * List<letter>를 List<LetterDto>로 변환합니다.
-     * @param letters
+     * 유저 쪽지방을 모두 확인합니다.
+     * @param
      * @return
      */
-    private List<LetterDto> getLetterDtos(List<Letter> letters) {
-        List<LetterDto> letterDtos = new ArrayList<>();
+    @GetMapping("/{user_id}")
+    public List<UserRoomResultData> getList(@PathVariable Long user_id)
+    {
+        Long max_roomId = letterService.getMaxRoomId(user_id);
+        List<Letter> messages = new ArrayList<>();
+
+        for(Long i=1L; i<=max_roomId; i++) {
+            Letter recentSms = letterService.getRecentSms(user_id, i);
+            messages.add(recentSms);
+        }
+        List<UserRoomResultData> userRoomDtos = getUserRoomDtos(messages);
+        return userRoomDtos;
+
+    }
+
+    private List<LetterResultData> getLetterDtos(List<Letter> letters) {
+        List<LetterResultData> letterDtos = new ArrayList<>();
 
         for (Letter letter : letters) {
             letterDtos.add(getLetterData(letter));
@@ -73,18 +90,41 @@ public class LetterController {
      * 게시글의 정보를 받아 게시글을 dto 데이터로 변환하여 반환합니다.
      * @return 입력된 dto 데이터로 변환된 값
      */
-    private LetterDto getLetterData(Letter letter)
+    private LetterResultData getLetterData(Letter letter)
     {
         if(letter == null)
             return null;
 
-        return LetterDto.builder()
-                .roomId(letter.getRoomId())
-                .sendId(letter.getSendId())
-                .recvId(letter.getRecvId())
+        return LetterResultData.builder()
                 .content(letter.getContent())
                 .create_at(letter.getCreate_at())
+                .read_chk(letter.getRead_chk())
+                .room_id(letter.getRoomId())
+                .user_id(letter.getUserId())
+                .view_at(letter.getView_at())
                 .build();
 
+    }
+
+    private List<UserRoomResultData> getUserRoomDtos(List<Letter> letters) {
+        List<UserRoomResultData> letterDtos = new ArrayList<>();
+
+        for (Letter letter : letters) {
+            letterDtos.add(getUserRoomData(letter));
+
+        }
+        return letterDtos;
+    }
+
+    private UserRoomResultData getUserRoomData(Letter letter)
+    {
+        if(letter == null)
+            return null;
+
+        return UserRoomResultData.builder()
+                .recentSms(letter.getContent())
+                .room_id(letter.getRoomId())
+                .create_at(letter.getCreate_at())
+                .build();
     }
 }
