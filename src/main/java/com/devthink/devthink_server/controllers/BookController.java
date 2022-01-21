7dd3098a25ea8167af625eca9434fcb530d8d31f
jaehee <vito822@naver.com>
2022-01-21 16:37:01 +0900

    Add: BookController class

---
+++ src/main/java/com/devthink/devthink_server/controllers/BookController.java
@@ -0,0 +1,15 @@
+package com.devthink.devthink_server.controllers;
+
+import com.devthink.devthink_server.application.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+}
