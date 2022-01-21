e062d6d3f13bdc303d0f71ccdb3ab20ff93b7619
linuxlight <lnxlht4j@gmail.com>
2022-01-21 15:12:29 +0900

    Feat: add connection with Post Entity

--- src/main/java/com/devthink/devthink_server/domain/Comment.java
+++ src/main/java/com/devthink/devthink_server/domain/Comment.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -26,7 +23,8 @@ public class Comment extends BaseTimeEntity{
     @ManyToOne(fetch = LAZY)
     User user;
 
-    //Post post;
+    @ManyToOne(fetch = LAZY)
+    Post post;
 
     @ManyToOne(fetch = LAZY)
     Review review;
