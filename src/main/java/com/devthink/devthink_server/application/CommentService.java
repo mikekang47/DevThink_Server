905e3a5dbeb88b08204a2a04e50e9d669005cb96
linuxlight <lnxlht4j@gmail.com>
2022-01-21 15:09:25 +0900

    Fix: createComment method in CommentService class now saves Comment of real User and Review

--- src/main/java/com/devthink/devthink_server/application/CommentService.java
+++ src/main/java/com/devthink/devthink_server/application/CommentService.java
@@ -1,10 +1,14 @@
 package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.Comment;
-import com.devthink.devthink_server.domain.CommentRepository;
+import com.devthink.devthink_server.domain.UserRepository;
+import com.devthink.devthink_server.errors.ReviewNotFoundException;
+import com.devthink.devthink_server.errors.UserNotFoundException;
+import com.devthink.devthink_server.infra.CommentRepository;
 import com.devthink.devthink_server.domain.Review;
 import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.dto.CommentRequestDto;
+import com.devthink.devthink_server.infra.ReviewRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,9 +19,15 @@ import java.util.Optional;
 @Transactional
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
 
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository,
+                          UserRepository userRepository,
+                          ReviewRepository reviewRepository) {
         this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.reviewRepository = reviewRepository;
     }
 
     /**
@@ -62,12 +72,23 @@ public class CommentService {
 
     /**
      * 입력된 comment 정보로 새로운 Comment를 생성합니다.
-     * @param user Comment를 생성하려고 하는 User
-     * @param review Comment가 달리게 되는 Review
-     * @param commentRequestDto
+     * @param commentRequestDto Comment를 생성하려고 하는 request
      * @return 생성된 Comment의 id 값
      */
-    public String createComment(User user, Review review, CommentRequestDto commentRequestDto) {
+    public String createComment(CommentRequestDto commentRequestDto) {
+        // request상에 userId 값이 들어있는지 확인합니다.
+        Long userId = Optional.ofNullable(commentRequestDto.getUser_id())
+                                .orElseThrow(()->new IllegalArgumentException("The userId cannot be null"));
+        // request상에 reviewId 값이 들어있는지 확인합니다.
+        Long reviewId = Optional.ofNullable(commentRequestDto.getReview_id())
+                .orElseThrow(()->new IllegalArgumentException("The reviewId cannot be null"));
+
+        // userId 값을 통하여 userRepository에서 User를 가져옵니다.
+        User user = findUser(userId);
+        // reviewId 값을 통하여 reviewRepository에서 Review를 가져옵니다.
+        Review review = findReview(reviewId);
+
+        // commentRepository에 새로운 댓글을 생성합니다.
         Comment comment = commentRepository.save(
                 Comment.builder()
                         .user(user)
@@ -86,5 +107,22 @@ public class CommentService {
     public void updateComment(Comment comment, String content) {
 
         comment.setContent(content);
+    }
+
+
+    /**
+     * 전달받은 사용자의 식별자를 이용하여 사용자를 DB에서 찾고, 없으면 Error를 보냅니다.
+     * @param id 찾고자 하는 사용자의 식별자
+     * @return 찾았을 경우 사용자를 반환. 찾지 못했을 경우 error를 반환.
+     */
+    private User findUser(Long id) {
+        return userRepository.findByIdAndDeletedIsFalse(id)
+                .orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    //리뷰 조회
+    private Review findReview(Long id) {
+        return reviewRepository.findByIdAndDeletedIsFalse(id)
+                .orElseThrow(() -> new ReviewNotFoundException(id));
     }
 }
