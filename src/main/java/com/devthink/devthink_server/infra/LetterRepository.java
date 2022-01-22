ab705085e489bff2da9533395ccdb0f0cf503df6
minu kim <kmw106933@naver.com>
2022-01-22 15:27:14 +0900

    refactor: insert query in letterRepository

--- src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
@@ -1,23 +1,24 @@
 package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Letter;
-import com.devthink.devthink_server.domain.Post;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
-
+import java.util.ArrayList;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
 
-    List<Letter> findByUserIdAndRoomIdOrderByIdDesc(Long userId, Long roomId);
+    //메시지 리스트 가져오기
+    @Query("select u from Letter u where u.id " +
+            "in (select max(u.id) from Letter group by u.roomId) " +
+            "and (u.senderId = :userId or u.targetId = :userId) order by u.id desc")
+    ArrayList<Letter> getMessageList(Long userId);
 
-    Letter findFirstByUserIdAndRoomIdOrderByIdDesc(Long userId, Long roomId);
 
-    @Modifying
-    @Query("select max(p.roomId) from Letter p where p.userId = :userId")
-    Long getMaxRoomId(Long userId);
+    // 안읽은 메시지 갯수 가져오기
+    @Query("select count(u.id) from Letter u where u.targetId = :userId and u.readCheck = 0 and u.roomId = :roomId")
+    Long countUnread(Long userId, Long roomId);
+
+
 }
