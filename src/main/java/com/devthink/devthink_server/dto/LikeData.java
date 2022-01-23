476ac609815d080792dfc4dc1ebc44ef8afd1137
mikekang47 <gyeong0308@gmail.com>
2022-01-23 23:48:48 +0900

    Feat: make like dto

---
+++ src/main/java/com/devthink/devthink_server/dto/LikeData.java
@@ -0,0 +1,16 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor
+public class LikeData {
+    private Long userId;
+
+    private Long postId;
+}
