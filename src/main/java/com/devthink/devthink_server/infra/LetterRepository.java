683d69175c8a69b9d0933977196a917a7ac9701d
minu kim <kmw106933@naver.com>
2022-01-23 20:11:09 +0900

    Feat: Add message content read and read processing function

--- src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
@@ -9,16 +9,25 @@ import java.util.ArrayList;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
 
-    //메시지 리스트 가져오기
+    // 방 별 메시지 리스트 가져오기
     @Query("select u from Letter u where u.id " +
             "in (select max(u.id) from Letter group by u.roomId) " +
             "and (u.senderId = :userId or u.targetId = :userId) order by u.id desc")
     ArrayList<Letter> getMessageList(Long userId);
 
-
-    // 안읽은 메시지 갯수 가져오기
+    // 안 읽은 메시지 갯수 가져오기
     @Query("select count(u.id) from Letter u where u.targetId = :userId and u.readCheck = 0 and u.roomId = :roomId")
     Long countUnread(Long userId, Long roomId);
 
+    // 메시지 내용 가져오기
+    @Query("select u from Letter u where u.roomId = :roomId and " +
+            "(u.senderId = :userId or u.targetId = :userId)")
+    ArrayList<Letter> getRoomLists(Long userId, Long roomId);
+
+    // 메시지 읽음 처리하기
+    @Modifying
+    @Query("update Letter u set u.readCheck = 1 where u.roomId = :roomid and " +
+            "u.readCheck = 0 and u.targetId = :userId")
+    int MessageReadCheck(Long userId, Long roomId);
 
 }
