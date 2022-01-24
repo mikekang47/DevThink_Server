d480cc17c010556c76a3e7228fc8444356817f8a
mikekang47 <gyeong0308@gmail.com>
2022-01-25 01:28:22 +0900

    Refactor: complete detail method in LikeController

--- src/main/java/com/devthink/devthink_server/controllers/LikeController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LikeController.java
@@ -1,7 +1,10 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.application.LikeService;
 import com.devthink.devthink_server.domain.Like;
+import com.devthink.devthink_server.dto.LikeData;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/likes")
 public class LikeController {
 
-    @GetMapping
-    public Like detail() {
-        return null;
+    private final LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @GetMapping("/{id}")
+    public LikeData detail(@PathVariable Long id) {
+        Like like = likeService.getLike(id);
+        return getLikeData(like);
+    }
+
+    private LikeData getLikeData(Like like) {
+        return LikeData.builder()
+                .userId(like.getUserId())
+                .postId(like.getPostId())
+                .build();
     }
 }
