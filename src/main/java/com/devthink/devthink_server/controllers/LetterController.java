192679a0e56b4f7a9cf447a04cb7361ef02187fa
minu kim <kmw106933@naver.com>
2022-01-24 15:27:29 +0900

    Feat: add messageList method

--- src/main/java/com/devthink/devthink_server/controllers/LetterController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LetterController.java
@@ -3,6 +3,7 @@ package com.devthink.devthink_server.controllers;
 import com.devthink.devthink_server.domain.Letter;
 import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.LetterAddData;
+import com.devthink.devthink_server.dto.LetterListData;
 import com.devthink.devthink_server.dto.LetterResultData;
 import com.devthink.devthink_server.dto.PostResultData;
 import com.devthink.devthink_server.service.LetterService;
@@ -21,7 +22,9 @@ import java.util.List;
 
 // 1. 쪽지 보내기  -> POST /messages
 // 2. 사용자 id로 방별 메시지 리스트 검색 -> GET /messages?user_id=숫자
-
+// ---> unread : 안 읽은 메시지 갯수
+// 3. 사용자 id와 방 id로 메시지 읽기 -> GET /messages/rooms?user_id=숫자&room_id=숫자
+// ---> readCheck 기본 값 0 : 안 읽은 경우, 1로 갱신하여 읽음 처리
 @RestController
 @RequestMapping("/messages")
 public class LetterController {
@@ -33,22 +36,15 @@ public class LetterController {
     }
 
     /**
-     * 사용자로부터 쪽지 정보를 받아 쪽지를 보냅니다.
+     * 사용자로부터 쪽지 정보를 받아 메시지 리스트에서 쪽지를 보냅니다.
      * @param letterAddData 쪽지를 보내기 위한 데이터
      * @return String 쪽지 성공 여부 문자열로 반환
      */
     @PostMapping
-    public ResponseEntity<String> addMessage(@RequestBody @Valid LetterAddData letterAddData){
-        ResponseEntity<String> entity = null;
-        try {
-            letterService.addMessage(letterAddData);
-            entity = new ResponseEntity<String>("쪽지를 성공적으로 보냈습니다.", HttpStatus.OK);
-        } catch (Exception e){
-            e.printStackTrace();
-            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-
-        }
-        return entity;
+    public LetterResultData messageSendInlist(@RequestBody @Valid LetterAddData letterAddData)
+    {
+        Letter letter = letterService.addMessage(letterAddData);
+        return getLetterData(letter);
     }
 
     /**
@@ -58,11 +54,25 @@ public class LetterController {
      * 해당 room에서 안읽은 메시지 갯수)
      */
     @GetMapping
-    public List<LetterResultData> messageList(@RequestParam Long user_id)
+    public List<LetterListData> messageList(@RequestParam Long user_id)
     {
         ArrayList<Letter> letters = letterService.messageList(user_id);
-        return getLetterDtos(letters);
+        return getLetterListDtos(letters);
     }
+
+    /**
+     * 사용자로부터 유저 id와 방 id를 받아 메시지 내용을 가져옵니다.
+     * @param user_id 사용자의 고유 id 값, room_id 사용자의 방 번호
+     * @return List<LetterResultData> 사용자의 방 번호에서 주고받은 메시지
+     */
+    @GetMapping("/rooms")
+    public List<LetterResultData> MessageRoomList(@RequestParam Long user_id, @RequestParam Long room_id)
+    {
+        ArrayList<Letter> letters = letterService.roomContentList(user_id, room_id);
+        return getLetterDtos(letters);
+
+    }
+
 
     /**
      * entity List를 받아 dto List 데이터로 변환하여 반환합니다.
@@ -74,6 +84,20 @@ public class LetterController {
 
         for (Letter letter : letters) {
             letterDtos.add(getLetterData(letter));
+        }
+        return letterDtos;
+    }
+
+    /**
+     * entity List를 받아 dto List 데이터로 변환하여 반환합니다.
+     * @param letters entity List
+     * @return 입력된 dto 데이터로 변환된 list
+     */
+    private List<LetterListData> getLetterListDtos(List<Letter> letters) {
+        List<LetterListData> letterDtos = new ArrayList<>();
+
+        for (Letter letter : letters) {
+            letterDtos.add(getLetterListData(letter));
         }
         return letterDtos;
     }
@@ -95,8 +119,27 @@ public class LetterController {
                 .roomId(letter.getRoomId())
                 .senderId(letter.getSenderId())
                 .targetId(letter.getTargetId())
-                .view_at(letter.getView_at())
                 .create_at(letter.getCreate_at())
+                .build();
+
+    }
+
+    /**
+     * 쪽지의 정보를 받아 쪽지를 dto 데이터로 변환하여 반환합니다.
+     * @param letter 쪽지 정보
+     * @return 입력된 dto 데이터로 변환된 값
+     */
+    private LetterListData getLetterListData(Letter letter)
+    {
+        if(letter == null)
+            return null;
+
+        return LetterListData.builder()
+                .content(letter.getContent())
+                .otherId(letter.getOther_id())
+                .create_at(letter.getCreate_at())
+                .roomId(letter.getRoomId())
+                .unread(letter.getUnread())
                 .build();
 
     }
