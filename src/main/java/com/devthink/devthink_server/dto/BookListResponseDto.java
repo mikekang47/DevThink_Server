2e7cc0bad43d406ea8df906ba90cfca6ea95de44
jaehee <vito822@naver.com>
2022-01-24 16:43:33 +0900

    Add: BookListResponseDto class

---
+++ src/main/java/com/devthink/devthink_server/dto/BookListResponseDto.java
@@ -0,0 +1,20 @@
+package com.devthink.devthink_server.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+public class BookListResponseDto {
+
+    private Long id;
+
+    private Integer isbn;
+
+    private Integer reviewCnt;
+
+    private BigDecimal scoreAvg;
+
+}
