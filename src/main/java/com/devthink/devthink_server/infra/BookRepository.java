151de572e1a289545ae3ea941e5c3973ef60d71c
jaehee <vito822@naver.com>
2022-01-25 10:41:30 +0900

    Fix: findAll methods error
    findAll -> findAllBy

--- src/main/java/com/devthink/devthink_server/infra/BookRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/BookRepository.java
@@ -3,6 +3,7 @@ package com.devthink.devthink_server.infra;
 import com.devthink.devthink_server.domain.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,10 +19,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> getBookByIsbn(int isbn);
 
-    List<Book> findAllOrderByScoreAvgDesc(Pageable pageable); // 평점 높은 순
-    List<Book> findAllOrderByScoreAvg(Pageable pageable); // 평점 낮은 순
-    List<Book> findAllOrderByReviewCntDesc(Pageable pageble); // 인기순 (리뷰 수 순)
-    List<Book> findAllOrderByCreateAtDesc(Pageable pageable); // 최신순
+    List<Book> findAllByOrderByScoreAvgDesc(); // 평점 높은 순
+    List<Book> findAllByOrderByScoreAvg(); // 평점 낮은 순
+    List<Book> findAllByOrderByReviewCntDesc(); // 인기순 (리뷰 수 순)
+    List<Book> findAllByOrderByCreateAtDesc(); // 최신순
     Optional<Book> findTopByReviewCnt(); // 베스트 리뷰
 
     @Query(value = "select round(avg(score),2) from Review where book_id = :id and deleted = false")
