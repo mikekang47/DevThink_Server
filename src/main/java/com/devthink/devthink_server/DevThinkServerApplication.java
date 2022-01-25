af320f88a3d25af3a01f1f2ad024d02e9b24a1ad
mikekang47 <gyeong0308@gmail.com>
2022-01-25 22:11:20 +0900

    Refactor: move @JpaAuditing from main to JpaConfig

--- src/main/java/com/devthink/devthink_server/DevThinkServerApplication.java
+++ src/main/java/com/devthink/devthink_server/DevThinkServerApplication.java
@@ -5,10 +5,9 @@ import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
 
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class DevThinkServerApplication {
 
