0ee7786756c4986fed896fbce68b88490d3d65e5
minu kim <kmw106933@naver.com>
2022-01-27 00:25:47 +0900

    refactor: Change the snake case to a camel case

--- src/main/java/com/devthink/devthink_server/dto/LetterResultData.java
+++ src/main/java/com/devthink/devthink_server/dto/LetterResultData.java
@@ -1,5 +1,6 @@
 package com.devthink.devthink_server.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,18 +14,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class LetterResultData {
 
+    @ApiModelProperty(notes = "방 번호", example = "1")
     private Long roomId;
 
+    @ApiModelProperty(notes = "메시지 발신자", example = "1")
     private Long senderId;
 
+    @ApiModelProperty(notes = "메시지 수신자", example = "2")
     private Long targetId;
 
+    @ApiModelProperty(notes = "메시지 내용", example = "example")
     private String content;
 
-    private LocalDateTime create_at;
+    @ApiModelProperty(notes = "메시지 생성 시각", example = "2022-01-26T22:07:17.0831141")
+    private LocalDateTime createAt;
 
-    private LocalDateTime view_at;
+    @ApiModelProperty(notes = "메시지 본 시각", example = "2022-01-26T22:07:17.0831141")
+    private LocalDateTime viewAt;
 
+    @ApiModelProperty(notes = "메시지 읽음 체크", example = "0")
     private Integer readCheck;
 
 }
