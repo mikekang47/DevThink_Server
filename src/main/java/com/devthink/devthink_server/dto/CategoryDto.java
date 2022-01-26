0ab2cd8f61543a3bf2432bdf4df67c2b83010554
minu kim <kmw106933@naver.com>
2022-01-27 00:25:08 +0900

    Feat: add ApiParam and ApiModelProperty in CategoryDto

--- src/main/java/com/devthink/devthink_server/dto/CategoryDto.java
+++ src/main/java/com/devthink/devthink_server/dto/CategoryDto.java
@@ -1,6 +1,8 @@
 package com.devthink.devthink_server.dto;
 
 import com.github.dozermapper.core.Mapping;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -12,9 +14,13 @@ import javax.validation.constraints.NotBlank;
 public class CategoryDto {
 
     @Mapping("id")
+    @ApiParam(value = "카테고리 고유 번호", example = "1")
+    @ApiModelProperty(notes = "카테고리 고유 번호", example = "1")
     private Long id;
 
     @Mapping("name")
+    @ApiParam(value = "카테고리 이름", example = "example", required = true)
+    @ApiModelProperty(notes = "카테고리 이름", example = "example")
     @NotBlank(message = "카테고리 이름을 입력해주세요.")
     private String name;
 
