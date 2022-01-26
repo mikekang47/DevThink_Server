74878ef0e454041f9402644fc3130e1796b897ce
minu kim <kmw106933@naver.com>
2022-01-27 00:25:54 +0900

    refactor: Change the snake case to a camel case

--- src/main/java/com/devthink/devthink_server/dto/PostDto.java
+++ src/main/java/com/devthink/devthink_server/dto/PostDto.java
@@ -1,6 +1,8 @@
 package com.devthink.devthink_server.dto;
 
 import com.github.dozermapper.core.Mapping;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -12,20 +14,30 @@ import javax.validation.constraints.NotBlank;
 public class PostDto {
 
     @Mapping("user_id")
-    private Long user_id;
+    @ApiModelProperty(value = "유저 아이디", example = "1")
+    @ApiParam(value = "유저 아이디", example = "1")
+    private Long userId;
 
     @Mapping("category_id")
-    private Long category_id;
+    @ApiModelProperty(value = "카테고리 아이디", example = "1")
+    @ApiParam(value = "카테고리 아이디", example = "1")
+    private Long categoryId;
 
     @NotBlank(message = "제목을 입력해주세요.")
+    @ApiModelProperty(value = "제목", example = "example")
+    @ApiParam(value = "제목", example = "example")
     @Mapping("title")
     private String title;
 
     @NotBlank(message = "글의 내용을 입력해주세요.")
+    @ApiModelProperty(value = "내용", example = "example1")
+    @ApiParam(value = "내용", example = "example1")
     @Mapping("content")
     private String content;
 
     @Mapping("status")
+    @ApiModelProperty(value = "글 상태", example = "active")
+    @ApiParam(value = "글 상태", example = "active")
     @Builder.Default
     private String status = "active";
 
