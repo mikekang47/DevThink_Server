c18fad1aa7472f8a3771a6fd21453bf0e3ce4e60
minu kim <kmw106933@naver.com>
2022-01-27 00:24:14 +0900

    Feat: add swagger annotation in PostController

--- src/main/java/com/devthink/devthink_server/controllers/PostController.java
+++ src/main/java/com/devthink/devthink_server/controllers/PostController.java
@@ -1,8 +1,11 @@
 package com.devthink.devthink_server.controllers;
 
+import com.devthink.devthink_server.application.PostService;
 import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.dto.PostDto;
-import com.devthink.devthink_server.service.PostService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,11 +39,14 @@ public class PostController {
     }
 
     /**
-     * 게시글에서 페이지를 요청하면 몇 페이지의 게시글을 가져옵니다.
+     * 사용자가 페이지를 요청하면 몇 페이지의 게시글을 가져옵니다.
      * @param page 사용자가 요청한 페이지 수
      * @return 몇 페이지의 게시글 (기본값 6개의 게시글)
      */
     @GetMapping
+    @ApiOperation(value = "게시글 리스트", notes = "사용자가 페이지를 요청하면 해당하는 페이지의 게시글을 가져옵니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "int", example ="1", value = "게시글의 페이지")})
     public List<PostDto> list(@RequestParam(value="page", defaultValue = "1") int page){
         List<Post> list = postService.list(page);
         return getPostDtos(list);
@@ -52,6 +58,9 @@ public class PostController {
      * @return id의 게시글
      */
     @GetMapping("/{id}")
+    @ApiOperation(value = "게시글 검색", notes = "게시글의 id를 검색하여 게시글을 가져옵니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", dataType = "Long", value = "게시글 고유 아이디")})
     public PostDto findById(@PathVariable Long id){
         Post post = postService.getPost(id);
         return getPostData(post);
@@ -65,6 +74,8 @@ public class PostController {
      */
     @PostMapping("/write")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "게시글 등록",
+            notes = "입력한 게시글의 정보(유저 id, 카테고리 id, 제목, 내용, status)를 입력받아 게시글을 생성합니다.")
     public PostDto write(@RequestBody @Valid PostDto postDto){
         Post post = postService.savePost(postDto);
         return getPostData(post);
@@ -78,6 +89,10 @@ public class PostController {
      */
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "게시글 수정",
+            notes = "입력한 게시글의 식별자 값과 valid한 게시글의 정보를 받아, 기존의 게시글을 입력한 정보로 변경합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", dataType = "Long", value = "게시글 고유 아이디")})
     public PostDto update(@PathVariable Long id, @RequestBody @Valid PostDto postDto){
         Post update = postService.update(id, postDto);
         return getPostData(update);
@@ -88,18 +103,25 @@ public class PostController {
      * @param id 게시글의 식별자
      */
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "게시글 삭제", notes = "입력한 게시글의 식별자 값을 받아 게시글을 삭제합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", dataType = "Long", value = "게시글 고유 아이디")})
     public void deletePost(@PathVariable Long id)
     {
         postService.deletePost(id);
     }
 
     /**
-     * keyword를 검색하여, keyword가 담긴 제목의 게시글을 탐색합니다.
-     * @param keyword 검색하는 제목
+     * 사용자로부터 keyword를 받아, 제목이 keyword인 게시글을 반환합니다.
+     * @param keyword 검색하려는 제목
      * @return keyword가 담긴 제목의 게시글
      */
 
     @GetMapping("/search")
+    @ApiOperation(value = "게시글 검색",
+            notes = "사용자로부터 keyword를 받아, 제목이 keyword인 게시글을 반환합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "keyword", dataType = "Long", value = "검색하고자 하는 게시글 제목")})
     public List<PostDto> search(@RequestParam String keyword)
     {
         List<Post> search = postService.search(keyword);
@@ -132,8 +154,8 @@ public class PostController {
             return null;
 
         return PostDto.builder()
-                .user_id(post.getUser_id())
-                .category_id(post.getCategory_id())
+                .userId(post.getUserId())
+                .categoryId(post.getCategoryId())
                 .title(post.getTitle())
                 .content(post.getContent())
                 .status(post.getStatus())
