7a71fcb06ebce254750d1b1965b238051313d30e
jaehee <vito822@naver.com>
2022-01-26 22:42:36 +0900

    Refactor: fix name of methods

--- src/main/java/com/devthink/devthink_server/controllers/ReviewController.java
+++ src/main/java/com/devthink/devthink_server/controllers/ReviewController.java
@@ -4,8 +4,6 @@ package com.devthink.devthink_server.controllers;
 import com.devthink.devthink_server.application.BookService;
 import com.devthink.devthink_server.application.ReviewService;
 import com.devthink.devthink_server.application.UserService;
-import com.devthink.devthink_server.common.Error;
-import com.devthink.devthink_server.common.ErrorMessage;
 import com.devthink.devthink_server.domain.Book;
 import com.devthink.devthink_server.domain.Review;
 import com.devthink.devthink_server.domain.User;
@@ -13,11 +11,9 @@ import com.devthink.devthink_server.dto.ReviewRequestDto;
 import com.devthink.devthink_server.dto.ReviewResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/reviews")
@@ -29,14 +25,14 @@ public class ReviewController {
     private final UserService userService;
 
     /**
-     * 리뷰 등록 API
+     * 입력한 valid한 리뷰 정보를 받아 리뷰를 생성합니다.
      * [POST] /reviews
      * @param reviewRequestDto (userId, bookIsbn, content, score)
-     * @return reviewId (새로 생성된 리뷰 아이디)
+     * @return 새로 생성된 리뷰 아이디
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String createReview(@Valid @RequestBody ReviewRequestDto reviewRequestDto){
+    public String create(@Valid @RequestBody ReviewRequestDto reviewRequestDto){
         User user = userService.getUser(reviewRequestDto.getUserId());
         Book book = bookService.getBookByIsbn(reviewRequestDto.getBookIsbn());
         String id = reviewService.createReview(user, book, reviewRequestDto);
@@ -45,21 +41,22 @@ public class ReviewController {
 
 
     /**
-     * 리뷰 상세 조회 API
+     * 주어진 id 의 리뷰를 상세 조회합니다.
      * [GET] /reviews/:id
      * @param id (조회할 리뷰 아이디)
-     * @return
+     * @return ReviewResponseDto (조회한 리뷰 정보 )
      */
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ReviewResponseDto getReview(@PathVariable("id") Long id){
+    public ReviewResponseDto detail(@PathVariable("id") Long id){
         Review review = reviewService.getReviewById(id);
         return review.toReviewResponseDto();
     }
 
+
     /**
-     * 리뷰 내용 수정 API
+     * 주어진 id 의 리뷰 내용을 수정합니다.
      * [PATCH] /reviews/:id/content
      * @param id (수정할 리뷰 아이디)
      */
@@ -71,11 +68,12 @@ public class ReviewController {
         return review.toReviewResponseDto();
     }
 
+
     /**
-     * 리뷰 점수 수정 API
+     * 주어진 id 의 리뷰 점수를 수정합니다.
      * [PATCH] /reviews/:id/score
      * @param id (수정할 리뷰 아이디 )
-     * @return review(삭제된 리뷰)
+     * @return ReviewResponseDto (삭제된 리뷰 )
      */
     @PatchMapping("/{id}/score")
     @ResponseStatus(HttpStatus.OK)
@@ -85,17 +83,19 @@ public class ReviewController {
         return review.toReviewResponseDto();
     }
 
+
     /**
-     * 리뷰 삭제 API
+     * 주어진 id 의 리뷰를 삭제합니다.
      * [DELETE] /reviews/:id
      * @param id
-     * @return review(삭제된 리뷰 객체)
+     * @return review(삭제된 리뷰 정보 )
      */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ReviewResponseDto deleteReview(@PathVariable("id") Long id){
+    public ReviewResponseDto destroy(@PathVariable("id") Long id){
         Review review = reviewService.getReviewById(id);
         reviewService.deleteReview(review);
         return review.toReviewResponseDto();
     }
+
 }
