7afe148cef6a9ecbd81327944c81f49f6664d134
mikekang47 <gyeong0308@gmail.com>
2022-01-27 21:41:30 +0900

    Refactor: change @ApiModelProperty to @ApiParam

--- src/main/java/com/devthink/devthink_server/dto/UserModificationData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserModificationData.java
@@ -1,7 +1,7 @@
 package com.devthink.devthink_server.dto;
 
 import com.github.dozermapper.core.Mapping;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,36 +19,34 @@ public class UserModificationData {
     @NotBlank(message = "닉네임을 입력해주세요.")
     @Size(max=8)
     @Mapping("nickname")
-    @ApiModelProperty(value = "사용자 닉네임", example = "싱크싱크")
+    @ApiParam(value = "사용자 닉네임", example = "싱크싱크")
     private String nickname;
 
     @NotBlank(message = "비멀번호를 입력해주세요.")
     @Size(min=8)
     @Mapping("password")
-    @ApiModelProperty(value = "사용자 비밀번호", example = "test12345678")
+    @ApiParam(value = "사용자 비밀번호", required = true, example = "test12345678")
     private String password;
 
     @NotBlank(message = "직무를 선택해주세요.")
     @Mapping("role")
-    @ApiModelProperty(value = "사용자 수준", example = "주니어")
+    @ApiParam(value = "사용자 수준", required = true, example = "주니어")
     private String role;
 
     @Mapping("stack")
-    @ApiModelProperty(value = "사용자가 사용하는 기술", example = "C++, Java, Spring")
+    @ApiParam(value = "사용자가 사용하는 기술", example = "C++, Java, Spring")
     private List<String> stack;
 
     @Mapping("blogAddr")
-    @ApiModelProperty(value = "사용자의 기술 블로그 주소", example = "mirrorofcode.tistory.com")
+    @ApiParam(value = "사용자의 기술 블로그 주소", example = "mirrorofcode.tistory.com")
     private String blogAddr;
 
     @Mapping("gitNickname")
-    @ApiModelProperty(value = "사용자의 github 닉네임", example = "mikekang47")
+    @ApiParam(value = "사용자의 github 닉네임", example = "mikekang47")
     private String gitNickname;
 
-    @ApiModelProperty(value = "사용자의 점수", example = "128")
     private Integer point;
 
-    @ApiModelProperty(value = "사용자 삭제 여부", example = "true")
     private boolean deleted;
     
 }
