431f8c0b4c1701aaf9fb2abbd62d1039b81c8cc5
minu kim <kmw106933@naver.com>
2022-01-27 15:31:07 +0900

    Feat: add findCreateAtBetween method in PostRepository

--- src/main/java/com/devthink/devthink_server/infra/PostRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/PostRepository.java
@@ -2,9 +2,15 @@ package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleContaining(String keyword);
+
+    // 좋아요 기능 추가
+    //Optional<Post> findCreateAtBetween(LocalDateTime start, LocalDateTime end);
 }
