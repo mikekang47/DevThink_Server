6ef4b838ad3bc17544f9321353c93f92daff5a2f
mikekang47 <gyeong0308@gmail.com>
2022-01-27 21:32:02 +0900

    Feat: add @ApiModelProperty for swagger in UserResultData

--- src/main/java/com/devthink/devthink_server/dto/UserResultData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserResultData.java
@@ -1,5 +1,6 @@
 package com.devthink.devthink_server.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,27 +13,39 @@ import java.util.List;
 public class UserResultData {
     private Long id;
 
+    @ApiModelProperty(value = "사용자 이메일", example = "test@email.com")
     private String email;
 
+    @ApiModelProperty(value = "사용자 이름", example = "김싱크")
     private String name;
 
+    @ApiModelProperty(value = "사용자 닉네임", example = "싱크싱크")
     private String nickname;
 
+    @ApiModelProperty(value = "사용자 전화번호", example = "010-1234-1234")
     private String phoneNum;
 
+    @ApiModelProperty(value = "사용자 수준", example = "주니어")
     private String role;
 
+    @ApiModelProperty(value = "사용자가 사용하는 기술", example = "C++, Java, Spring")
     private List<String> stack;
 
+    @ApiModelProperty(value = "사용자의 기술 블로그 주소", example = "mirrorofcode.tistory.com")
     private String blogAddr;
 
+    @ApiModelProperty(value = "사용자의 github 닉네임", example = "mikekang47")
     private String gitNickname;
 
+    @ApiModelProperty(value = "사용자의 점수", example = "128")
     private Integer point;
 
+    @ApiModelProperty(value = "사용자 삭제 여부", example = "true")
     private boolean deleted;
 
+    @ApiModelProperty(value = "사용자 생성 시각", example = "2022-01-24T16:19:01.359146")
     private LocalDateTime create_at;
 
+    @ApiModelProperty(value = "사용자 수정 시각", example = "2022-01-24T16:19:01.359146")
     private LocalDateTime update_at;
 }
