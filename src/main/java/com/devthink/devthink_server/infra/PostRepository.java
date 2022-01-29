84a72983047739e6ed9a154183bec2f4932e911f
minu kim <kmw106933@naver.com>
2022-01-29 14:01:06 +0900

    refactor: Add category factor

--- src/main/java/com/devthink/devthink_server/infra/PostRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/PostRepository.java
@@ -1,24 +1,20 @@
 package com.devthink.devthink_server.infra;
 
-import com.devthink.devthink_server.domain.Letter;
 import com.devthink.devthink_server.domain.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
     List<Post> findByTitleContaining(String keyword);
 
     Post save(Post post);
 
-
-    @Query("select p from Post p where (p.createAt between :start and :end) " +
-            "order by p.likeNumber Desc")
-    List<Post> getBestPosts(LocalDateTime start, LocalDateTime end, Pageable pageable);
-
+    @Query("select p from Post p where (p.createAt between :start and :end) and p.category.id = :category " +
+            "order by p.heart desc")
+    List<Post> getBestPost(Long category, LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
