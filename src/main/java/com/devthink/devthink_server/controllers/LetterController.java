244a898924c41f90196a4e6f1665a1621910890a
minu kim <kmw106933@naver.com>
2022-01-29 13:59:04 +0900

    Feat: add UserService

--- src/main/java/com/devthink/devthink_server/controllers/LetterController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LetterController.java
@@ -1,6 +1,7 @@
 package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.application.LetterService;
+import com.devthink.devthink_server.application.UserService;
 import com.devthink.devthink_server.domain.Letter;
 import com.devthink.devthink_server.dto.LetterAddData;
 import com.devthink.devthink_server.dto.LetterListData;
@@ -25,11 +26,13 @@ import java.util.List;
 // ---> unread : 안 읽은 메시지 갯수
 // 3. 사용자 id와 방 id로 메시지 읽기 -> GET /messages/rooms?user_id=숫자&room_id=숫자
 // ---> readCheck 기본 값 0 : 안 읽은 경우, 1로 갱신하여 읽음 처리
+
 @RestController
 @RequestMapping("/messages")
 public class LetterController {
 
     private LetterService letterService;
+    private UserService userService;
 
     public LetterController(LetterService letterService) {
         this.letterService = letterService;
@@ -50,20 +53,18 @@ public class LetterController {
     }
 
     /**
-     * 사용자로 부터 유저 id를 받아 방 별 메시지 리스트를 반환합니다.
-     * @param user_id 사용자의 고유 id 값
-     * @return List<LetterResultData> 메시지 리스트(최근 메시지, 보낸 사람 Id,
-     * 해당 room에서 안읽은 메시지 갯수)
+     * 유저 id를 받아 메시지 리스트를 반환합니다.
+     * @param userId 고유 id 값
+     * @return List<LetterResultData> 메시지 리스트
      */
     @GetMapping
     @ApiOperation(value = "메시지 리스트",
             notes = "사용자로부터 유저 고유 id를 받아 방 별 메시지 리스트를 반환합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "user_id", dataType = "Long", value = "유저 고유 아이디")})
-
-    public List<LetterListData> messageList(@RequestParam Long user_id)
+    public List<LetterListData> messageList(@RequestParam Long userId)
     {
-        ArrayList<LetterModificationData> letters = letterService.messageList(user_id);
+        ArrayList<LetterModificationData> letters = letterService.messageList(userId);
         return getLetterLists(letters);
     }
 
