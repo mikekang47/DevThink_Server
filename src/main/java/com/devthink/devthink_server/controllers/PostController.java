f16d7a199411b296c2ec93639e40b9f180b4855e
minu kim <kmw106933@naver.com>
2022-01-29 14:00:30 +0900

    refactor: make description simple

--- src/main/java/com/devthink/devthink_server/controllers/PostController.java
+++ src/main/java/com/devthink/devthink_server/controllers/PostController.java
@@ -19,10 +19,10 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
+
 /**
  * 사용자의 HTTP 요청을 처리하는 클래스입니다.
  */
@@ -32,9 +32,7 @@ import java.util.List;
 // 3. 글 업데이트 -> PUT /posts/{id}
 // 4. 글 삭제 -> DELETE /posts/{id}
 // 5. 페이지 가져오기 -> GET /posts?page=숫자
-// ----> 기본 1페이지, 6개의 게시글 가져오기, 최신 날짜순이 기본
 // 6. 키워드로 제목 검색 -> GET /posts/search?keyword=문자열
-// ----> 문자열이 제목인 게시글 가져오기
 
 public class PostController {
 
@@ -43,9 +41,9 @@ public class PostController {
     private final CategoryService categoryService;
 
     /**
-     * 사용자가 페이지를 요청하면 몇 페이지의 게시글을 가져옵니다.
-     * @param page 사용자가 요청한 페이지 수
-     * @return 몇 페이지의 게시글 (기본값 6개의 게시글)
+     * 페이지를 요청하면 페이지의 게시글을 가져옵니다.
+     * @param page 요청 페이지(기본 값: 1개)
+     * @return 페이지의 글(기본 값: 6개)
      */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -58,9 +56,9 @@ public class PostController {
     }
 
     /**
-     * 게시글의 id를 검색하여 게시글을 가져옵니다.
-     * @param id 게시글의 고유 id값
-     * @return id의 게시글
+     * 게시글의 Id를 검색하여 게시글을 반환합니다.
+     * @param id 게시글의 Id
+     * @return Id인 게시글
      */
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -70,13 +68,12 @@ public class PostController {
     public PostResponseData findById(@PathVariable Long id){
         Post post = postService.getPost(id);
         return getPostData(post);
-
     }
 
     /**
-     * 입력한 게시글의 정보(유저 id, 카테고리 id, 제목, 내용, status)를 입력받아 게시글을 생성합니다.
-     * @param postRequestData 입력한 vaild한 게시글의 정보
-     * @return  게시글 생성
+     * 게시글의 정보를 입력받아 게시글을 생성합니다.
+     * @param postRequestData 게시글의 정보
+     * @return 게시글 생성
      */
     @PostMapping("/write")
     @ResponseStatus(HttpStatus.CREATED)
@@ -87,16 +84,23 @@ public class PostController {
         Category category = categoryService.getCategory(postRequestData.getCategoryId());
 
         Post post = postService.savePost(user, category, postRequestData);
-        return getPostData(post);
+        PostResponseData postData = getPostData(post);
+
+        if(post.getImageUrl().isEmpty()){
+            postData.setImage(false);
+        }
+        else{
+            postData.setImage(true);
+        }
+        return postData;
     }
 
     /**
-     * 입력한 게시글의 식별자 값과 valid한 게시글의 정보를 받아, 기존의 게시글을 입력한 정보로 변경합니다.
+     * 게시글 식별자 값과 정보를 받아, 게시글을 입력한 정보로 변경합니다.
      * @param id 게시글의 식별자
-     * @param postRequestData 제목, 내용, status
-     * @return 기존 게시글의 정보 수정
+     * @param postRequestData 제목, 내용
+     * @return 게시글 정보 수정
      */
-
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "게시글 수정",
@@ -109,33 +113,30 @@ public class PostController {
     }
 
     /**
-     * 입력한 게시글의 식별자 값을 받아 게시글을 삭제합니다.
-     * @param id 게시글의 식별자
+     * 게시글의 식별자를 받아 게시글을 삭제합니다.
+     * @param id 게시글 식별자
      */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "게시글 삭제", notes = "입력한 게시글의 식별자 값을 받아 게시글을 삭제합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", dataType = "Long", value = "게시글 고유 아이디")})
-    public void deletePost(@PathVariable Long id)
-    {
+    public void deletePost(@PathVariable Long id){
         postService.deletePost(id);
     }
 
     /**
-     * 사용자로부터 keyword를 받아, 제목이 keyword인 게시글을 반환합니다.
-     * @param keyword 검색하려는 제목
-     * @return keyword가 담긴 제목의 게시글
+     * 키워드를 입력받아, 제목이 담긴 게시글을 반환합니다.
+     * @param keyword 검색 키워드
+     * @return 제목이 담긴 게시글
      */
-
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "게시글 검색",
             notes = "사용자로부터 keyword를 받아, 제목이 keyword인 게시글을 반환합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "keyword", dataType = "Long", value = "검색하고자 하는 게시글 제목")})
-    public List<PostResponseData> search(@RequestParam String keyword)
-    {
+    public List<PostResponseData> search(@RequestParam String keyword){
         List<Post> search = postService.search(keyword);
         List<PostResponseData> postRequestData = getPostList(search);
         return postRequestData;
@@ -150,16 +151,16 @@ public class PostController {
             notes = "사용자로부터 카테고리 id를 받아, 베스트 게시글을 가져옵니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "categoryId", dataType = "Long", value = "카테고리 아이디")})
-    public PostResponseData searchBest(@RequestParam Long categoryId)
-    {
-        Post bestPost = postService.getBestPost(categoryId);
-        return getPostData(bestPost);
+    public List<PostResponseData> searchBest(@RequestParam Long categoryId){
+        Category category = categoryService.getCategory(categoryId);
+        List<Post> bestPost = postService.getBestPost(categoryId);
+        return getPostList(bestPost);
     }
 
     /**
-     * entity List를 받아 dto List 데이터로 변환하여 반환합니다.
-     * @param posts entity List
-     * @return 입력된 dto 데이터로 변환된 list
+     * 게시글을 받아 Response 데이터로 변환하여 반환합니다.
+     * @param posts 게시글 list
+     * @return Response list
      */
     private List<PostResponseData> getPostList(List<Post> posts) {
         List<PostResponseData> postRequestData = new ArrayList<>();
@@ -171,24 +172,25 @@ public class PostController {
     }
 
     /**
-     * 게시글의 정보를 받아 게시글을 dto 데이터로 변환하여 반환합니다.
-     * @param post 게시글 정보
-     * @return 입력된 dto 데이터로 변환된 값
+     * 게시글을 받아 Response 데이터로 변환합니다.
+     * @param post 게시글
+     * @return Response 데이터
      */
-    private PostResponseData getPostData(Post post)
-    {
+    private PostResponseData getPostData(Post post){
         if(post == null)
             return null;
 
         return PostResponseData.builder()
-                .category_id(post.getCategory().getId())
-                .user_id(post.getUser().getId())
+                .categoryId(post.getCategory().getId())
+                .userId(post.getUser().getId())
+                .nickname(post.getUser().getNickname())
                 .title(post.getTitle())
                 .content(post.getContent())
-                .status(post.getStatus())
+                .imageUrl(post.getImageUrl())
+                .deleted(post.getDeleted())
                 .updateAt(post.getUpdateAt())
                 .createAt(post.getCreateAt())
-                .like(post.getLikeNumber())
+                .heart(post.getHeart())
                 .id(post.getId())
                 .build();
     }
