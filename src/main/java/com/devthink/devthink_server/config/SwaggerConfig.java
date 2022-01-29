1b10cc6c9dafd2ab8f1573b0039027414c25a0ec
jaehee <vito822@naver.com>
2022-01-29 16:47:04 +0900

    Fix: change swagger version to 3

--- src/main/java/com/devthink/devthink_server/config/SwaggerConfig.java
+++ src/main/java/com/devthink/devthink_server/config/SwaggerConfig.java
@@ -1,8 +1,5 @@
 package com.devthink.devthink_server.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.classmate.TypeResolver;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -12,23 +9,18 @@ import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+import java.util.List;
+
 @RequiredArgsConstructor
+@Configuration
 public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
@@ -38,8 +30,8 @@ public class SwaggerConfig {
 
     @Bean
     public Docket commonApi(){
-
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
                 .alternateTypeRules(AlternateTypeRules
                         .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .apiInfo(apiInfo())
