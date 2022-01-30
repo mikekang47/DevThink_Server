c06910f039ae00cac80253a965f740ae1a125239
linuxlight <lnxlht4j@gmail.com>
2022-01-30 17:38:06 +0900

    Feat: add swagger annotation in CommentRequestDto class

--- src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
+++ src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
@@ -1,5 +1,7 @@
 package com.devthink.devthink_server.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,15 +12,20 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @AllArgsConstructor
+@ApiModel("댓글 등록 정보")
 public class CommentRequestDto {
 
+    @ApiParam(value = "회원 식별자", required = true, example = "1")
     @NotBlank
     private final Long userId;
 
+    @ApiParam(value = "게시글 식별자", example = "1")
     private final Long postId;
 
+    @ApiParam(value = "리뷰 식별자", example = "1")
     private final Long reviewId;
 
+    @ApiParam(value = "댓글 내용", required = true, example = "안녕하세요")
     @NotBlank
     @Size(min=1)
     private final String content;
