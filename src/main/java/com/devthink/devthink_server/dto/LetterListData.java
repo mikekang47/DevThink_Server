0f4677b4fa6b95bebe583a1cfdeca41bfd67eed7
minu kim <kmw106933@naver.com>
2022-01-31 18:01:49 +0900

    Feat: add totalPage entity

--- src/main/java/com/devthink/devthink_server/dto/LetterListData.java
+++ src/main/java/com/devthink/devthink_server/dto/LetterListData.java
@@ -1,33 +1,57 @@
 package com.devthink.devthink_server.dto;
 
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LetterListData {
+
     @ApiModelProperty(notes = "방 번호", example = "1")
-    private Long roomId;    // 방 번호
+    private Long roomId;
 
-    @ApiModelProperty(notes = "상대방 아이디", example = "1")
-    private Long otherId;  // 상대방 아이디
+    @ApiModelProperty(notes = "보낸 사람", example = "1")
+    private Long senderId;
 
-    @ApiModelProperty(notes = "최근 메시지", example = "example")
-    private String content; // 최근 메시지
+    @ApiModelProperty(notes = "받는 사람", example = "1")
+    private Long targetId;
 
-    @ApiModelProperty(notes = "메시지 보낸 시각", example = "2022-01-26T22:07:17.0831141")
-    private LocalDateTime createAt; // 메시지 보낸 시각
+    @ApiModelProperty(notes = "메시지 내용", example = "test")
+    private String content;
 
-    @ApiModelProperty(notes = "안 읽은 메시지 갯수", example = "1")
-    private Long unread; // 안 읽은 메시지 수
+    @ApiModelProperty(notes = "본 시각", example = "2022-01-26T22:07:17.083114")
+    private LocalDateTime viewAt;
 
-    @ApiModelProperty(notes = "상대방 프로필 이미지", example = "example")
-    private String image; // 상대방 이미지;
+    @ApiModelProperty(notes = "생성 시각", example = "2022-01-26T22:07:17.083114")
+    private LocalDateTime createAt;
+
+    @ApiModelProperty(notes = "읽음 체크", example = "false")
+    private Boolean readCheck;
+
+    @ApiModelProperty(notes = "새 메시지 갯수", example = "1")
+    private Long unRead;
+
+    @ApiModelProperty(notes = "상대방 아이디", example = "test")
+    private Long otherId;
+
+    @ApiModelProperty(notes = "상대방 닉네임", example = "test")
+    private String otherNick;
+
+    @ApiModelProperty(notes = "상대방 프로필 이미지", example = "example.com")
+    private String profile;
+
+    @ApiModelProperty(notes = "하트 포함 여부", example = "true")
+    private Boolean heart;
+
+    @ApiModelProperty(notes = "총 페이지 수", example = "1")
+    private int totalPage;
+
+    public void changeUnRead(Long unRead){
+        this.unRead = unRead;
+    }
 }
