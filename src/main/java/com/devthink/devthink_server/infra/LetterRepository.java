0a54a842609190034b797c51c5f1b61c41171066
minu kim <kmw106933@naver.com>
2022-01-31 18:02:31 +0900

    refactor: delete MessageReadCheck and existChat

--- src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/LetterRepository.java
@@ -1,11 +1,14 @@
 package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Letter;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.ManyToOne;
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,36 +17,21 @@ public interface LetterRepository extends JpaRepository<Letter, Long> {
 
     // 방 별 메시지 리스트 가져오기
     @Query("select u from Letter u where u.id " +
-            "in (select max(p.id) from Letter p group by p.roomId) " +
-            "and (u.senderId = :userId or u.targetId = :userId) order by u.id desc")
-    ArrayList<Letter> getMessageList(Long userId);
+            "in (select max(p.id) from Letter p group by p.room.roomId) " +
+            "and (u.sender.id = :userId or u.target.id = :userId) order by u.id desc")
+    Page<Letter> getMessageList(Long userId, Pageable pageable);
 
     // 안읽은 메시지 개수 가져오기
-    @Query("select count(u.id) from Letter u where u.targetId = :userId and u.readCheck = 0 and u.roomId = :roomId")
+    @Query("select count(u.id) from Letter u where u.target.id = :userId and u.readCheck = false and u.room.roomId=:roomId")
     Long countUnread(Long userId, Long roomId);
 
     // 메시지 내용 가져오기
-    @Query("select u from Letter u where u.roomId = :roomId and " +
-            "(u.senderId = :userId or u.targetId = :userId)")
-    ArrayList<Letter> getRoomLists(Long userId, Long roomId);
+    @Query("select u from Letter u where u.room.roomId = :roomId and " +
+            "(u.sender.id = :userId or u.target.id = :userId)")
+    List<Letter> getRoomLists(Long userId, Long roomId);
 
-    // 메시지 읽음 처리하기
-    @Modifying
-    @Query("update Letter u set u.readCheck = 1, u.viewAt = :date where u.roomId = :roomId and " +
-            "u.readCheck = 0 and u.targetId = :userId")
-    int MessageReadCheck(Long userId, Long roomId, LocalDateTime date);
+    @Query("select u from Letter u where u.room.roomId = :roomId and " +
+            "u.target.id = :userId and u.readCheck = false")
+    ArrayList<Letter> getUnreadLists(Long userId, Long roomId);
 
-    // 메시지 이력 검색, 게시글 개수 반환하기
-    @Query("select count(u.id) from Letter u " +
-            "where (u.targetId = :targetId and u.senderId = :senderId) or (u.senderId = :targetId and u.targetId = :senderId)")
-    int existChat(Long targetId, Long senderId);
-
-    // 메시지 테이블의 방번호 최댓값 가져오기
-    @Query("select max(u.roomId) from Letter u")
-    Long maxRoom();
-
-    // 메시지 내역의 방 번호를 가져오기
-    @Query("select u from Letter u where (u.targetId = :targetId " +
-            "and u.senderId = :senderId) or (u.senderId = :targetId and u.targetId = :senderId)")
-    List<Letter> selectRoom(Long targetId, Long senderId, Pageable pageable);
 }
