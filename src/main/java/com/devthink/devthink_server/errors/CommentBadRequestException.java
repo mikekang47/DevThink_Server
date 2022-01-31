03ef9aeadbcded7e9b8303a853a53fbecfce3219
linuxlight <lnxlht4j@gmail.com>
2022-01-31 18:43:31 +0900

    Add: split CommentBadRequestException class into PostCommentBadRequestException, ReviewCommentBadRequestException classes

--- src/main/java/com/devthink/devthink_server/errors/CommentBadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.devthink.devthink_server.errors;
-
-public class CommentBadRequestException extends RuntimeException {
-    public CommentBadRequestException() {
-        super("Can't create comment on Review and Post at the same time, or both null");
-    }
-}
