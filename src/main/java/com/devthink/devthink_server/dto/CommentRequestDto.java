b40c408899d40b3cf303eb2fcfff9f3f664231d7
linuxlight <lnxlht4j@gmail.com>
2022-01-31 16:07:28 +0900

    Refactor: change from ApiParam to ApiModelProperty in CommentRequestDto class

--- src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
+++ src/main/java/com/devthink/devthink_server/dto/CommentRequestDto.java
@@ -1,7 +1,7 @@
 package com.devthink.devthink_server.dto;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,17 +16,17 @@ import javax.validation.constraints.Size;
 @ApiModel("댓글 등록 정보")
 public class CommentRequestDto {
 
-    @ApiParam(value = "회원 식별자", required = true, example = "1")
+    @ApiModelProperty(value = "회원 식별자", required = true, example = "1")
     @NotNull
     private final Long userId;
 
-    @ApiParam(value = "게시글 식별자", example = "1")
+    @ApiModelProperty(value = "게시글 식별자", example = "1")
     private final Long postId;
 
-    @ApiParam(value = "리뷰 식별자", example = "1")
+    @ApiModelProperty(value = "리뷰 식별자", example = "1")
     private final Long reviewId;
 
-    @ApiParam(value = "댓글 내용", required = true, example = "안녕하세요")
+    @ApiModelProperty(value = "댓글 내용", required = true, example = "안녕하세요")
     @NotBlank
     @Size(min=1)
     private final String content;
