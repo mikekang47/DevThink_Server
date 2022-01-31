bf74476f628b99faa8b08e4b35684e6ce6912b28
minu kim <kmw106933@naver.com>
2022-01-31 18:01:35 +0900

    refactor: modify messageSendList method

--- src/main/java/com/devthink/devthink_server/controllers/LetterController.java
+++ src/main/java/com/devthink/devthink_server/controllers/LetterController.java
@@ -1,19 +1,22 @@
 package com.devthink.devthink_server.controllers;
 
 import com.devthink.devthink_server.application.LetterService;
+import com.devthink.devthink_server.application.UserRoomService;
 import com.devthink.devthink_server.application.UserService;
 import com.devthink.devthink_server.domain.Letter;
-import com.devthink.devthink_server.dto.LetterAddData;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.domain.UserRoom;
 import com.devthink.devthink_server.dto.LetterListData;
-import com.devthink.devthink_server.dto.LetterModificationData;
+import com.devthink.devthink_server.dto.LetterSendData;
 import com.devthink.devthink_server.dto.LetterResultData;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,154 +26,110 @@ import java.util.List;
 
 // 1. 쪽지 보내기  -> POST /messages
 // 2. 사용자 id로 방별 메시지 리스트 검색 -> GET /messages?user_id=숫자
-// ---> unread : 안 읽은 메시지 갯수
+// ---> unread : 안 읽은 메시지 갯수, totalPage : 총 페이지 수
 // 3. 사용자 id와 방 id로 메시지 읽기 -> GET /messages/rooms?user_id=숫자&room_id=숫자
 // ---> readCheck 기본 값 0 : 안 읽은 경우, 1로 갱신하여 읽음 처리
 
 @RestController
 @RequestMapping("/messages")
+@RequiredArgsConstructor
 public class LetterController {
 
-    private LetterService letterService;
-    private UserService userService;
-
-    public LetterController(LetterService letterService) {
-        this.letterService = letterService;
-    }
+    private final LetterService letterService;
+    private final UserService userService;
+    private final UserRoomService userRoomService;
 
     /**
-     * 사용자로부터 쪽지 정보를 받아 메시지 리스트에서 쪽지를 보냅니다.
+     * 쪽지 정보를 받아 리스트에서 쪽지를 보냅니다.
      * @param letterAddData 쪽지를 보내기 위한 데이터
-     * @return String 쪽지 성공 여부 문자열로 반환
+     * @return LetterResultData 쪽지 성공 여부
      */
     @PostMapping
     @ApiOperation(value = "메시지 전송",
             notes = "사용자로부터 메시지 정보를 받아 메시지 리스트에서 쪽지를 보냅니다.")
-    public LetterResultData messageSendInlist(@RequestBody @Valid LetterAddData letterAddData) {
-        Letter letter = letterService.addMessage(letterAddData);
+    @ResponseStatus(HttpStatus.CREATED)
+    public LetterResultData messageSendList(@RequestBody @Valid LetterSendData letterAddData) {
+        User sender = userService.getUser(letterAddData.getSenderId());
+        User target = userService.getUser(letterAddData.getTargetId());
+        UserRoom userRoom;
+
+        if (letterAddData.getRoomId() == 0) {
+            int existRoom = userRoomService.existChat(sender.getId(), target.getId());
+
+            if (existRoom == 0) {
+                Long maxRoom = userRoomService.getMaxRoom();
+
+                if (maxRoom == null)
+                    userRoom = userRoomService.save(sender, target, 1L);
+                else
+                    userRoom = userRoomService.save(sender, target, maxRoom + 1);
+
+            } else {
+                userRoom = userRoomService.getExistUserRoom(sender.getId(), target.getId());
+            }
+        } else {
+            userRoom = userRoomService.getUserRoom(sender.getId(), target.getId(), letterAddData.getRoomId());
+        }
+        Letter letter = letterService.addMessage(userRoom, sender, target, letterAddData);
         return getLetterData(letter);
     }
 
     /**
-     * 유저 id를 받아 메시지 리스트를 반환합니다.
-     * @param userId 고유 id 값
+     * 유저 id와 페이지를 받아 메시지 리스트를 반환합니다.
+     *
+     * @param userId, page 고유 id 값, 페이지(기본 1페이지)
      * @return List<LetterResultData> 메시지 리스트
      */
     @GetMapping
     @ApiOperation(value = "메시지 리스트",
-            notes = "사용자로부터 유저 고유 id를 받아 방 별 메시지 리스트를 반환합니다.")
+            notes = "유저 고유 id를 받아 방 리스트를 반환합니다.")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "user_id", dataType = "Long", value = "유저 고유 아이디")})
-    public List<LetterListData> messageList(@RequestParam Long userId) {
-        ArrayList<LetterModificationData> letters = letterService.messageList(userId);
-        return getLetterLists(letters);
+            @ApiImplicitParam(name = "userId", dataType = "Long", value = "유저 고유 아이디"),
+            @ApiImplicitParam(name = "page", dataType = "int", value = "페이지")
+    })
+    public List<LetterListData> messageList(@RequestParam Long userId, @RequestParam(value = "page", defaultValue = "1") int page) {
+        return letterService.messageList(userId, page);
     }
 
     /**
      * 사용자로부터 유저 id와 방 id를 받아 메시지 내용을 가져옵니다.
-     * @param user_id 사용자의 고유 id 값, room_id 사용자의 방 번호
+     *
+     * @param userId 사용자의 고유 id 값, roomId 사용자의 방 번호
      * @return List<LetterResultData> 사용자의 방 번호에서 주고받은 메시지
      */
     @GetMapping("/rooms")
     @ApiOperation(value = "메시지 내용 가져오기",
             notes = "사용자로부터 유저 고유 id와 방 id를 받아 메시지 내용을 가져옵니다.")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "user_id", dataType = "Long", value = "유저 고유 아이디"),
-            @ApiImplicitParam(name = "room_id", dataType = "Long", value = "방 아이디")
+            @ApiImplicitParam(name = "userId", dataType = "Long", value = "유저 아이디"),
+            @ApiImplicitParam(name = "roomId", dataType = "Long", value = "방 아이디")
     })
-    public List<LetterResultData> MessageRoomList(@RequestParam Long user_id, @RequestParam Long room_id) {
-        ArrayList<LetterModificationData> letters = letterService.roomContentList(user_id, room_id);
-        return getLetterDtos(letters);
-
-    }
-
-    /**
-     * LetterModifyData를 받아 LetterListData로 변환합니다.
-     * @param letter  변환하고자 하는 데이터
-     * @return LetterListData 방 별 리스트 데이터
-     */
-    private LetterListData getLetterListData(LetterModificationData letter){
-        if(letter == null)
-            return null;
-
-        return LetterListData.builder()
-                .unread(letter.getUnread())
-                .otherId(letter.getOtherId())
-                .content(letter.getContent())
-                .roomId(letter.getRoomId())
-                .createAt(letter.getCreateAt())
-                .image(letter.getProfile())
-                .build();
-    }
-
-    /**
-     * LetterModifyData List를 받아 LetterListData List 데이터로 변환하여 반환합니다.
-     * @param letters 변환하고자 하는 letterModifyData List
-     * @return 입력된 LetterListData 데이터로 변환된 list
-     */
-    private List<LetterListData> getLetterLists(List<LetterModificationData> letters) {
-        List<LetterListData> letterDtos = new ArrayList<>();
-
-        for (LetterModificationData letter : letters) {
-            letterDtos.add(getLetterListData(letter));
-        }
-        return letterDtos;
-    }
-
-
-    /**
-     * LetterModifyData List를 받아 LetterResultData List 데이터로 변환하여 반환합니다.
-     * @param letters LetterModifyData List
-     * @return LetterResultData로 변환된 list
-     */
-    private List<LetterResultData> getLetterDtos(List<LetterModificationData> letters) {
-        List<LetterResultData> letterDtos = new ArrayList<>();
-
-        for (LetterModificationData letter : letters) {
-            letterDtos.add(getLetterResultData(letter));
-        }
-        return letterDtos;
+    public List<LetterResultData> MessageRoomList(@RequestParam Long userId, @RequestParam Long roomId) {
+        List<LetterResultData> letters = letterService.roomContentList(userId, roomId);
+        return letters;
     }
 
     /**
      * 쪽지의 정보를 받아 쪽지를 dto 데이터(LetterResultData)로 변환하여 반환합니다.
+     *
      * @param letter 쪽지 정보
      * @return 입력된 dto 데이터(LetterResultData)로 변환된 값
      */
     private LetterResultData getLetterData(Letter letter) {
-        if(letter == null)
+        if (letter == null)
             return null;
 
         return LetterResultData.builder()
+                .sendNick(letter.getSender().getNickname())
+                .targetNick(letter.getTarget().getNickname())
                 .content(letter.getContent())
-                .readCheck(letter.getReadCheck())
-                .roomId(letter.getRoomId())
-                .senderId(letter.getSenderId())
-                .targetId(letter.getTargetId())
+                .readCheck(letter.isReadCheck())
+                .roomId(letter.getRoom().getRoomId())
+                .senderId(letter.getSender().getId())
+                .targetId(letter.getTarget().getId())
                 .createAt(letter.getCreateAt())
                 .viewAt(letter.getViewAt())
+                .heart(letter.isHeart())
                 .build();
-
-    }
-
-    /**
-     * LetterModifyData를 받아 쪽지를 LetterResult 데이터로 변환하여 반환합니다.
-     * @param letter 쪽지 정보
-     * @return LetterResult 데이터로 변환된 값
-     */
-    private LetterResultData getLetterResultData(LetterModificationData letter) {
-        if(letter == null)
-            return null;
-
-        return LetterResultData.builder()
-                .content(letter.getContent())
-                .readCheck(letter.getReadCheck())
-                .roomId(letter.getRoomId())
-                .senderId(letter.getSenderId())
-                .targetId(letter.getTargetId())
-                .createAt(letter.getCreateAt())
-                .viewAt(letter.getViewAt())
-                .build();
-
     }
 }
