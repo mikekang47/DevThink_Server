9cc7bbea46ce38b23eaeede8edcd7ab6bdf983e7
linuxlight <lnxlht4j@gmail.com>
2022-01-31 16:06:50 +0900

    Feat: add ApiModelProperty in CommentResponseDto class

--- src/main/java/com/devthink/devthink_server/dto/CommentResponseDto.java
+++ src/main/java/com/devthink/devthink_server/dto/CommentResponseDto.java
@@ -1,5 +1,7 @@
 package com.devthink.devthink_server.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,19 +14,25 @@ import javax.validation.constraints.NotNull;
 @Setter
 @AllArgsConstructor
 @Builder
+@ApiModel("댓글 조회 정보")
 public class CommentResponseDto {
 
+    @ApiModelProperty(value = "댓글 식별자", example = "1")
     @NotNull
     private final Long commentId;
 
+    @ApiModelProperty(value = "사용자 닉네임", example = "싱크싱크")
     @NotBlank
     private final String userNickname;
 
+    @ApiModelProperty(value = "사용자 수준", example = "주니어")
     @NotBlank
     private final String userRole;
 
+    @ApiModelProperty(value = "사용자 프로필 이미지 주소", example = "https://picsum.photos/200")
     private final String userImageUrl;
 
+    @ApiModelProperty(value = "댓글 내용", example = "안녕하세요")
     @NotBlank
     private final String content;
 
