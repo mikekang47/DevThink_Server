6760582d96a4fb38d771832e6a26ca30b7a366c9
linuxlight <lnxlht4j@gmail.com>
2022-01-31 16:08:37 +0900

    Feat: add ApiImplicitParam in CommentController class

--- src/main/java/com/devthink/devthink_server/controllers/CommentController.java
+++ src/main/java/com/devthink/devthink_server/controllers/CommentController.java
@@ -5,6 +5,7 @@ import com.devthink.devthink_server.domain.Comment;
 import com.devthink.devthink_server.dto.CommentRequestDto;
 import com.devthink.devthink_server.dto.CommentResponseDto;
 import com.devthink.devthink_server.errors.CommentContentEmptyException;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -37,9 +38,11 @@ public class CommentController {
 
     /**
      * 특정 사용자의 Comment를 조회합니다.
+     * @param userIdx 댓글을 조회할 사용자의 식별자
      * @return 특정 사용자가 작성한 Comment 리스트
      */
     @ApiOperation(value = "사용자 댓글 조회", notes = "특정 사용자의 댓글을 조회합니다.", response = List.class)
+    @ApiImplicitParam(name = "userIdx", value = "댓글을 조회할 사용자의 식별자")
     @GetMapping("/user/{userIdx}")
     public List<CommentResponseDto> getUserComments(@PathVariable("userIdx") Long userIdx) {
         List<Comment> comments = commentService.getUserComments(userIdx);
@@ -48,9 +51,11 @@ public class CommentController {
 
     /**
      * 특정 Post의 Comment를 조회합니다.
+     * @param postIdx 조회할 대상 게시글의 식별자
      * @return 특정 게시물에 작성된 Comment 리스트
      */
     @ApiOperation(value = "게시글 댓글 조회", notes = "특정 게시글의 댓글을 조회합니다.", response = List.class)
+    @ApiImplicitParam(name = "postIdx", value = "조회할 대상 게시글의 식별자")
     @GetMapping("/post/{postIdx}")
     public List<CommentResponseDto> getPostComments(@PathVariable("postIdx") Long postIdx) {
         List<Comment> comments = commentService.getPostComments(postIdx);
@@ -59,9 +64,11 @@ public class CommentController {
 
     /**
      * 특정 Review의 Comment를 조회합니다.
+     * @param reviewIdx 조회할 대상 리뷰의 식별자
      * @return 특정 리뷰에 작성된 Comment 리스트
      */
     @ApiOperation(value = "리뷰 댓글 조회", notes = "특정 리뷰의 댓글을 조회합니다.", response = List.class)
+    @ApiImplicitParam(name = "reviewIdx", value = "조회할 대상 리뷰의 식별자")
     @GetMapping("/review/{reviewIdx}")
     public List<CommentResponseDto> getReviewComments(@PathVariable("reviewIdx") Long reviewIdx) {
         List<Comment> comments = commentService.getReviewComments(reviewIdx);
@@ -87,6 +94,7 @@ public class CommentController {
     @ApiOperation(value = "댓글 수정",
             notes = "입력된 댓글의 식별자로 수정할 댓글을 찾아, 주어진 데이터로 댓글의 정보를 갱신합니다.",
             response = CommentResponseDto.class)
+    @ApiImplicitParam(name = "commentId", value = "수정할 댓글의 식별자")
     @PatchMapping("/{commentId}")
     @ResponseStatus(HttpStatus.OK)
     public CommentResponseDto updateComment(@PathVariable("commentId") Long commentId,
@@ -103,6 +111,7 @@ public class CommentController {
      * @param commentId 삭제할 Comment의 식별자
      */
     @ApiOperation(value = "댓글 삭제", notes = "입력된 댓글의 식별자로 댓글을 찾아 삭제합니다.")
+    @ApiImplicitParam(name = "commentId", value = "삭제할 Comment의 식별자")
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(@PathVariable("commentId") Long commentId,
