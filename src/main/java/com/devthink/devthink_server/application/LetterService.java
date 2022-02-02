a26a61549c5247c18bd632c3731efa69f9570cb7
minu kim <kmw106933@naver.com>
2022-02-02 15:53:28 +0900

    refactor: make stream method in getMessage method

--- src/main/java/com/devthink/devthink_server/application/LetterService.java
+++ src/main/java/com/devthink/devthink_server/application/LetterService.java
@@ -1,6 +1,7 @@
 package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.Letter;
+import com.devthink.devthink_server.domain.Post;
 import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.domain.UserRoom;
 import com.devthink.devthink_server.dto.LetterResultData;
@@ -8,22 +9,15 @@ import com.devthink.devthink_server.dto.LetterSendData;
 import com.devthink.devthink_server.dto.LetterListData;
 import com.devthink.devthink_server.infra.LetterRepository;
 import com.devthink.devthink_server.infra.UserRepository;
-import com.devthink.devthink_server.infra.UserRoomRepository;
-import com.github.dozermapper.core.Mapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-
-/**
- * 사용자의 요청을 받아, 실제 내부에서 작동하는 클래스 입니다.
- */
+import java.util.stream.Collectors;
 
 @Transactional
 @Service
@@ -32,15 +26,11 @@ public class LetterService {
 
     private final LetterRepository letterRepository;
     private final UserRepository userRepository;
-    private final UserRoomRepository userRoomRepository;
-    private final Mapper mapper;
 
     /**
-     * 메시지 정보를 받아 메시지를 저장합니다.
-     * @param
-     * @return Letter 사용자가 보낸 메시지
+     * 메시지 정보를 받아 메시지를 등록합니다.
      */
-    public Letter addMessage(UserRoom userRoom, User sender, User target, LetterSendData data) {
+    public Letter createMessage(UserRoom userRoom, User sender, User target, LetterSendData data) {
         Letter letter = letterRepository.save(
                 Letter.builder()
                         .content(data.getContent())
@@ -55,93 +45,56 @@ public class LetterService {
 
     /**
      * 전달받은 유저 아이디의 방 메시지 리스트를 불러옵니다.
-     *
-     * @param userId 유저 아이디
-     * @return List<Letter> 메시지 리스트
+     * @param user 유저 정보
      */
-    public List<LetterListData> messageList(Long userId, int page) {
-        Page<Letter> lists = letterRepository.getMessageList(userId, PageRequest.of(page - 1, 8));
-        int totalPage = lists.getTotalPages();
+    public List<LetterListData> getMessageList(User user, Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 8, Sort.by(Sort.Direction.DESC, "id"));
 
-        List<Letter> list = lists.getContent();
-        List<LetterListData> letterListData = new ArrayList<>();
+        List<Letter> messageList = letterRepository.getMessageList(user.getId(), pageable).getContent();
 
-        for (Letter letter : list) {
-            letterListData.add(
-                    LetterListData.builder()
-                            .roomId(letter.getRoom().getRoomId())
-                            .senderId(letter.getSender().getId())
-                            .targetId(letter.getTarget().getId())
-                            .viewAt(letter.getViewAt())
-                            .content(letter.getContent())
-                            .readCheck(letter.isReadCheck())
-                            .heart(letter.isHeart())
-                            .createAt(letter.getCreateAt())
-                            .totalPage(totalPage)
-                            .build()
-            );
-        }
-        for (LetterListData letter : letterListData) {
-            Long unread = letterRepository.countUnread(userId, letter.getRoomId());
-            letter.changeUnRead(unread);
+        List<LetterListData> letterListData = messageList.stream()
+                .map(Letter::toLetterListData)
+                .collect(Collectors.toList());
 
-            String image = null;
-            String Nickname = null;
-            if (userId == letter.getSenderId()) {
-                image = userRepository.findImageUrlById(letter.getTargetId());
-                Nickname = userRepository.findNicknameById(letter.getTargetId());
-            } else if (userId == letter.getTargetId()) {
-                image = userRepository.findImageUrlById(letter.getSenderId());
-                Nickname = userRepository.findNicknameById(letter.getSenderId());
-            }
-            letter.setProfile(image);
-            letter.setOtherNick(Nickname);
-
-            if (userId == letter.getSenderId()) {
-                letter.setOtherId(letter.getTargetId());
-            } else {
-                letter.setOtherId(letter.getSenderId());
-            }
-        }
+        setMessageList(user, letterListData);
         return letterListData;
     }
 
     /**
-     * 전달 받은 유저의 방 별 메시지 내용을 가져오고, 조회수를 업데이트 합니다.
-     *
-     * @param userId 조회하고자 하는 유저 id
-     *               room_id 조회하고자 하는 유저의 방 id
-     * @return ArrayList<Letter> 메시지 리스트 값 반환
+     * 전달 받은 유저의 방 메시지를 읽음 처리 합니다.
+     * @param user, userRoom (유저, 유저 방)
      */
-    public List<LetterResultData> roomContentList(Long userId, Long roomId) {
-        LocalDateTime date = LocalDateTime.now();
-
-        ArrayList<Letter> unreadLists = letterRepository.getUnreadLists(userId, roomId);
-        for (Letter unreadList : unreadLists) {
+    public List<LetterResultData> getMessage(User user, UserRoom userRoom) {
+        List<Letter> unReadLists = letterRepository.getUnReadLists(user.getId(), userRoom.getRoomId());
+        for (Letter unreadList : unReadLists) {
             unreadList.setReadCheck(true);
-            unreadList.setViewAt(date);
-            letterRepository.save(unreadList);
         }
 
-        List<Letter> roomLists = letterRepository.getRoomLists(userId, roomId);
-        List<LetterResultData> LetterLists = new ArrayList<>();
+        List<Letter> roomLists = letterRepository.getRoomLists(user.getId(), userRoom.getRoomId());
+        return roomLists.stream()
+                .map(Letter::toLetterResultData)
+                .collect(Collectors.toList());
+    }
 
-        for (Letter roomList : roomLists) {
-            LetterLists.add(
-                    LetterResultData.builder()
-                            .roomId(roomList.getRoom().getRoomId())
-                            .sendNick(roomList.getSender().getNickname())
-                            .targetNick(roomList.getTarget().getNickname())
-                            .heart(roomList.isHeart())
-                            .targetId(roomList.getTarget().getId())
-                            .senderId(roomList.getSender().getId())
-                            .content(roomList.getContent())
-                            .createAt(roomList.getCreateAt())
-                            .viewAt(roomList.getViewAt())
-                            .readCheck(roomList.isReadCheck())
-                            .build()
-            );
+    // 상대방 프로필, 닉네임 설정
+    private void setMessageList(User user, List<LetterListData> letterListData) {
+        for (LetterListData letter : letterListData) {
+            Long unread = letterRepository.countUnread(user.getId(), letter.getRoomId());
+            letter.setUnRead(unread);
+
+            String image, Nickname;
+            if (user.getId() == letter.getSenderId()) {
+                image = userRepository.findImageUrlById(letter.getTargetId());
+                Nickname = userRepository.findNicknameById(letter.getTargetId());
+                letter.setOtherId(letter.getTargetId());
+            } else {
+                image = userRepository.findImageUrlById(letter.getSenderId());
+                Nickname = userRepository.findNicknameById(letter.getSenderId());
+                letter.setOtherId(letter.getSenderId());
+            }
+            letter.setProfile(image);
+            letter.setOtherNick(Nickname);
         }
-        return LetterLists;
     }
 }
