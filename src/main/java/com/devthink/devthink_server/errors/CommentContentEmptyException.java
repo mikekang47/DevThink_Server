5b18cc355285682947a6122c5ea58553c7a158f0
linuxlight <lnxlht4j@gmail.com>
2022-02-02 15:07:54 +0900

    Refactor: remove CommentContentEmptyException class

--- src/main/java/com/devthink/devthink_server/errors/CommentContentEmptyException.java
+++
@@ -1,7 +0,0 @@
-package com.devthink.devthink_server.errors;
-
-public class CommentContentEmptyException extends RuntimeException {
-    public CommentContentEmptyException() {
-        super("Comment content cannot be empty");
-    }
-}
