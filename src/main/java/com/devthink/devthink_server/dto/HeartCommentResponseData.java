9c66c05eca6628001ed6759000460835497babd9
mikekang47 <gyeong0308@gmail.com>
2022-02-04 17:46:37 +0900

    Feat: add Swagger description

--- src/main/java/com/devthink/devthink_server/dto/HeartCommentResponseData.java
+++ src/main/java/com/devthink/devthink_server/dto/HeartCommentResponseData.java
@@ -1,16 +1,19 @@
 package com.devthink.devthink_server.dto;
 
-import lombok.AllArgsConstructor;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@ApiModel(value = "댓글 좋아요 생성 정보")
 public class HeartCommentResponseData {
     private Long id;
 
+    @ApiParam(value = "사용자 식별자", required = true, example = "24")
     private Long userId;
 
+    @ApiParam(value = "댓글 식별자", required = true, example = "24")
     private Long commentId;
 }
