132a3594aa1b2a563f53386e1601cbf6aac97997
linuxlight <lnxlht4j@gmail.com>
2022-02-06 02:06:00 +0900

    Feat: add findByCommentId method in ReplyRepository class

--- src/main/java/com/devthink/devthink_server/infra/ReplyRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/ReplyRepository.java
@@ -2,6 +2,7 @@ package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,8 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     Reply save(Reply comment);
 
     List<Reply> findByUserId(Long userIdx);
+
+    List<Reply> findByCommentId(@Param(value = "commentId") Long commentId);
+
 
 }
