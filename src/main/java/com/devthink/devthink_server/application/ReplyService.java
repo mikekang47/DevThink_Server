98d879cebaeabae8d0e80fe72b3266c6e0df981a
linuxlight <lnxlht4j@gmail.com>
2022-02-06 01:34:34 +0900

    Feat: add getReplies, getReplyResponseDataList method in ReplyService class

--- src/main/java/com/devthink/devthink_server/application/ReplyService.java
+++ src/main/java/com/devthink/devthink_server/application/ReplyService.java
@@ -1,9 +1,13 @@
 package com.devthink.devthink_server.application;
 
+import com.devthink.devthink_server.domain.Reply;
+import com.devthink.devthink_server.dto.ReplyResponseData;
 import com.devthink.devthink_server.infra.ReplyRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,6 +16,27 @@ public class ReplyService {
 
     public ReplyService(ReplyRepository replyRepository) {
         this.replyRepository = replyRepository;
+    }
+
+    /**
+     * 모든 Reply를 조회합니다.
+     * @return 조회된 모든 Reply
+     */
+    public List<ReplyResponseData> getReplies() {
+        return getReplyResponseDataList(replyRepository.findAll());
+    }
+
+    /**
+     * entity List를 받아 dto List 데이터로 변환하여 반환합니다.
+     * @param replies entity List
+     * @return 입력된 dto 데이터로 변환된 list
+     */
+    private List<ReplyResponseData> getReplyResponseDataList(List<Reply> replies) {
+        List<ReplyResponseData> replyResponseData = new ArrayList<>();
+
+        for (Reply reply : replies)
+            replyResponseData.add(reply.toReplyResponseData());
+        return replyResponseData;
     }
 
 }
