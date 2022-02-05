c5491f7204b4d62dca638729cc6859fd2310341e
linuxlight <lnxlht4j@gmail.com>
2022-02-06 01:35:07 +0900

    Feat: add getReplies method in ReplyController class

--- src/main/java/com/devthink/devthink_server/controllers/ReplyController.java
+++ src/main/java/com/devthink/devthink_server/controllers/ReplyController.java
@@ -3,7 +3,12 @@ package com.devthink.devthink_server.controllers;
 import com.devthink.devthink_server.application.CommentService;
 import com.devthink.devthink_server.application.ReplyService;
 import com.devthink.devthink_server.application.UserService;
+import com.devthink.devthink_server.dto.ReplyResponseData;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies")
@@ -20,5 +25,17 @@ public class ReplyController {
         this.commentService = commentService;
         this.userService = userService;
     }
+
+    /**
+     * 모든 Reply를 조회합니다.
+     * @return DB 상에 존재하는 모든 Reply 리스트
+     */
+    @ApiOperation(value = "전체 대댓글 조회", notes = "모든 대댓글을 조회합니다.", response = List.class)
+    @GetMapping
+    @ApiIgnore
+    public List<ReplyResponseData> getReplies() {
+        return replyService.getReplies();
+    }
+
 
 }
