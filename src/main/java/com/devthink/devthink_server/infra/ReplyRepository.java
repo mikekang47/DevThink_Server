a17be5e6865a9f4d32ae0fcc6e2c9f90d39b3d48
linuxlight <lnxlht4j@gmail.com>
2022-02-06 01:08:45 +0900

    Add: create ReplyRepository interface

---
+++ src/main/java/com/devthink/devthink_server/infra/ReplyRepository.java
@@ -0,0 +1,17 @@
+package com.devthink.devthink_server.infra;
+
+import com.devthink.devthink_server.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findAll();
+
+    Optional<Reply> findById(Long id);
+
+    Reply save(Reply comment);
+}
