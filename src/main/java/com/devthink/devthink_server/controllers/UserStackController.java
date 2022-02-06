6dfbf3c91851d4d4ac9997fdab752141aea1f427
mikekang47 <gyeong0308@gmail.com>
2022-02-07 03:57:55 +0900

    Refactor: add swagger ui description change detail return type StackData to List<StackData>

--- src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
@@ -5,26 +5,45 @@ import com.devthink.devthink_server.application.UserStackService;
 import com.devthink.devthink_server.domain.UserStack;
 import com.devthink.devthink_server.dto.StackData;
 import com.devthink.devthink_server.dto.UserStackData;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 사용자의 스택에 관한 클래스입니다.
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users/stacks")
 public class UserStackController {
     private final UserStackService userStackService;
 
+    /**
+     * 전달받은 사용자 식별자로 사용자의 스택을 조회힙니다.
+     * @param userId 사용자 식별자
+     * @return 사용자의 스택
+     */
     @GetMapping("/{userId}")
-    public StackData detail(@PathVariable Long userId) {
-        UserStack userStack = userStackService.getUserStack(userId);
-        return getUserStackData(userStack);
+    @ApiOperation(value = "사용자 스택 조회", notes="사용자의 스택을 조회합니다.")
+    @ApiImplicitParam(name="userId", dataType = "Long", value = "사용자 식별자")
+    public List<StackData> detail(@PathVariable Long userId) {
+        List<UserStack> sources = userStackService.getUserStack(userId);
+        List<StackData> stacks = new ArrayList<>();
+        for(UserStack stack : sources) {
+            stacks.add(getUserStackData(stack));
+        }
+        return stacks;
     }
 
     @PostMapping("/enroll")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "사용자 스택 생성", notes = "사용자의 스택을 생성합니다.")
     public StackData createUserStack(@RequestBody @Valid UserStackData userStackData) {
         UserStack userStack = userStackService.create(userStackData);
         return getUserStackData(userStack);
