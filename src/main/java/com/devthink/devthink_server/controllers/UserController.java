927c3865db714e9efcc23e42bfb4a56d573fad6f
mikekang47 <gyeong0308@gmail.com>
2022-02-07 03:45:12 +0900

    Refactor: add more swagger description in UserController

--- src/main/java/com/devthink/devthink_server/controllers/UserController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.dto.UserModificationData;
 import com.devthink.devthink_server.dto.UserResultData;
 import com.devthink.devthink_server.dto.UserRegistrationData;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,22 +28,24 @@ public class UserController {
     }
 
     /**
-     * 사용자의 이메일을 받아와 이미 저장되어 있는 이메일인지 확인합니다.
+     * 전달받은 사용자의 이메일이 이미 저장되어 있는 이메일인지 확인합니다.
      * @param userEmail 입력한 사용자의 이메일.
      * @return 이메일이 저장되어 있는지 여부.
      */
-    @ApiOperation(value = "사용자 이메일", notes = "DB에 입력된 이메일의 존재 여부를 리턴합니다.")
+    @ApiOperation(value = "이메일 중복확인", notes = "DB에 입력된 이메일의 존재 여부를 리턴합니다.")
+    @ApiImplicitParam(name="userEmail", dataType = "String", value="사용자 이메일")
     @GetMapping("/emailCheck/{userEmail}")
     public ResponseEntity<Boolean> checkEmail(@PathVariable String userEmail) {
         return ResponseEntity.ok(userService.isDuplicateEmail(userEmail));
     }
 
     /**
-     * 사용자의 닉네임을 받아와 이미 저장되어 있는 닉네임인지 확인합니다.
+     * 전달받은 사용자의 닉네임이 이미 저장되어 있는 닉네임인지 확인합니다.
      * @param nickname 입력한 사용자의 닉네임.
      * @return 닉네임이 저장되어 있는지 여부.
      */
-    @ApiOperation(value="사용자 닉네임", notes = "DB에 입력된 닉네임의 존재 여부를 리턴합니다.")
+    @ApiOperation(value="닉네임 중복확인", notes = "DB에 입력된 닉네임의 존재 여부를 리턴합니다.")
+    @ApiImplicitParam(name="nickname", dataType = "String", value="사용자 닉네임")
     @GetMapping("/nicknameCheck/{nickname}")
     public ResponseEntity<Boolean> checkNickname(@PathVariable String nickname) {
         return ResponseEntity.ok(userService.isDuplicateNickname(nickname));
@@ -54,20 +57,21 @@ public class UserController {
      * @return 사용자
      */
     @GetMapping("/{id}")
-    @ApiOperation(value="사용자 식별자", notes = "입력된 식별자와 일치하는 사용자의 정보를 리턴합니다.")
+    @ApiOperation(value="사용자 조회", notes = "입력된 식별자와 일치하는 사용자의 정보를 리턴합니다.")
+    @ApiImplicitParam(name="id", dataType = "Long", value="사용자 식별자")
     public UserResultData detail(@PathVariable Long id) {
         User user = userService.getUser(id);
         return getUserResultData(user);
     }
 
     /**
-     * 입력한 valid한 사용자의 정보를 받아 사용자를 생성합니다.
+     * 전달받은 valid한 사용자의 정보를 받아 사용자를 생성합니다.
      * @param userRegistrationData 입력한 valid한 사용자의 정보
      * @return 사용자 생성.
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value="가입할 사용자 정보", notes = "입력된 사용자의 정보로 회원 가입을 진행합니다.")
+    @ApiOperation(value="사용자 생성", notes = "입력된 사용자의 정보로 회원 가입을 진행합니다.")
     public UserResultData create(@RequestBody @Valid UserRegistrationData userRegistrationData) {
         User user = userService.registerUser(userRegistrationData);
         return getUserResultData(user);
@@ -75,13 +79,14 @@ public class UserController {
     }
 
     /**
-     * 입력한 사용자의 식별자 값과, valid한 사용자의 정보를 받아, 기존의 정보를 입력한 정보로 변경합니다.
+     * 전달받은 사용자의 식별자 값과, valid한 사용자의 정보를 받아, 기존의 정보를 입력한 정보로 변경합니다.
      * @param id 사용자의 식별자
      * @param modificationData 사용자의 새로운 정보
      * @return 기존 사용자의 정보 수정
      */
     @PatchMapping("/{id}")
-    @ApiOperation(value = "정보를 갱신할 사용자의 식별자와 갱신할 정보", notes = "입력된 사용자의 식별자로 수정할 사용자를 찾아, 주어진 데이터로 사용자의 정보를 갱신합니다.")
+    @ApiOperation(value = "사용자 업데이트", notes = "전달받은 사용자의 식별자로 수정할 사용자를 찾아, 주어진 데이터로 사용자의 정보를 갱신합니다.")
+    @ApiImplicitParam(name="id", dataType = "Long", value="사용자 식별자")
     public UserResultData update(
             @PathVariable Long id,
             @RequestBody @Valid UserModificationData modificationData
@@ -91,12 +96,13 @@ public class UserController {
     }
 
     /**
-     * 입력한 사용자의 식별자 값을 전달받아, 해당하는 사용자를 삭제합니다.
+     * 전달받은 사용자의 식별자 값을 전달받아, 해당하는 사용자를 삭제합니다.
      * @param id 삭제하고자 하는 사용자의 식별자
      */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiOperation(value = "삭제할 사용자의 식별자", notes = "입력된 사용자 식별자를 가진 사용자를 삭제합니다.")
+    @ApiOperation(value = "사용자 삭제", notes = "전달받은 사용자 식별자를 가진 사용자를 삭제합니다.")
+    @ApiImplicitParam(name="id", dataType = "Long", value="사용자 식별자")
     public void destroy(@PathVariable Long id) {
         userService.deleteUser(id);
     }
