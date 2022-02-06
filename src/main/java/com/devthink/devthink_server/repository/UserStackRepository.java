e47c8573aabdc6de754722364ed58eff45f03077
mikekang47 <gyeong0308@gmail.com>
2022-02-07 02:11:26 +0900

    Feat: add save method in UserStackRepository

--- src/main/java/com/devthink/devthink_server/repository/UserStackRepository.java
+++ src/main/java/com/devthink/devthink_server/repository/UserStackRepository.java
@@ -1,2 +1,12 @@
-package com.devthink.devthink_server.repository;public class UserStackRepository {
+package com.devthink.devthink_server.repository;
+
+import com.devthink.devthink_server.domain.UserStack;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserStackRepository extends JpaRepository<UserStack, Long> {
+
+    UserStack save(UserStack userStack);
+
 }
