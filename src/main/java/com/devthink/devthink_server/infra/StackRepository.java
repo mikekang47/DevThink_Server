558699e20d0f55b19901e4575d9c6501e786e9a9
mikekang47 <gyeong0308@gmail.com>
2022-02-07 02:14:39 +0900

    Feat: add findAll() method in StackRepository

--- src/main/java/com/devthink/devthink_server/infra/StackRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/StackRepository.java
@@ -4,6 +4,7 @@ import com.devthink.devthink_server.domain.Stack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +13,7 @@ public interface StackRepository extends JpaRepository<Stack, Long> {
     Optional<Stack> findById(Long id);
 
     Stack save(Stack stack);
+
+    List<Stack> findAll();
 
 }
