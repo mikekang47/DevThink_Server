77ca6201f9ab3a6392ee5152f561f3f39bdc7650
mikekang47 <gyeong0308@gmail.com>
2022-02-07 02:47:09 +0900

    Feat: add detail method in UserStackController

--- src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
@@ -5,6 +5,7 @@ import com.devthink.devthink_server.application.UserStackService;
 import com.devthink.devthink_server.domain.UserStack;
 import com.devthink.devthink_server.dto.StackData;
 import com.devthink.devthink_server.dto.UserStackData;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,11 @@ import javax.validation.Valid;
 @RequestMapping("/users/stacks")
 public class UserStackController {
     private final UserStackService userStackService;
+
+    @Getter("/{id}")
+    public StackData detail(@PathVariable Long userId) {
+        UserStack userStack = userStackService.getUserStack(userId);
+    }
 
     @PostMapping("/enroll")
     @ResponseStatus(HttpStatus.CREATED)
