04193cbdf868f6faecee46e9e04ab787c819c636
mikekang47 <gyeong0308@gmail.com>
2022-02-07 03:14:04 +0900

    Refactor: change method name to getUserStackData change pathvariable name

--- src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
@@ -5,7 +5,6 @@ import com.devthink.devthink_server.application.UserStackService;
 import com.devthink.devthink_server.domain.UserStack;
 import com.devthink.devthink_server.dto.StackData;
 import com.devthink.devthink_server.dto.UserStackData;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +17,24 @@ import javax.validation.Valid;
 public class UserStackController {
     private final UserStackService userStackService;
 
-    @Getter("/{id}")
+    @GetMapping("/{userId}")
     public StackData detail(@PathVariable Long userId) {
         UserStack userStack = userStackService.getUserStack(userId);
+        return getUserStackData(userStack);
     }
 
     @PostMapping("/enroll")
     @ResponseStatus(HttpStatus.CREATED)
     public StackData createUserStack(@RequestBody @Valid UserStackData userStackData) {
         UserStack userStack = userStackService.create(userStackData);
-        return toResponseData(userStack);
+        return getUserStackData(userStack);
     }
     
-    private StackData toResponseData(UserStack userStack) {
+    private StackData getUserStackData(UserStack userStack) {
         return StackData.builder()
                 .name(userStack.getStack().getName())
                 .build();
 
     }
+
 }
