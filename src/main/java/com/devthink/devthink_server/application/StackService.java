8a870a7d1777677bf4a26254fc85c032d77c3dca
mikekang47 <gyeong0308@gmail.com>
2022-02-07 02:27:59 +0900

    Feat: add getStacks and register method in StackService

--- src/main/java/com/devthink/devthink_server/application/StackService.java
+++ src/main/java/com/devthink/devthink_server/application/StackService.java
@@ -1,29 +1,37 @@
 package com.devthink.devthink_server.application;
 
 import com.devthink.devthink_server.domain.Stack;
+import com.devthink.devthink_server.dto.StackData;
 import com.devthink.devthink_server.errors.StackNotFoundException;
 import com.devthink.devthink_server.infra.StackRepository;
+import com.github.dozermapper.core.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class StackService {
     private final StackRepository stackRepository;
+    private final Mapper mapper;
 
-    public List<String> getStacks(List<Long> stackIds) {
-        List<String> stacks = new ArrayList<>();
-        for(Long id: stackIds) {
-            Stack stack = stackRepository.findById(id).orElseThrow(() -> new StackNotFoundException(id));
-            if(stack != null) {
-                stacks.add(stack.getName());
-            }
-        }
-        return stacks;
+    public Stack getStack(Long id) {
+        return findStack(id);
     }
 
+    private Stack findStack(Long id) {
+        return stackRepository.findById(id).orElseThrow(() -> new StackNotFoundException(id));
+    }
 
+    public List<Stack> getStacks() {
+        return stackRepository.findAll();
+    }
+
+    public Stack register(StackData stackData) {
+        Stack stack = mapper.map(stackData, Stack.class);
+        return stackRepository.save(stack);
+    }
 }
