428d33808212d2602f457a6b05100a97766b209a
mikekang47 <gyeong0308@gmail.com>
2022-02-06 17:30:49 +0900

    Feat: add method for saving and finding in StackRepository

--- src/main/java/com/devthink/devthink_server/infra/StackRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/StackRepository.java
@@ -1,2 +1,16 @@
-package com.devthink.devthink_server.infra;public class StackRepository {
+package com.devthink.devthink_server.infra;
+
+import com.devthink.devthink_server.domain.Stack;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StackRepository extends JpaRepository<Stack, Long> {
+
+    Optional<Stack> findById(Long id);
+
+    Stack save(Stack stack);
+
 }
