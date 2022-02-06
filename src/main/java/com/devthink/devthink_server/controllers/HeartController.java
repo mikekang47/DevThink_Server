a484397ad8280a4bb47dd3d22c9640326a06e701
mikekang47 <gyeong0308@gmail.com>
2022-02-07 03:26:09 +0900

    Refactor: change request API pathvariable to obejct

--- src/main/java/com/devthink/devthink_server/controllers/HeartController.java
+++ src/main/java/com/devthink/devthink_server/controllers/HeartController.java
@@ -35,53 +35,53 @@ public class HeartController {
     }
 
     /**
-     * 좋아요를 생성하려는 게시글의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다.
-     * @param postId 좋아요를 생성하려는 게시글의 식별자
-     * @param userId 좋아요를 생성하는 사용자의 식별자
+     * 좋아요를 생성하려는 게시글 객체와 사용자 객체로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다.
+     * @param post 좋아요를 생성하려는 게시글
+     * @param user 좋아요를 생성하는 사용자
      * @return 생성된 좋아요의 정보
      */
     @ApiOperation(
-            value= "좋아요를 생성하려는 게시글의 식별자와 좋아요를 생성하려는 사용자의 식별자",
-            notes = "좋아요를 생성하려는 게시글의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다."
+            value= "좋아요를 생성하려는 게시글 객체와 좋아요를 생성하려는 사용자 객체",
+            notes = "좋아요를 생성하려는 게시글의 객체와 사용자의 객체로 새로운 좋아요를 생성하여, 생성된 좋아요의 정보를 리턴합니다."
     )
-    @PostMapping("/post/{postId}/{userId}")
+    @PostMapping("/post")
     @ResponseStatus(HttpStatus.CREATED)
-    public HeartPostResponseData createPostHeart(@PathVariable Long postId, @PathVariable Long userId) {
-        Heart heart = heartService.createPostHeart(postId, userId);
+    public HeartPostResponseData createPostHeart(@RequestBody Post post, @RequestBody User user) {
+        Heart heart = heartService.createPostHeart(post, user);
         return getPostHeartData(heart);
     }
 
     /**
      * 좋아요를 생성하려는 댓글의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다.
-     * @param commentId 좋아요를 생성하려는 댓글의 식별자
-     * @param userId 좋아요를 생성하는 사용자의 식별자
+     * @param comment 좋아요를 생성하려는 댓글의 식별자
+     * @param user 좋아요를 생성하는 사용자의 식별자
      * @return 생성된 좋아요의 정보
      */
     @ApiOperation(
-            value = "좋아요를 생성하려는 댓글의 식별자와 좋아요를 생성하는 사용자의 식별자",
-            notes = "좋아요를 생성하려는 댓글의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다."
+            value = "좋아요를 생성하려는 댓글 객체와 좋아요를 생성하는 사용자 객체",
+            notes = "좋아요를 생성하려는 댓글의 객체와 사용자 객체로 새로운 좋아요를 생성하여, 생성된 좋아요의 정보를 리턴합니다."
     )
-    @PostMapping("/comment/{commentId}/{userId}")
+    @PostMapping("/comment")
     @ResponseStatus(HttpStatus.CREATED)
-    public HeartCommentResponseData createCommentHeart(@PathVariable Long commentId, @PathVariable Long userId) {
-        Heart heart = heartService.createCommentHeart(commentId, userId);
+    public HeartCommentResponseData createCommentHeart(@RequestBody Comment comment, @RequestBody User user) {
+        Heart heart = heartService.createCommentHeart(comment, user);
         return getCommentHeartData(heart);
     }
 
     /**
-     * 좋아요를 생성하려는 리뷰의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다.
-     * @param reviewId 좋아요를 생성하려는 게시글의 식별자
-     * @param userId 좋아요를 생성하는 사용자의 식별자
+     * 좋아요를 생성하려는 리뷰 객체와 사용자 객체로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다.
+     * @param review 좋아요를 생성하려는 게시글
+     * @param user 좋아요를 생성하는 사용자
      * @return 생성된 좋아요의 정보
      */
     @ApiOperation(
-            value="좋아요를 생성하려는 게시글의 식별자와 좋아요를 생성하는 사용자의 식별자",
-            notes ="좋아요를 생성하려는 리뷰의 식별자와 사용자의 식별자로 새로운 좋아요를 생성하여, 그 정보를 리턴합니다."
+            value="좋아요를 생성하려는 게시글 객체와 좋아요를 생성하는 사용자 객체",
+            notes ="좋아요를 생성하려는 리뷰 객체와 사용자 객체로 새로운 좋아요를 생성하여, 생성된 좋아요의 정보를 리턴합니다."
     )
-    @PostMapping("/review/{reviewtId}/{userId}")
+    @PostMapping("/review")
     @ResponseStatus(HttpStatus.CREATED)
-    public HeartReviewResponseData createReviewHeart(@PathVariable Long reviewId, @PathVariable Long userId) {
-        Heart heart = heartService.createReviewHeart(reviewId, userId);
+    public HeartReviewResponseData createReviewHeart(@RequestBody Review review, @RequestBody User user) {
+        Heart heart = heartService.createReviewHeart(review, user);
         return getReviewHeartData(heart);
     }
 
