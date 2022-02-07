d563a64ed8ecf425dea1e41a4121d9e6183959b5
mikekang47 <gyeong0308@gmail.com>
2022-02-07 15:48:37 +0900

    Feat: add findById method in UserStackRepository

--- src/main/java/com/devthink/devthink_server/infra/UserStackRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/UserStackRepository.java
@@ -1,10 +1,11 @@
-package com.devthink.devthink_server.repository;
+package com.devthink.devthink_server.infra;
 
 import com.devthink.devthink_server.domain.UserStack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserStackRepository extends JpaRepository<UserStack, Long> {
@@ -12,4 +13,6 @@ public interface UserStackRepository extends JpaRepository<UserStack, Long> {
     UserStack save(UserStack userStack);
 
     List<UserStack> findAllByUserId(Long userId);
+
+    Optional<UserStack> findById(Long stackId);
 }
