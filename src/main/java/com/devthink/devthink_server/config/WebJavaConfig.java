32af046c747b7fe0c8d1375cd24b10773de9d740
mikekang47 <gyeong0308@gmail.com>
2022-02-07 17:28:41 +0900

    Feat: make WebJavaConfig

---
+++ src/main/java/com/devthink/devthink_server/config/WebJavaConfig.java
@@ -0,0 +1,22 @@
+package com.devthink.devthink_server.config;
+
+import com.devthink.devthink_server.filters.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebJavaConfig implements WebMvcConfigurer {
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    public WebJavaConfig(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor);
+    }
+    
+}
