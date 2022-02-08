2cb44190833dfff91e485cc71baab3697b241520
mikekang47 <gyeong0308@gmail.com>
2022-02-08 23:28:19 +0900

    Refactor: make clear about JwtuAuthenticationFilter

--- src/main/java/com/devthink/devthink_server/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/devthink/devthink_server/filters/JwtAuthenticationFilter.java
@@ -1,6 +1,13 @@
 package com.devthink.devthink_server.filters;
 
+import com.devthink.devthink_server.application.AuthenticationService;
+import com.devthink.devthink_server.errors.InvalidTokenException;
+import com.devthink.devthink_server.security.UserAuthentication;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -10,18 +17,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    private final AuthenticationService authenticationService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationService authenticationService) {
         super(authenticationManager);
+        this.authenticationService = authenticationService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain chain) throws IOException, ServletException {
-        // TODO : authentication
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization != null) {
+            String accessToken = authorization.substring("Bearer ".length());
+            Long userId = authenticationService.parseToken(accessToken);
+            Authentication authentication = new UserAuthentication(userId);
+
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
         
+
         chain.doFilter(request, response);
 
     }
+    
 }
