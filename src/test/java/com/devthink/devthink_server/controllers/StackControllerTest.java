cd3b38d404acb1264f0daf0a10ea2f8cfa596c4f
mikekang47 <gyeong0308@gmail.com>
2022-02-11 03:10:51 +0900

    Test: make createTest about stack

--- src/test/java/com/devthink/devthink_server/controllers/StackControllerTest.java
+++ src/test/java/com/devthink/devthink_server/controllers/StackControllerTest.java
@@ -3,18 +3,23 @@ package com.devthink.devthink_server.controllers;
 import com.devthink.devthink_server.application.AuthenticationService;
 import com.devthink.devthink_server.application.StackService;
 import com.devthink.devthink_server.domain.Stack;
+import com.devthink.devthink_server.dto.StackData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,12 +39,31 @@ class StackControllerTest {
     void setUp() {
         Stack stack = new Stack(1L, "C/C++");
         given(stackService.getStacks()).willReturn(List.of(stack));
+
+        given(stackService.register(any(StackData.class))).will(
+                invocation -> {
+                    StackData stackData = invocation.getArgument(0);
+
+                    return Stack.builder()
+                            .name(stackData.getName())
+                            .build();
+
+                });
     }
     
     @Test
-    void 모든_스택을_불러오는_경우() throws Exception {
+    void 모든_스택을_조회하는_경우() throws Exception {
         mvc.perform(get("/stacks"))
                 .andExpect(content().string(containsString("C/C++")))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 스택을_등록하려는_경우() throws Exception {
+        mvc.perform(post("/stacks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"C/C++\"}")
+                )
+                .andExpect(status().isCreated());
     }
 }
