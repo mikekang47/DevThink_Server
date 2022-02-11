f7786d5d043b5e5007f24c56a041357a233fede8
jaehee <vito822@naver.com>
2022-02-11 15:43:17 +0900

    Feat: add required option in swagger

--- src/main/java/com/devthink/devthink_server/dto/BookRequestData.java
+++ src/main/java/com/devthink/devthink_server/dto/BookRequestData.java
@@ -14,18 +14,18 @@ import javax.validation.constraints.NotNull;
 public class BookRequestData {
 
     @NotNull
-    @ApiModelProperty(value = "책 isbn 번호", example = "8960773417")
+    @ApiModelProperty(value = "책 isbn 번호", required = true, example = "8960773417")
     private String isbn;
 
     @NotBlank
-    @ApiModelProperty(value = "책 이름", example = "토비의 스프링 3.1 Vol. 1: 스프링의 이해와 원리(에이콘 오픈소스 프로그래밍 시리즈 18)")
+    @ApiModelProperty(value = "책 이름", required = true, example = "토비의 스프링 3.1 Vol. 1: 스프링의 이해와 원리(에이콘 오픈소스 프로그래밍 시리즈 18)")
     private String name;
 
     @NotBlank
-    @ApiModelProperty(value = "책 저자", example = "이일민")
+    @ApiModelProperty(value = "책 저자", required = true, example = "이일민")
     private String writer;
 
-    @ApiModelProperty(value = "책 표지 이미지 url", example = "https://image.aladin.co.kr/product/19700/99/cover500/e896077341_2.jpg")
+    @ApiModelProperty(value = "책 표지 이미지 url", required = true, example = "https://image.aladin.co.kr/product/19700/99/cover500/e896077341_2.jpg")
     private String imgUrl;
 
 }
