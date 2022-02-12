1f6df2fa6559f0862bc715693c24fed13b3d6305
mikekang47 <gyeong0308@gmail.com>
2022-02-13 03:42:22 +0900

    Refactor: chagne return type for UserStackController Feat: add deleteUserStack method Feat: add @PreAuthorize

--- src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
+++ src/main/java/com/devthink/devthink_server/controllers/UserStackController.java
@@ -5,11 +5,11 @@ import com.devthink.devthink_server.application.AuthenticationService;
 import com.devthink.devthink_server.application.UserStackService;
 import com.devthink.devthink_server.domain.UserStack;
 import com.devthink.devthink_server.dto.StackData;
-import com.devthink.devthink_server.dto.UserStackData;
+import com.devthink.devthink_server.dto.UserStackRequestData;
+import com.devthink.devthink_server.dto.UserStackResponseData;
 import com.devthink.devthink_server.security.UserAuthentication;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -22,25 +22,30 @@ import java.util.List;
  * 사용자의 스택에 관한 클래스입니다.
  */
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/users/stacks")
 public class UserStackController {
     private final UserStackService userStackService;
     private final AuthenticationService authenticationService;
 
+    public UserStackController(UserStackService userStackService, AuthenticationService authenticationService) {
+        this.userStackService = userStackService;
+        this.authenticationService = authenticationService;
+    }
+
     /**
      * 전달받은 사용자 식별자로 사용자의 스택을 조회힙니다.
-     * @param accessToken 사용자 토큰
+     *
      * @return 사용자의 스택
      */
-    @GetMapping("/{accessToken}")
+    @GetMapping
     @ApiOperation(value = "사용자 스택 조회", notes="사용자의 스택을 조회합니다.")
+    @PreAuthorize("isAuthenticated()")
     @ApiImplicitParam(name="userId", dataType = "Long", value = "사용자 식별자")
-    public List<StackData> detail(@PathVariable String accessToken) {
-        Long userId = authenticationService.parseToken(accessToken);
+    public List<UserStackResponseData> detail(UserAuthentication userAuthentication) {
+        Long userId = userAuthentication.getUserId();
 
         List<UserStack> sources = userStackService.getUserStacks(userId);
-        List<StackData> stacks = new ArrayList<>();
+        List<UserStackResponseData> stacks = new ArrayList<>();
         for(UserStack stack : sources) {
             stacks.add(getUserStackData(stack));
         }
@@ -51,23 +56,24 @@ public class UserStackController {
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
     @ApiOperation(value = "사용자 스택 생성", notes = "사용자의 스택을 생성합니다.")
-    public StackData createUserStack(@RequestBody @Valid UserStackData userStackData) {
-        Long userId = authenticationService.parseToken(userStackData.getAccessToken());
-        UserStack userStack = userStackService.create(userId, userStackData);
+    public UserStackResponseData createUserStack(@RequestBody @Valid UserStackRequestData userStackRequestData, UserAuthentication userAuthentication) {
+        Long userId = userAuthentication.getUserId();
+        UserStack userStack = userStackService.create(userId, userStackRequestData);
         return getUserStackData(userStack);
     }
 
-    @PatchMapping("/{accessToken}")
+    @DeleteMapping("{id}")
     @ApiOperation(value = "사용자 스택 수정", notes = "사용자의 스택을 수정합니다.")
-    public StackData updateUserStack(@PathVariable String accessToken, @RequestBody @Valid UserStackData userStackData) {
-        Long userId = authenticationService.parseToken(accessToken);
-        UserStack userStack = userStackService.update(userId, userStackData);
-        return getUserStackData(userStack);
+    @PreAuthorize("isAuthenticated()")
+    public void deleteUserStack(@PathVariable Long id,UserAuthentication userAuthentication) {
+        userStackService.destroy(id);
     }
 
-    private StackData getUserStackData(UserStack userStack) {
-        return StackData.builder()
-                .name(userStack.getStack().getName())
+    private UserStackResponseData getUserStackData(UserStack userStack) {
+        return UserStackResponseData.builder()
+                .id(userStack.getId())
+                .stackId(userStack.getStack().getId())
+                .userId(userStack.getUser().getId())
                 .build();
 
     }
