906b5cb86fb8b42f75f3bb6893d8afda1afcb856
mikekang47 <gyeong0308@gmail.com>
2022-02-13 04:26:26 +0900

    Refactor: about createPostHeart in HeartService

--- src/main/java/com/devthink/devthink_server/application/HeartService.java
+++ src/main/java/com/devthink/devthink_server/application/HeartService.java
@@ -3,16 +3,23 @@ package com.devthink.devthink_server.application;
 import com.devthink.devthink_server.domain.*;
 import com.devthink.devthink_server.errors.*;
 import com.devthink.devthink_server.infra.*;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class HeartService {
     private final HeartRepository heartRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public HeartService(HeartRepository heartRepository, PostRepository postRepository,
+                        UserRepository userRepository) {
+        this.heartRepository = heartRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
 
     /**
      * 전달받은 식별자로 좋아요의 정보를 가져오고, 없을경우 예외를 발생합니다.
@@ -26,14 +33,25 @@ public class HeartService {
 
     /**
      * 전달받은 게시글 식별자와 사용자 식별자로 좋아요를 생성합니다.
-     * @param post 좋아요 누를 게시글
-     * @param user 좋아요 누른 사용자
+     * @param postId 좋아요 누를 게시글의 식별자
+     * @param userId 좋아요 누른 사용자의 식별자
      * @return 생성된 게시글 좋아요 객체
      */
-    public Heart createPostHeart(Post post, User user) {
+    public Heart createPostHeart(Long postId, Long userId) {
+        Post post = findPost(postId);
         post.updateHeart(post.getHeartCnt()+1);
+        User user = findUser(userId);
         Heart heart = Heart.builder().user(user).post(post).build();
         return heartRepository.save(heart);
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findByIdAndDeletedIsFalse(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    private Post findPost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId));
     }
 
     /**
