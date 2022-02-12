13ad4f8b10df04061f35a35db0d89de3013d0baa
mikekang47 <gyeong0308@gmail.com>
2022-02-13 03:14:42 +0900

    Refactor: change class name to UserStackRequestData

--- src/main/java/com/devthink/devthink_server/dto/UserStackRequestData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserStackRequestData.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @Builder
 @ApiModel("사용자 스택 정보")
-public class UserStackData {
+public class UserStackRequestData {
     @NotNull
     @ApiModelProperty(value = "스택 식별자", example = "20")
     private Long stackId;
