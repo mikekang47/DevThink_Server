76fbf270d6f927fa212d53a802da980a558d03fb
mikekang47 <gyeong0308@gmail.com>
2022-02-13 02:56:01 +0900

    Fix: bug fix about UserStackData

--- src/main/java/com/devthink/devthink_server/dto/UserStackData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserStackData.java
@@ -5,18 +5,17 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @ApiModel("사용자 스택 정보")
 public class UserStackData {
     @NotNull
-    @ApiModelProperty(value = "사용자 토큰", example = "1231dafaber324d1")
-    private String accessToken;
-
     @ApiModelProperty(value = "스택 식별자", example = "20")
     private Long stackId;
 
