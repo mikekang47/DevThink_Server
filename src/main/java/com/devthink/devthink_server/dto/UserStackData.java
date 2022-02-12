eeec8e091184238190dea315e7339f27a2384c3f
mikekang47 <gyeong0308@gmail.com>
2022-02-12 21:08:15 +0900

    Refactor: change userId to accessToken

--- src/main/java/com/devthink/devthink_server/dto/UserStackData.java
+++ src/main/java/com/devthink/devthink_server/dto/UserStackData.java
@@ -3,17 +3,19 @@ package com.devthink.devthink_server.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@Builder
 @ApiModel("사용자 스택 정보")
 public class UserStackData {
     @NotNull
-    @ApiModelProperty(value = "사용자 식별자", example = "20")
-    private Long userId;
+    @ApiModelProperty(value = "사용자 토큰", example = "1231dafaber324d1")
+    private String accessToken;
 
     @ApiModelProperty(value = "스택 식별자", example = "20")
     private Long stackId;
