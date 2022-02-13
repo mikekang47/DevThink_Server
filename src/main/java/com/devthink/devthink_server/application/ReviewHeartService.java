4ee8470647ab2111c0a47fc9bac62599ce52fd8f
mikekang47 <gyeong0308@gmail.com>
2022-02-14 02:44:43 +0900

    Feat: move data from HeartController, Service, Repository to ReviewHeart Controller and Service, Repository

--- src/main/java/com/devthink/devthink_server/application/ReviewHeartService.java
+++ src/main/java/com/devthink/devthink_server/application/ReviewHeartService.java
@@ -1,2 +1,69 @@
-package com.devthink.devthink_server.application;public class ReviewHeartService {
+package com.devthink.devthink_server.application;
+
+import com.devthink.devthink_server.domain.Heart;
+import com.devthink.devthink_server.domain.Review;
+import com.devthink.devthink_server.domain.ReviewHeart;
+import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.errors.HeartAlreadyExistsException;
+import com.devthink.devthink_server.errors.HeartNotFoundException;
+import com.devthink.devthink_server.errors.ReviewNotFoundException;
+import com.devthink.devthink_server.errors.UserNotFoundException;
+import com.devthink.devthink_server.infra.ReviewRepository;
+import com.devthink.devthink_server.infra.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewHeartService {
+
+    private final ReviewHeartRepository reviewHeartRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+
+    public ReviewHeartService(ReviewHeartRepository reviewHeartRepository, UserRepository userRepository, ReviewRepository reviewRepository) {
+        this.reviewHeartRepository = reviewHeartRepository;
+        this.userRepository = userRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    /**
+     * 전달받은 리뷰 객체와 사용자 객체로 좋아요를 생성합니다.
+     *
+     * @param reviewId 리뷰 식별자
+     * @param userId   사용자 식별자
+     * @return 생성된 댓글 좋아요 객체
+     */
+    public ReviewHeart createReviewHeart(Long reviewId, Long userId) {
+        Review review = findReview(reviewId);
+        User user = findUser(userId);
+
+        if(reviewHeartRepository.existsByReviewIdAndUserId(reviewId, userId)) {
+            throw new HeartAlreadyExistsException();
+        }
+
+        review.updateHeart(review.getHeartCnt() + 1);
+        ReviewHeart reviewHeart = ReviewHeart.builder()
+                .user(user)
+                .review(review)
+                .build();
+        return reviewHeartRepository.save(reviewHeart);
+    }
+
+    public void destroyReviewHeart(Long reviewId) {
+        Review review = findReview(reviewId);
+        ReviewHeart reviewHeart = reviewHeartRepository.findByReviewId(reviewId)
+                .orElseThrow(HeartNotFoundException::new);
+
+        review.updateHeart(review.getHeartCnt() - 1);
+        reviewHeartRepository.deleteById(reviewHeart.getId());
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findByIdAndDeletedIsFalse(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    private Review findReview(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewNotFoundException(reviewId));
+    }
 }
--- src/main/java/com/devthink/devthink_server/controllers/ReviewHeartController.java
+++ src/main/java/com/devthink/devthink_server/controllers/ReviewHeartController.java
@@ -1,2 +1,71 @@
-package com.devthink.devthink_server.controllers;public class ReviewHeartController {
+package com.devthink.devthink_server.controllers;
+
+import com.devthink.devthink_server.application.ReviewHeartService;
+import com.devthink.devthink_server.domain.Heart;
+import com.devthink.devthink_server.domain.ReviewHeart;
+import com.devthink.devthink_server.dto.ReviewHeartResponseData;
+import com.devthink.devthink_server.security.UserAuthentication;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("reviews/hearts")
+public class ReviewHeartController {
+    private final ReviewHeartService reviewHeartService;
+
+    public ReviewHeartController(ReviewHeartService reviewHeartService) {
+        this.reviewHeartService = reviewHeartService;
+    }
+
+    /**
+     * 좋아요를 생성하려는 리뷰 식별자와 사용자 식별자로 새로운 리뷰 좋아요를 생성하여, 그 정보를 리턴합니다.
+     * @param reviewId 좋아요를 생성하려는 리뷰 식별자
+     * @return 생성된 좋아요의 정보
+     */
+    @ApiOperation(
+            value = "리뷰 좋아요 생성",
+            notes ="좋아요를 생성하려는 리뷰의 식별자와 사용자 토큰으로 새로운 리뷰 좋아요를 생성하여, 생성된 좋아요의 정보를 리턴합니다. 헤더에 사용자 토큰 주입을 필요로 합니다.",
+            response = ReviewHeartResponseData.class
+    )
+    @PostMapping("/reviews/{reviewId}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReviewHeartResponseData createReviewHeart(@PathVariable Long reviewId, UserAuthentication userAuthentication) {
+        Long userId = userAuthentication.getUserId();
+        ReviewHeart reviewHeart = reviewHeartService.createReviewHeart(reviewId, userId);
+        return getReviewHeartData(reviewHeart);
+    }
+
+    /**
+     * 좋아요를 취소하고자 하는 리뷰의 식별자를 받아 좋아요를 취소합니다.
+     * @param reviewId 좋아요를 취소하고자 하는 리뷰의 식별자
+     */
+    @ApiOperation(
+            value= "리뷰 좋아요 삭제(좋아요 취소)",
+            notes= "좋아요를 취소하고자 하는 댓글의 식별자를 받아 좋아요를 취소합니다. 헤더에 사용자 토큰 주입을 필요로 합니다."
+    )
+    @ApiImplicitParam(name="reviewId", dataType = "integer", value = "좋아요를 취소하고자 하는 리뷰의 식별자")
+    @DeleteMapping("/reviews/{reviewId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("isAuthenticated()")
+    public void destroyReviewHeart(@PathVariable Long reviewId) {
+        reviewHeartService.destroyReviewHeart(reviewId);
+    }
+
+    /**
+     * 리뷰와 관련한 좋아요의 정보를 받아서 dto로 변환후 리턴합니다.
+     * @param reviewHeart 변환할 좋아요의 정보
+     * @return dto로 변환된 좋아요 정보
+     */
+    private ReviewHeartResponseData getReviewHeartData(ReviewHeart reviewHeart) {
+        return ReviewHeartResponseData.builder()
+                .id(reviewHeart.getId())
+                .userId(reviewHeart.getUser().getId())
+                .reviewId(reviewHeart.getReview().getId())
+                .build();
+    }
+
 }
