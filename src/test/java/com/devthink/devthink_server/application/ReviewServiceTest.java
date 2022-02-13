31ae75a74628c57022a93dc32bdc465537fd93ef
jaehee <vito822@naver.com>
2022-02-14 02:40:10 +0900

    Test: refactor ReviewServiceTest

--- src/test/java/com/devthink/devthink_server/application/ReviewServiceTest.java
+++ src/test/java/com/devthink/devthink_server/application/ReviewServiceTest.java
@@ -3,6 +3,7 @@ package com.devthink.devthink_server.application;
 import com.devthink.devthink_server.domain.Book;
 import com.devthink.devthink_server.domain.Review;
 import com.devthink.devthink_server.domain.User;
+import com.devthink.devthink_server.dto.ReviewModificationData;
 import com.devthink.devthink_server.dto.ReviewRequestData;
 import com.devthink.devthink_server.infra.BookRepository;
 import com.devthink.devthink_server.infra.ReviewRepository;
@@ -33,7 +34,7 @@ class ReviewServiceTest {
     UserRepository userRepository;
 
     User user = User.builder().id(1L).point(10).build();
-    Book book = Book.builder().id(1L).isbn("1234567891234").build();
+    Book book = Book.builder().id(1L).isbn("1234567891234").reviewCnt(1).scoreAvg(BigDecimal.valueOf(3.0)).build();
     Review review = Review.builder()
             .id(1L)
             .user(user)
@@ -48,35 +49,69 @@ class ReviewServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this); //@Mock이 붙은 객체를 생성, 초기화
         reviewService = new ReviewService(reviewRepository, bookRepository);
-
-        given(reviewRepository.save(any(Review.class))).willReturn(review);
-
-        given(reviewRepository.findByIdAndDeletedIsFalse(1L)).willReturn(Optional.of(review));
-
-        given(userRepository.save(user)).willReturn(user);
     }
 
     @Test
-    void 리뷰작성시_유저_포인트_증가() {
+    void 리뷰생성성공() {
         //given
         int prePoint = user.getPoint();
         ReviewRequestData reviewRequestData = ReviewRequestData.builder()
                 .point(review.getPoint())
+                .score(review.getScore())
                 .build();
+        given(reviewRepository.save(any(Review.class))).willReturn(review);
+        given(userRepository.save(any(User.class))).willReturn(user);
+        given(bookRepository.save(any(Book.class))).willReturn(book);
+        given(bookRepository.calcScoreAvg(book.getId())).willReturn(BigDecimal.valueOf(3.75));
         //when
         reviewService.createReview(user, book, reviewRequestData);
         //then
-        assertThat(user.getPoint()).isEqualTo(prePoint + review.getPoint());
+        assertThat(user.getPoint()).isEqualTo(prePoint + review.getPoint()); // 유저 포인트 증가
+        assertThat(book.getReviewCnt()).isEqualTo(2);   // 책 리뷰 수 증가
+        assertThat(book.getScoreAvg()).isEqualTo(BigDecimal.valueOf(3.75)); // 책 평점 변화
     }
 
     @Test
-    void 리뷰삭제시_유저_포인트_감소() {
+    void 리뷰수정성공() {
+        //given
+        ReviewModificationData reviewModificationData = ReviewModificationData.builder().title("수정제목").content("수정내용").score(BigDecimal.valueOf(4.5)).build();
+        Review modifiedReview = Review.builder()
+                .id(1L)
+                .user(user)
+                .book(book)
+                .title(reviewModificationData.getTitle())
+                .content(reviewModificationData.getContent())
+                .score(reviewModificationData.getScore())
+                .point(5)
+                .build();
+        given(reviewRepository.findByIdAndDeletedIsFalse(any(Long.class))).willReturn(Optional.of(review));
+        given(bookRepository.calcScoreAvg(book.getId())).willReturn(reviewModificationData.getScore());
+        given(bookRepository.save(any(Book.class))).willReturn(book);
+        given(reviewRepository.save(any(Review.class))).willReturn(modifiedReview);
+        BigDecimal preScoreAvg = book.getScoreAvg();
+        //when
+        reviewService.update(1L, reviewModificationData);
+        //then
+        assertThat(review.getTitle()).isEqualTo(reviewModificationData.getTitle()); // title 변경
+        assertThat(review.getContent()).isEqualTo(reviewModificationData.getContent()); // content 변경
+        assertThat(review.getScore()).isEqualTo(reviewModificationData.getScore()); // score 변경
+        assertThat(book.getScoreAvg()).isEqualTo(reviewModificationData.getScore()); // 책 평점 계산
+    }
+
+    @Test
+    void 리뷰삭제성공() {
         //given
         int prePoint = user.getPoint();
+        int preReviewCnt = book.getReviewCnt();
+        given(reviewRepository.findByIdAndDeletedIsFalse(any(Long.class))).willReturn(Optional.of(review));
+        given(bookRepository.calcScoreAvg(book.getId())).willReturn(BigDecimal.valueOf(0));
         //when
         reviewService.deleteReview(review.getId());
         //then
-        assertThat(user.getPoint()).isEqualTo(prePoint - review.getPoint());
+        assertThat(review.getDeleted()).isEqualTo(true); // 리뷰 deleted == false
+        assertThat(user.getPoint()).isEqualTo(prePoint - review.getPoint()); // 유저 포인트 감소
+        assertThat(book.getScoreAvg()).isEqualTo(BigDecimal.valueOf(0)); // 책 평점 계산
+        assertThat(book.getReviewCnt()).isEqualTo(preReviewCnt - 1); // 책 리뷰 수 감소
     }
 
 }
