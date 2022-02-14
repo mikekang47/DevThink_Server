74ab44f4278287e66f2e170d638ac4d44fc08c4e
minu kim <kmw106933@naver.com>
2022-02-14 22:22:06 +0900

    refactor: change error response in PostReportBadRequestException

--- src/main/java/com/devthink/devthink_server/errors/PostReportBadRequestException.java
+++ src/main/java/com/devthink/devthink_server/errors/PostReportBadRequestException.java
@@ -1,5 +1,5 @@
 package com.devthink.devthink_server.errors;
 
 public class PostReportBadRequestException extends RuntimeException {
-    public PostReportBadRequestException() { super("can't report post with invalid approach!"); }
+    public PostReportBadRequestException() { super("Can't report own posts!"); }
 }
