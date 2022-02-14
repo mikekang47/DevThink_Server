53e3cfa662c4913d1968db1719b27f71a2e2cd2b
minu kim <kmw106933@naver.com>
2022-02-14 22:23:05 +0900

    refactor: rename PostDeleteBadRequestException to UserNotMatchException

--- src/main/java/com/devthink/devthink_server/errors/PostDeleteBadRequestException.java
+++
@@ -1,5 +0,0 @@
-package com.devthink.devthink_server.errors;
-
-public class PostDeleteBadRequestException extends RuntimeException {
-    public PostDeleteBadRequestException() {  super("Can't delete post with invalid userId!"); }
-}
