40ef53a6a55f3cbd961fe2dc2eb6946a2248d2ba
minu kim <kmw106933@naver.com>
2022-02-15 23:07:16 +0900

    refactor: add query annotation in UserRepository

--- src/main/java/com/devthink/devthink_server/infra/UserRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/UserRepository.java
@@ -23,8 +23,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByNickname(String userNickName);
 
-    String findImageUrlById(Long targetId);
-    
+    @Query("select u.imageUrl from User u where u.id = :id")
+    String findImageUrlById(Long id);
+
+    @Query("select u.nickname from User u where u.id = :id")
     String findNicknameById(Long id);
 
 }
