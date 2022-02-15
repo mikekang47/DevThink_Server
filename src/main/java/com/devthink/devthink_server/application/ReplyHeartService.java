2c2ba9c408426e3b2e161bb47ef9f2d8a015a6bf
mikekang47 <gyeong0308@gmail.com>
2022-02-16 02:08:28 +0900

    Feat: make ReplyHeartService and make create method

---
+++ src/main/java/com/devthink/devthink_server/application/ReplyHeartService.java
@@ -0,0 +1,12 @@
+package com.devthink.devthink_server.application;
+
+import com.devthink.devthink_server.domain.ReplyHeart;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReplyHeartService {
+
+    public ReplyHeart create(Long replyId, Long userId) {
+        return null;
+    }
+}
