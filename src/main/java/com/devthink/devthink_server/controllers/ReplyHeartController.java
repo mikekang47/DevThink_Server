f15fab19a3829f2fa46ce8c606eaca572c5faa82
mikekang47 <gyeong0308@gmail.com>
2022-02-16 01:24:05 +0900

    Feat: make ReplyHeartController

---
+++ src/main/java/com/devthink/devthink_server/controllers/ReplyHeartController.java
@@ -0,0 +1,11 @@
+package com.devthink.devthink_server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/replies/hearts")
+public class ReplyHeartController {
+
+
+}
