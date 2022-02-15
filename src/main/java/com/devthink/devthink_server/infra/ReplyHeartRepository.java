0bf248dba8642f02d495b77e6b51cbb0f6f8900d
mikekang47 <gyeong0308@gmail.com>
2022-02-16 02:41:45 +0900

    Feat: make ReplyHeartRepository

---
+++ src/main/java/com/devthink/devthink_server/infra/ReplyHeartRepository.java
@@ -0,0 +1,11 @@
+package com.devthink.devthink_server.infra;
+
+import com.devthink.devthink_server.domain.ReplyHeart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyHeartRepository extends JpaRepository<ReplyHeart, Long> {
+
+    boolean existsByReplyIdAndUserId(Long replyId, Long userId);
+}
