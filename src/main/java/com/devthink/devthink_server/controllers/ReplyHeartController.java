bb10a1a771ab9a695e580bc36b0eb7961e7a31ac
mikekang47 <gyeong0308@gmail.com>
2022-02-16 02:06:48 +0900

    Refactor: add authentication process in ReplyController

--- src/main/java/com/devthink/devthink_server/controllers/ReplyHeartController.java
+++ src/main/java/com/devthink/devthink_server/controllers/ReplyHeartController.java
@@ -1,11 +1,30 @@
 package com.devthink.devthink_server.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devthink.devthink_server.application.ReplyHeartService;
+import com.devthink.devthink_server.domain.ReplyHeart;
+import com.devthink.devthink_server.security.UserAuthentication;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/replies/hearts")
 public class ReplyHeartController {
 
+    private final ReplyHeartService replyHeartService;
+
+    public ReplyHeartController(ReplyHeartService replyHeartService) {
+        this.replyHeartService = replyHeartService;
+    }
+
+    @PostMapping("/{replyId}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createReplyHeart(@PathVariable Long replyId, UserAuthentication userAuthentication) {
+        Long userId = userAuthentication.getUserId();
+        ReplyHeart replyHeart = replyHeartService.create(replyId, userId);
+
+    }
 
 }
