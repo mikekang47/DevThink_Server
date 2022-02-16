fadf3896a7e399de071e4488ef1e912fb9d91a3d
mikekang47 <gyeong0308@gmail.com>
2022-02-17 01:19:41 +0900

    Feat: add deleteById method in ReplyUserRepository

--- src/main/java/com/devthink/devthink_server/infra/ReplyHeartRepository.java
+++ src/main/java/com/devthink/devthink_server/infra/ReplyHeartRepository.java
@@ -4,10 +4,14 @@ import com.devthink.devthink_server.domain.ReplyHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReplyHeartRepository extends JpaRepository<ReplyHeart, Long> {
 
     boolean existsByReplyIdAndUserId(Long replyId, Long userId);
 
     ReplyHeart save(ReplyHeart replyHeart);
+
+    Optional<ReplyHeart> findByReplyIdAndUserId(Long replyId, Long userId);
 }
