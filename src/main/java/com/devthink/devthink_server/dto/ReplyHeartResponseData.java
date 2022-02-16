c54eb07e5c1741e1b02945e791fafea9a51034a2
mikekang47 <gyeong0308@gmail.com>
2022-02-17 01:44:46 +0900

    Feat: add swagger description in ReplyHeartResponseData

--- src/main/java/com/devthink/devthink_server/dto/ReplyHeartResponseData.java
+++ src/main/java/com/devthink/devthink_server/dto/ReplyHeartResponseData.java
@@ -1,5 +1,7 @@
 package com.devthink.devthink_server.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,12 +9,15 @@ import javax.validation.constraints.NotBlank;
 
 @Builder
 @Getter
+@ApiModel(value = "답글 좋아요 생성 정보")
 public class ReplyHeartResponseData {
     private Long id;
 
     @NotBlank
+    @ApiParam(name="userId", value="사용자 식별자", required = true, example="12")
     private Long userId;
 
     @NotBlank
+    @ApiParam(name="replyId", value="답글 식별자", required = true, example="12")
     private Long replyId;
 }
