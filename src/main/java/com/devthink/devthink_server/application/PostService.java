7d03ad42604a5e47a28511458fb6767270fc22b2
minu kim <kmw106933@naver.com>
2022-02-17 23:24:38 +0900

    refactor: add description in checkPostReport

--- src/main/java/com/devthink/devthink_server/application/PostService.java
+++ src/main/java/com/devthink/devthink_server/application/PostService.java
@@ -42,7 +42,9 @@ public class PostService {
 
     /**
      * 해당 유저가 해당 게시글을 신고한 기록이 있는지 확인합니다.
-     *
+     * @param user 게시글을 신고한 유저
+     * @param post 유저가 신고한 게시글
+     * @return boolean 신고 기록 여부
      */
     public boolean checkPostReport(User user, Post post){
         return postReportRepository.existsPostReport(user.getId(), post.getId());
@@ -127,7 +129,7 @@ public class PostService {
      * @param post 삭제할 게시글
      * @return Post 삭제된 게시글
      */
-    public Post deletePost(User user, Post post){
+    public void deletePost(User user, Post post){
         getPostById(post.getId());
         // 만약 유저 아이디와 게시글의 유저 아이디가 같다면
         if(user.getId() == post.getUser().getId()) {
@@ -137,7 +139,6 @@ public class PostService {
         else {
             throw new UserNotMatchException();
         }
-        return post;
     }
 
     /**
