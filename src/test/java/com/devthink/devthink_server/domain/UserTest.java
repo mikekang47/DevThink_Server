2d8f985bb995d16402e55bffe236fbf8f37cc3e9
mikekang47 <gyeong0308@gmail.com>
2022-02-20 23:40:58 +0900

    Test: change passwordEncoder injection way in Usertest

--- src/test/java/com/devthink/devthink_server/domain/UserTest.java
+++ src/test/java/com/devthink/devthink_server/domain/UserTest.java
@@ -1,10 +1,20 @@
 package com.devthink.devthink_server.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     @Test
     void changeWith() {
@@ -27,7 +37,7 @@ class UserTest {
     void 비밀번호_변경() {
         User user = User.builder().build();
 
-        user.changePassword("TEST");
+        user.changePassword("TEST", passwordEncoder);
 
         assertThat(user.getPassword()).isNotEmpty();
         assertThat(user.getPassword()).isNotEqualTo("TEST");
@@ -45,13 +55,11 @@ class UserTest {
 
     @Test
     void 존재하는_사용자_인증() {
-        User user = User.builder()
-                .password("test")
-                .deleted(true)
-                .build();
+        User user = User.builder().build();
+        user.changePassword("test", passwordEncoder);
 
-        assertThat(user.authenticate("test")).isFalse();
-        assertThat(user.authenticate("xxx")).isFalse();
+        assertThat(user.authenticate("test", passwordEncoder)).isTrue();
+        assertThat(user.authenticate("xxx", passwordEncoder)).isFalse();
 
     }
 
