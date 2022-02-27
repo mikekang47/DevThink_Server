f528a0b73f37e7c0fe8987c02e9be581d3d83d40
minu kim <kmw106933@naver.com>
2022-02-27 15:57:09 +0900

    Feat: get valid messageList and valid readCheck test in LetterServiceTest

--- src/test/java/com/devthink/devthink_server/application/LetterServiceTest.java
+++ src/test/java/com/devthink/devthink_server/application/LetterServiceTest.java
@@ -3,11 +3,15 @@ package com.devthink.devthink_server.application;
 import com.devthink.devthink_server.domain.Letter;
 import com.devthink.devthink_server.domain.User;
 import com.devthink.devthink_server.domain.UserRoom;
+import com.devthink.devthink_server.dto.LetterListData;
 import com.devthink.devthink_server.dto.LetterSendData;
 import com.devthink.devthink_server.infra.LetterRepository;
 import com.devthink.devthink_server.infra.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +46,56 @@ public class LetterServiceTest {
                             .build();
                 }
         );
+
+        given(letterRepository.getMessageList(any(Long.class))).will(
+                invocation -> {
+                    User sender = User.builder().id(1L).build();
+                    User target = User.builder().id(2L).build();
+                    UserRoom userRoom = UserRoom.builder().roomId(1L).user1(sender).user2(target).build();
+
+                    Letter letter = Letter.builder()
+                            .id(1L)
+                            .room(userRoom)
+                            .sender(sender)
+                            .target(target)
+                            .content("test")
+                            .build();
+
+                    List<Letter> letters = new ArrayList<>();
+                    letters.add(letter);
+                    return letters;
+                }
+        );
+
+        given(letterRepository.getUnReadLists(any(Long.class), any(Long.class))).will(
+                invocation -> {
+                    User sender = User.builder().id(1L).build();
+                    Long userId = invocation.getArgument(0);
+                    Long roomId = invocation.getArgument(1);
+
+                    User target = User.builder().id(userId).build();
+                    UserRoom userRoom = UserRoom.builder().roomId(roomId).user1(sender).user2(target).build();
+
+                    Letter letter = Letter.builder()
+                            .id(1L)
+                            .room(userRoom)
+                            .sender(sender)
+                            .target(target)
+                            .content("test")
+                            .readCheck(false)
+                            .build();
+
+                    List<Letter> letters = new ArrayList<>();
+                    letters.add(letter);
+
+                    return letters;
+                }
+        );
+
+        given(letterRepository.countUnread(any(Long.class), any(Long.class))).willReturn(1L);
+
+        given(userRepository.findImageUrlById(any(Long.class))).willReturn("test.com");
+        given(userRepository.findNicknameById(any(Long.class))).willReturn("tester");
     }
 
     @Test
@@ -58,4 +112,29 @@ public class LetterServiceTest {
 
         verify(letterRepository).save(any(Letter.class));
     }
+
+    @Test
+    void 올바른_정보로_메시지_리스트를_불러오는_경우() {
+        User user = User.builder().id(2L).build();
+
+        List<LetterListData> messageList = letterService.getMessageList(user);
+
+        assertThat(messageList.get(0).getContent()).isEqualTo("test");
+        assertThat(messageList.get(0).getOtherNick()).isEqualTo("tester");
+        assertThat(messageList.get(0).getProfile()).isEqualTo("test.com");
+    }
+
+    @Test
+    void 올바른_정보로_메시지를_읽음처리_하는_경우() {
+        User sender = User.builder().id(1L).build();
+        User target = User.builder().id(2L).build();
+        UserRoom userRoom = UserRoom.builder().roomId(1L).user1(sender).user2(target).build();
+
+        List<Letter> letters = letterService.setReadCheck(target, userRoom);
+
+        assertThat(letters.get(0).getSender().getId()).isEqualTo(1L);
+        assertThat(letters.get(0).getTarget().getId()).isEqualTo(2L);
+        assertThat(letters.get(0).isReadCheck()).isEqualTo(true);
+    }
+
 }
