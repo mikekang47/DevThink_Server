1bbd4f678025a5438a3783141980e2a6ac89aa84
minu kim <kmw106933@naver.com>
2022-03-10 16:40:53 +0900

    refactor: change the order in LetterListData

--- src/main/java/com/devthink/devthink_server/dto/LetterListData.java
+++ src/main/java/com/devthink/devthink_server/dto/LetterListData.java
@@ -24,16 +24,10 @@ public class LetterListData {
     @ApiModelProperty(notes = "메시지 내용", example = "test")
     private String content;
 
-    @ApiModelProperty(notes = "생성 시각", example = "2022-01-26T22:07:17.083114")
-    private LocalDateTime createAt;
-
-    @ApiModelProperty(notes = "읽음 체크", example = "false")
-    private Boolean readCheck;
-
     @ApiModelProperty(notes = "새 메시지 갯수", example = "1")
     private Long unRead;
 
-    @ApiModelProperty(notes = "상대방 아이디", example = "test")
+    @ApiModelProperty(notes = "상대방 인덱스", example = "test")
     private Long otherId;
 
     @ApiModelProperty(notes = "상대방 닉네임", example = "test")
@@ -42,7 +36,7 @@ public class LetterListData {
     @ApiModelProperty(notes = "상대방 프로필 이미지", example = "example.com")
     private String profile;
 
-    @ApiModelProperty(notes = "하트 포함 여부", example = "true")
-    private Boolean heart;
+    @ApiModelProperty(notes = "보낸 시각", example = "2022-01-26T22:07:17.083114")
+    private LocalDateTime createAt;
 
 }
