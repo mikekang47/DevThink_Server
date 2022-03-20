10e4f89098fe8676d125455f4bae7eaeea483d51
Seokjoo Cho <lnxlht4j@gmail.com>
2022-03-20 19:53:33 +0900

    Refactor: all methods in ReplyService class now returns Reply entity

--- src/main/java/com/devthink/devthink_server/application/ReplyService.java
+++ src/main/java/com/devthink/devthink_server/application/ReplyService.java
@@ -3,7 +3,6 @@ package com.devthink.devthink_server.application;
 import com.devthink.devthink_server.domain.Comment;
 import com.devthink.devthink_server.domain.Reply;
 import com.devthink.devthink_server.domain.User;
-import com.devthink.devthink_server.dto.ReplyResponseData;
 import com.devthink.devthink_server.errors.CommentNotFoundException;
 import com.devthink.devthink_server.errors.ReplyNotFoundException;
 import com.devthink.devthink_server.errors.UserNotFoundException;
@@ -13,7 +12,6 @@ import com.devthink.devthink_server.infra.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -35,8 +33,8 @@ public class ReplyService {
      * 모든 Reply를 조회합니다.
      * @return 조회된 모든 Reply
      */
-    public List<ReplyResponseData> getReplies() {
-        return getReplyResponseDataList(replyRepository.findAll());
+    public List<Reply> getReplies() {
+        return replyRepository.findAll();
     }
 
     /**
@@ -54,13 +52,13 @@ public class ReplyService {
      * @param userIdx 대댓글을 조회할 사용자의 식별자
      * @return 특정 사용자가 작성한 Reply 리스트
      */
-    public List<ReplyResponseData> getUserReplies(Long userIdx) {
+    public List<Reply> getUserReplies(Long userIdx) {
         if (!userRepository.existsById(userIdx))
             throw new UserNotFoundException(userIdx);
         List<Reply> userReplies = replyRepository.findByUserId(userIdx);
         if (userReplies.isEmpty())
             throw new ReplyNotFoundException();
-        return getReplyResponseDataList(userReplies);
+        return userReplies;
     }
 
     /**
@@ -68,13 +66,13 @@ public class ReplyService {
      * @param commentIdx 조회할 대상 댓글의 식별자
      * @return 특정 댓글에 작성된 Reply 리스트
      */
-    public List<ReplyResponseData> getCommentReplies(Long commentIdx) {
+    public List<Reply> getCommentReplies(Long commentIdx) {
         if (!commentRepository.existsById(commentIdx))
             throw new CommentNotFoundException(commentIdx);
         List<Reply> commentReplies = replyRepository.findByCommentId(commentIdx);
         if (commentReplies.isEmpty())
             throw new ReplyNotFoundException();
-        return getReplyResponseDataList(commentReplies);
+        return commentReplies;
     }
 
     /**
@@ -82,9 +80,9 @@ public class ReplyService {
      * @param user Reply를 등록하려고 하는 User
      * @param comment Reply와 연결되는 Comment
      * @param content Reply의 내용
-     * @return 생성된 Reply의 결과 정보
+     * @return 생성된 Reply
      */
-    public ReplyResponseData createReply(User user, Comment comment, String content) {
+    public Reply createReply(User user, Comment comment, String content) {
         // replyRepository에 새로운 대댓글을 생성합니다.
         return replyRepository.save(
                 Reply.builder()
@@ -92,18 +90,19 @@ public class ReplyService {
                         .comment(comment)
                         .content(content)
                         .build()
-        ).toReplyResponseData();
+        );
     }
 
     /**
      * replyId를 통하여 기존의 Reply를 수정합니다.
      * @param replyId 수정할 Reply의 식별자
      * @param content 수정할 content 내용
+     * @return 수정된 Reply
      */
-    public ReplyResponseData updateReply(Long replyId, String content) {
+    public Reply updateReply(Long replyId, String content) {
         Reply reply = getReply(replyId);
         reply.setContent(content);
-        return replyRepository.save(reply).toReplyResponseData();
+        return replyRepository.save(reply);
     }
 
     /**
@@ -113,19 +112,6 @@ public class ReplyService {
     public void deleteReply(Long replyId) {
         getReply(replyId);
         replyRepository.deleteById(replyId);
-    }
-
-    /**
-     * entity List를 받아 dto List 데이터로 변환하여 반환합니다.
-     * @param replies entity List
-     * @return 입력된 dto 데이터로 변환된 list
-     */
-    private List<ReplyResponseData> getReplyResponseDataList(List<Reply> replies) {
-        List<ReplyResponseData> replyResponseData = new ArrayList<>();
-
-        for (Reply reply : replies)
-            replyResponseData.add(reply.toReplyResponseData());
-        return replyResponseData;
     }
 
 }
